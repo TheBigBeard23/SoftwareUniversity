7bab75750c6a60e60901a7dab52c238e3d92961b
Vladislav Dimitrov <Bones4pk@gmail.com>
2021-12-21 22:11:08 +0200

    Add new exercises

---
+++ Programming Basics/02.ConditionalStatements/GodzillaVsKong/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace GodzillaVsKong
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double movieBudget = double.Parse(Console.ReadLine());
+            int extrasCount = int.Parse(Console.ReadLine());
+            double suitPrice = double.Parse(Console.ReadLine());
+
+            double decorPrice = movieBudget * 0.10;
+            double suitsTotalPrice = extrasCount*suitPrice;
+            double movieTotalPrice = decorPrice + suitsTotalPrice;
+
+            if (extrasCount > 150)
+            {
+                suitsTotalPrice -= suitsTotalPrice * 0.10;
+            }
+
+            if (movieBudget >= movieTotalPrice)
+            {
+                Console.WriteLine($"Action!{Environment.NewLine}" +
+                                  $"Wingard starts filming with { movieBudget - movieTotalPrice:f2} leva left.");
+            }
+
+            else
+            {
+                Console.WriteLine($"Not enough money!{Environment.NewLine}" +
+                                  $"Wingard needs {movieTotalPrice-movieBudget:f2} leva more.");
+            }
+        }
+    }
+}
---
+++ Programming Basics/02.ConditionalStatements/ToyShop/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ToyShop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double tripCost = double.Parse(Console.ReadLine());
+
+            int puzzlesCount = int.Parse(Console.ReadLine());
+            int dollsCount = int.Parse(Console.ReadLine());
+            int bearsCount = int.Parse(Console.ReadLine());
+            int minionsCount = int.Parse(Console.ReadLine());
+            int trucksCount = int.Parse(Console.ReadLine());
+
+            int toysTotalCount = puzzlesCount + dollsCount + bearsCount +
+                                 minionsCount + trucksCount;
+
+            double totalSum = puzzlesCount * 2.60 +
+                              dollsCount * 3.00 +
+                              bearsCount * 4.10 +
+                              minionsCount * 8.20 +
+                              trucksCount * 2.00;
+            if (toysTotalCount >= 50)
+            {
+                totalSum -= totalSum * 0.25;
+            }
+            totalSum -= totalSum * 0.1;
+
+            if (totalSum >= tripCost)
+            {
+                Console.WriteLine($"Yes! {totalSum - tripCost:f2} lv left.");
+            }
+            else
+            {
+                Console.WriteLine($"Not enough money! {tripCost - totalSum:f2} lv needed.");
+            }
+
+
+        }
+    }
+}
+
