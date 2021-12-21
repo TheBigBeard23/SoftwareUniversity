658c15d00e424fa1172c73a8371cbcfc2e557558
Vladislav Dimitrov <Bones4pk@gmail.com>
2021-12-21 18:17:42 +0200

    Edit exercises

---
+++ Programming Basics/02.ConditionalStatements/BonusScore/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace BonusScore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int points = int.Parse(Console.ReadLine());
+            double bonus = 0.0;
+            if (points % 2 == 0)
+            {
+                bonus++;
+
+            }
+            if(points % 10==5)
+            {
+                bonus += 2;
+            }
+            if (points <= 100)
+            {
+                bonus += 5;
+            }
+            else if (points > 100)
+            {
+                bonus += points * 0.2;
+            }
+            else if(points > 1000)
+            {
+                bonus += points * 0.1;
+            }
+            Console.WriteLine(bonus);
+            Console.WriteLine(points + bonus);
+        }
+    }
+}
