73adfbd8af0423864e706eb40dccf4f14b8063df
Vladislav Dimitrov <Bones4pk@gmail.com>
2021-12-22 20:19:50 +0200

    Add new exercises

---
+++ Programming Basics/03.ConditionalStatementsAdvanced/DayOfWeek/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DayOfWeek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> dayOfTheWeek = new Dictionary<int, string>();
+            dayOfTheWeek[1] = "Monday";
+            dayOfTheWeek[2] = "Tuesday";
+            dayOfTheWeek[3] = "Wednesday";
+            dayOfTheWeek[4] = "Thursday";
+            dayOfTheWeek[5] = "Friday";
+            dayOfTheWeek[6] = "Saturday";
+            dayOfTheWeek[7] = "Sunday";
+
+            int num = int.Parse(Console.ReadLine());
+
+            if (dayOfTheWeek.ContainsKey(num))
+            {
+                Console.WriteLine(dayOfTheWeek[num]);
+            }
+            else
+            {
+                Console.WriteLine("Error");
+            }
+        }
+    }
+}
---
+++ Programming Basics/03.ConditionalStatementsAdvanced/WeekendOrWorkingDay/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WeekendOrWorkingDay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
