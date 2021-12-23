537b72f10ff14f352f4771d840a218be4502bd5b
TheBigBeard23 <Bones4pk@gmail.com>
2021-12-23 18:29:44 +0200

    Add new exercise

--- Programming Basics/03.ConditionalStatementsAdvanced/Cinema/Program.cs
+++ Programming Basics/03.ConditionalStatementsAdvanced/Cinema/Program.cs
@@ -6,7 +6,30 @@ namespace Cinema
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+            string type = Console.ReadLine();
+            int rows = int.Parse(Console.ReadLine());
+            int columns = int.Parse(Console.ReadLine());
+      
+            int seats = rows * columns;
+            double price = 0;
+  
+            switch (type)
+            {
+                case "Premiere":
+                    price = 12;
+                    break;
+                case "Normal":
+                    price = 7.50;
+                    break;
+                case "Discount":
+                    price = 5;
+                    break;
+
+            }
+            double total = price * seats;
+
+            Console.WriteLine($"{total:f2} leva");
         }
     }
 }
