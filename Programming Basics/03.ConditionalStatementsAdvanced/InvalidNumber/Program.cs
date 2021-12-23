9a07cfb5c82bfa1d5e0aaa1f9e9da430f3937935
Vladislav Dimitrov <Bones4pk@gmail.com>
2021-12-23 15:29:57 +0200

    Add new exercise

---
+++ Programming Basics/03.ConditionalStatementsAdvanced/InvalidNumber/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace InvalidNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
