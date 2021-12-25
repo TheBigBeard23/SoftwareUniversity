233df8dd1765e01973794e5d8a53f2c481e16f7a
TheBigBeard23 <Bones4pk@gmail.com>
2021-12-25 22:19:46 +0200

    Add new folder

--- Programming Basics/03.ConditionalStatementsAdvanced/HotelRoom/Program.cs
+++ Programming Basics/03.ConditionalStatementsAdvanced/HotelRoom/Program.cs
@@ -6,7 +6,91 @@ namespace HotelRoom
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string month = Console.ReadLine();
+            int numberNights = int.Parse(Console.ReadLine());
+
+            double studioPrice = 0;
+            double apartmentPrice = 0;
+            double wholeStudioPrice = 0;
+            double wholeApartmentPrice = 0;
+
+            switch (month)
+            {
+                case "May":
+                case "October":
+                    studioPrice = 50;
+                    apartmentPrice = 65;
+                    if (numberNights > 7 && numberNights < 14)
+                    {
+                        studioPrice = studioPrice - (studioPrice * 0.05);
+                        wholeApartmentPrice = apartmentPrice * numberNights;
+                        wholeStudioPrice = studioPrice * numberNights;
+                        Console.WriteLine($"Apartment: {wholeApartmentPrice:f2} lv.");
+                        Console.WriteLine($"Studio: {wholeStudioPrice:f2} lv.");
+                    }
+                    else if (numberNights > 14)
+                    {
+                        studioPrice = studioPrice - (studioPrice * 0.30);
+                        apartmentPrice = apartmentPrice - (apartmentPrice * 0.10);
+                        wholeApartmentPrice = apartmentPrice * numberNights;
+                        wholeStudioPrice = studioPrice * numberNights;
+                        Console.WriteLine($"Apartment: {wholeApartmentPrice:f2} lv.");
+                        Console.WriteLine($"Studio: {wholeStudioPrice:f2} lv.");
+                    }
+                    else
+                    {
+                        wholeApartmentPrice = apartmentPrice * numberNights;
+                        wholeStudioPrice = studioPrice * numberNights;
+                        Console.WriteLine($"Apartment: {wholeApartmentPrice:f2} lv.");
+                        Console.WriteLine($"Studio: {wholeStudioPrice:f2} lv.");
+                    }
+                    break;
+
+                case "June":
+                case "September":
+                    studioPrice = 75.20;
+                    apartmentPrice = 68.70;
+                    if (numberNights > 14)
+                    {
+                        studioPrice = studioPrice - (studioPrice * 0.20);
+                        apartmentPrice = apartmentPrice - (apartmentPrice * 0.10);
+                        wholeApartmentPrice = apartmentPrice * numberNights;
+                        wholeStudioPrice = studioPrice * numberNights;
+                        Console.WriteLine($"Apartment: {wholeApartmentPrice:f2} lv.");
+                        Console.WriteLine($"Studio: {wholeStudioPrice:f2} lv.");
+                    }
+                    else
+                    {
+                        wholeStudioPrice = studioPrice * numberNights;
+                        wholeApartmentPrice = apartmentPrice * numberNights;
+                        Console.WriteLine($"Apartment: {wholeApartmentPrice:f2} lv.");
+                        Console.WriteLine($"Studio: {wholeStudioPrice:f2} lv.");
+                    }
+                    break;
+                case "July":
+                case "August":
+                    studioPrice = 76;
+                    apartmentPrice = 77;
+                    if (numberNights > 14)
+                    {
+                        apartmentPrice = apartmentPrice - (apartmentPrice * 0.10);
+                        wholeApartmentPrice = apartmentPrice * numberNights;
+                        wholeStudioPrice = studioPrice * numberNights;
+                        Console.WriteLine($"Apartment: {wholeApartmentPrice:f2} lv.");
+                        Console.WriteLine($"Studio: {wholeStudioPrice:f2} lv.");
+                    }
+                    else
+                    {
+                        wholeApartmentPrice = apartmentPrice * numberNights;
+                        wholeStudioPrice = studioPrice * numberNights;
+                        Console.WriteLine($"Apartment: {wholeApartmentPrice:f2} lv.");
+                        Console.WriteLine($"Studio: {wholeStudioPrice:f2} lv.");
+                    }
+                    break;
+                default:
+                    break;
+            }
+            
         }
     }
 }
--- Programming Basics/03.ConditionalStatementsAdvanced/OperationsBetweenNumbers/Program.cs
+++ Programming Basics/03.ConditionalStatementsAdvanced/OperationsBetweenNumbers/Program.cs
@@ -6,15 +6,12 @@ namespace OperationsBetweenNumbers
     {
         static void Main(string[] args)
         {
-            int n1 = int.Parse(Console.ReadLine());
-            int n2 = int.Parse(Console.ReadLine());
+            double n1 = double.Parse(Console.ReadLine());
+            double n2 = double.Parse(Console.ReadLine());
             char operation = char.Parse(Console.ReadLine());
             double result = 0; 
             string oddOrEven = "";
-            if (n2 == 0)
-            {
-                Console.WriteLine($"Cannot divide {n1} by zero");
-            }
+           
             switch (operation)
             {
                 case '+':
@@ -42,17 +39,14 @@ namespace OperationsBetweenNumbers
                     Console.WriteLine($"{n1} - {n2} = {result} - {oddOrEven}");
                     break;
                 case '/':
-                   
+                    if (n2 == 0)
+                    {
+                        Console.WriteLine($"Cannot divide {n1} by zero");
+                        break;
+                    }
                     result = n1 / n2;
-                    if (result % 2 == 0)
-                    {
-                        oddOrEven = "even";
-                    }
-                    else
-                    {
-                        oddOrEven = "odd";
-                    }
-                    Console.WriteLine($"{n1} / {n2} = {result} - {oddOrEven}");
+                    
+                    Console.WriteLine($"{n1} / {n2} = {result}");
                     break;
                 case '*':
                     result = n1 * n2;
@@ -64,19 +58,18 @@ namespace OperationsBetweenNumbers
                     {
                         oddOrEven = "odd";
                     }
+
                     Console.WriteLine($"{n1} * {n2} = {result} - {oddOrEven}");
                     break;
                 case '%':
+                    if (n2 == 0)
+                    {
+                        Console.WriteLine($"Cannot divide {n1} by zero");
+                        break;
+                    }
                     result = n1 % n2;
-                    if (result % 2 == 0)
-                    {
-                        oddOrEven = "even";
-                    }
-                    else
-                    {
-                        oddOrEven = "odd";
-                    }
-                    Console.WriteLine($"{n1} % {n2} = {result} - {oddOrEven}");
+                  
+                    Console.WriteLine($"{n1} % {n2} = {result}");
                     break;
 
                 default:
