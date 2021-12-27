7bedda502d4e26181323f889a6c0914864fe90a5
TheBigBeard23 <Bones4pk@gmail.com>
2021-12-27 16:46:25 +0200

    Add new exercise

---
+++ Programming Basics/04.ForLoops/NumberSequence/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NumberSequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+            List<int> numbers = new List<int>();
+
+            for (int i = 0; i < count; i++)
+            {
+                numbers.Add(int.Parse(Console.ReadLine()));
+            }
+            var maxNum = numbers.OrderByDescending(n => n).FirstOrDefault();
+
+            Console.WriteLine($"Max number: {maxNum}");
+        }
+    }
+}
