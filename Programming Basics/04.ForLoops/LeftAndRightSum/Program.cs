742ecd3108412114b859b11385fd02eac5f30523
Vladislav Dimitrov <Bones4pk@gmail.com>
2021-12-29 13:58:13 +0200

    Add new exercise

--- Programming Basics/04.ForLoops/LeftAndRightSum/Program.cs
+++ Programming Basics/04.ForLoops/LeftAndRightSum/Program.cs
@@ -6,7 +6,27 @@ namespace LeftAndRightSum
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int numbers = int.Parse(Console.ReadLine());
+            int firstSum = 0;
+            int secondSum = 0;
+
+            for (int i = 0; i < numbers; i++)
+            {
+                firstSum += int.Parse(Console.ReadLine());
+            }
+            for (int i = 0; i < numbers; i++)
+            {
+                secondSum += int.Parse(Console.ReadLine());
+            }
+
+            if (firstSum == secondSum)
+            {
+                Console.WriteLine($"Yes, sum = {firstSum}");
+            }
+            else
+            {
+                Console.WriteLine($"No, diff = {Math.Abs( secondSum-firstSum)}");
+            }
         }
     }
 }
