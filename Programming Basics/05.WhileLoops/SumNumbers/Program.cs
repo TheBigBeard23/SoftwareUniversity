545c6dc470b587def94c7bf26fe11d4120760b8c
TheBigBeard23 <Bones4pk@gmail.com>
2021-12-30 19:48:33 +0200

    Add new exercise

---
+++ Programming Basics/05.WhileLoops/SumNumbers/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SumNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
