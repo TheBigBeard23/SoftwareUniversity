e6111a969db2686048486d67b3e2bd6a0d924dfb
TheBigBeard23 <Bones4pk@gmail.com>
2021-12-30 20:16:52 +0200

    Add new exercise

---
+++ Programming Basics/05.WhileLoops/MaxNumber/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MaxNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
