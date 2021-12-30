6409552400ce25bec05ad5893ecfd1c14acb28b7
TheBigBeard23 <Bones4pk@gmail.com>
2021-12-30 20:20:19 +0200

    Add new exercise

---
+++ Programming Basics/05.WhileLoops/MinNumber/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MinNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
