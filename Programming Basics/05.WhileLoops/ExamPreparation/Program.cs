ac9b029eb3c90c951765a3a5a86957b859ce53b6
TheBigBeard23 <Bones4pk@gmail.com>
2022-01-02 16:49:03 +0200

    Add new exercise

---
+++ Programming Basics/05.WhileLoops/ExamPreparation/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace ExamPreparation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int poorGradesCount = int.Parse(Console.ReadLine());
+
+            string lastProblem = "";
+            double score = 0.0;
+            int problemsCount = 0;
+            int currentPoorGrades = 0;
+
+            while (poorGradesCount>currentPoorGrades)
+            {
+                
+                string problem = Console.ReadLine();
+                if (problem == "Enough")
+                {
+                    break;
+                }
+                int grade = int.Parse(Console.ReadLine());
+
+                
+
+                lastProblem = problem;
+
+                if (grade <= 4)
+                {
+                    currentPoorGrades++;
+                }
+                else
+                {
+                    problemsCount++;
+                    score += grade;
+                }
+            }
+
+            if (poorGradesCount == currentPoorGrades)
+            {
+                Console.WriteLine($"You need a break, {poorGradesCount} poor grades.");
+            }
+            else
+            {
+                Console.WriteLine($"Average score: {score/problemsCount:f2}");
+                Console.WriteLine($"Number of problems: {problemsCount}");
+                Console.WriteLine($"Last problem: {lastProblem}");
+            }
+        }
+    }
+}
