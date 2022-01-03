e8717dad2ea9680649581ca882b725b61793f62f
Vladislav Dimitrov <Bones4pk@gmail.com>
2022-01-03 16:30:49 +0200

    Add new exercise

--- Programming Basics/05.WhileLoops/Cake/Program.cs
+++ Programming Basics/05.WhileLoops/Cake/Program.cs
@@ -6,7 +6,39 @@ namespace Cake
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            int cakeWidth = int.Parse(Console.ReadLine());
+            int cakeHeight = int.Parse(Console.ReadLine());
+            int cakePieces = cakeHeight * cakeWidth;
+
+            string input = "";
+            int takenPieces = 0;
+            while ((input = Console.ReadLine())!="STOP")
+            {
+                takenPieces = int.Parse(input);
+
+                if (cakePieces > takenPieces)
+                {
+                    cakePieces -= takenPieces;
+                }
+                else
+                {
+                    break;
+                }
+
+
+            }
+            if (input == "STOP")
+            {
+                Console.WriteLine($"{cakePieces} pieces are left.");
+            }
+            else
+            {
+                Console.WriteLine($"No more cake left! You need {takenPieces-cakePieces} pieces more.");
+            }
+
+
+
         }
     }
 }
--- Programming Basics/05.WhileLoops/Moving/Program.cs
+++ Programming Basics/05.WhileLoops/Moving/Program.cs
@@ -6,7 +6,38 @@ namespace Moving
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int width = int.Parse(Console.ReadLine());
+            int length = int.Parse(Console.ReadLine());
+            int height = int.Parse(Console.ReadLine());
+
+            int placeVolume = width * length * height;
+            string input = "";
+            int currentBoxVolume = 0;
+
+            while ((input=Console.ReadLine())!="Done")
+            {
+                currentBoxVolume = int.Parse(input);
+
+                if (placeVolume > currentBoxVolume)
+                {
+                    placeVolume -= currentBoxVolume;
+                }
+                else
+                {
+                    break;
+                }
+
+
+            }
+
+            if (input == "Done")
+            {
+                Console.WriteLine($"{placeVolume} Cubic meters left.");
+            }
+            else
+            {
+                Console.WriteLine($"No more free space! You need {currentBoxVolume-placeVolume} Cubic meters more.");
+            }
         }
     }
 }
