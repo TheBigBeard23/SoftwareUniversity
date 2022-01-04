130fd74fc3dadb62d579c9243521fe3d0d63cb06
Vladislav Dimitrov <Bones4pk@gmail.com>
2022-01-04 17:46:48 +0200

    Add new exercise

---
+++ Programming Basics/06.NestedLoops/Travelling/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Travelling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string destination = string.Empty;
+            int price = 0;
+            int savings = 0;
+            string command = string.Empty;
+
+            while (true)
+            {
+                destination = Console.ReadLine();
+
+                if (destination == "End")
+                {
+                    break;
+                }
+
+                price = int.Parse(Console.ReadLine());
+
+                while (price>savings)
+                {
+                    command = Console.ReadLine();
+
+                    if (command == "End")
+                    {
+                        return;
+                    }
+
+                    savings += int.Parse(command);
+
+                }
+                
+                Console.WriteLine($"Going to {destination}!");
+
+                savings = 0;
+            }
+
+
+
+        }
+    }
+}
