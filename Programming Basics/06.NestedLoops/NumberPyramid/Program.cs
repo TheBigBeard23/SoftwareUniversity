db129bcdd0eb8bfd325bb3e2ac2131336aeb8fba
Vladislav Dimitrov <Bones4pk@gmail.com>
2022-01-05 12:00:32 +0200

    Add new exercise

---
+++ Programming Basics/06.NestedLoops/NumberPyramid/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace NumberPyramid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int currentNum = 1;
+            bool isBigger = false;
+
+            for (int row = 1; row <= n; row++)
+            {
+                for (int col = 1; col <= row; col++)
+                {
+                    if (currentNum > n)
+                    {
+                        isBigger = true;
+                        break;
+                    }
+                    Console.Write(currentNum+" ");
+                    currentNum++;
+                }
+                if (isBigger)
+                {
+                    break;
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
