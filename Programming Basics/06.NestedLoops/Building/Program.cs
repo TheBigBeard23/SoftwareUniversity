5bcfc60c7d6990b6819e42ce72308ea917e88fc0
Vladislav Dimitrov <Bones4pk@gmail.com>
2022-01-05 11:50:29 +0200

    Add new exercise

---
+++ Programming Basics/06.NestedLoops/Building/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Building
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int floors = int.Parse(Console.ReadLine());
+            int roomsOnFloor = int.Parse(Console.ReadLine());
+            string typeOfRooms = "";
+            for (int i = floors; i >= 1; i--)
+            {
+                if (i == floors)
+                {
+                    typeOfRooms = "L";
+                }
+                else if (i % 2 == 0)
+                {
+
+                    typeOfRooms = "O";
+                }
+                else
+                {
+                    typeOfRooms = "A";
+                }
+
+                for (int k = 0; k < roomsOnFloor; k++)
+                {
+                    Console.Write($"{typeOfRooms}{i}{k} ");
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
