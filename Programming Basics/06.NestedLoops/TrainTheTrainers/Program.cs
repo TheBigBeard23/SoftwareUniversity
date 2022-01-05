030b7cbd91640cb32f2d35a6730a1e1e9136691f
Vladislav Dimitrov <Bones4pk@gmail.com>
2022-01-05 20:55:27 +0200

    Add new exercise

---
+++ Programming Basics/06.NestedLoops/TrainTheTrainers/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace TrainTheTrainers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int gradesCount = int.Parse(Console.ReadLine());
+            string input = string.Empty;
+
+            double avgGrade = 0.0;
+            int presentationsCount = 0;
+
+            while ((input = Console.ReadLine())!="Finish")
+            {
+                presentationsCount++;
+                double currentAvgGrade = 0.0;
+
+                for (int i = 0; i < gradesCount; i++)
+                {
+                    currentAvgGrade += double.Parse(Console.ReadLine());
+                }
+
+                currentAvgGrade = currentAvgGrade / gradesCount;
+                avgGrade += currentAvgGrade;
+                Console.WriteLine($"{input} - {currentAvgGrade}.");
+            }
+
+            avgGrade = avgGrade / presentationsCount;
+
+            Console.WriteLine($"Student's final assessment is {avgGrade}.");
+
+
+
+        }
+    }
+}
