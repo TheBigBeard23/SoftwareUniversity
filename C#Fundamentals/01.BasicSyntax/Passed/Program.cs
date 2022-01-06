30ddcd23a7d467d3fea2df226f6b7d5bc98ce77f
Vladislav Dimitrov <Bones4pk@gmail.com>
2022-01-06 11:10:43 +0200

    Add new exercise

---
+++ C#Fundamentals/01.BasicSyntax/Passed/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Passed
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
