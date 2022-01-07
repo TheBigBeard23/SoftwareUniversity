a310effe836510e90e157fba71ad2b959903e52d
TheBigBeard23 <Bones4pk@gmail.com>
2022-01-07 21:23:45 +0200

    Add new exercise

---
+++ C#Fundamentals/01.BasicSyntax/BackIn30Minutes/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BackIn30Minutes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
