7bf255dcd865281e5cd1ca93e7e1020e89f35dd2
TheBigBeard23 <Bones4pk@gmail.com>
2022-01-08 12:22:21 +0200

    Add new exercise

---
+++ C#Fundamentals/01.BasicSyntax/MultiplicationTable2.0/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MultiplicationTable2._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNum = int.Parse(Console.ReadLine());
+            int secondNum = int.Parse(Console.ReadLine());
+
+            if (firstNum > 10 || secondNum > 10)
+            {
+                Console.WriteLine(firstNum + " X " + secondNum + " = " + (firstNum * secondNum));
+            }
+            else if (secondNum < 10 || 10 > firstNum)
+            {
+                for (int i = secondNum; i <= 10; i++)
+                {
+
+                    Console.WriteLine(firstNum + " X " + i + " = " + (firstNum * i));
+                }
+            }
+        }
+    }
+}
