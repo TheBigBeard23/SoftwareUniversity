2d241e562a8de44cd94962cc39127ca3b06ddbf1
TheBigBeard23 <Bones4pk@gmail.com>
2022-01-09 11:13:37 +0200

    Add new exercise

---
+++ C#Fundamentals/01.BasicSyntax/PrintAndSum/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace PrintAndSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNum = int.Parse(Console.ReadLine());
+            int secondNum = int.Parse(Console.ReadLine());
+            int totalSum = 0;
+
+            for (int i = firstNum; i <= secondNum; i++)
+            {
+                Console.Write(i + " ");
+                totalSum += i;
+
+            }
+            Console.WriteLine("");
+            Console.WriteLine("Sum: " + totalSum);
+        }
+    }
+}
---
+++ C#Fundamentals/01.BasicSyntax/Vacation/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace Vacation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double groupMember = double.Parse(Console.ReadLine());
+            string typeOfGroup = Console.ReadLine();
+            string dayOfWeek = Console.ReadLine();
+            double sum = 0;
+
+            switch (dayOfWeek)
+            {
+                case ("Friday"):
+
+                    if (typeOfGroup == "Students")
+                    {
+                        sum = groupMember * 8.45;
+                    }
+                    else if (typeOfGroup == "Business")
+                    {
+                        sum = groupMember * 10.90;
+                    }
+                    else if (typeOfGroup == "Regular")
+                    {
+                        sum = groupMember * 15;
+                    }
+
+
+                    break;
+
+                case ("Saturday"):
+                    if (typeOfGroup == "Students")
+                    {
+                        sum = groupMember * 9.80;
+                    }
+                    else if (typeOfGroup == "Business")
+                    {
+                        sum = groupMember * 15.60;
+                    }
+                    else if (typeOfGroup == "Regular")
+                    {
+                        sum = groupMember * 20;
+                    }
+
+                    break;
+
+                case ("Sunday"):
+                    if (typeOfGroup == "Students")
+                    {
+                        sum = groupMember * 10.46;
+                    }
+                    else if (typeOfGroup == "Business")
+                    {
+                        sum = groupMember * 16;
+                    }
+                    else if (typeOfGroup == "Regular")
+                    {
+                        sum = groupMember * 22.50;
+                    }
+
+                    break;
+
+
+            }
+            if (typeOfGroup == "Students" && groupMember >= 30)
+            {
+                sum = sum - (sum * 0.15);
+            }
+            else if (typeOfGroup == "Business" && groupMember >= 100)
+            {
+                sum = sum - ((sum / groupMember) * 10);
+            }
+            else if (typeOfGroup == "Regular" && groupMember >= 10 && groupMember <= 20)
+            {
+                sum = sum - (sum * 0.05);
+            }
+
+            Console.WriteLine("Total price: " + "{0:F2}", sum);
+        }
+    }
+}
