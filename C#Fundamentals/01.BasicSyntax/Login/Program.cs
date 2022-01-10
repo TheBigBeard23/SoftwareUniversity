db2edea8b65661ef17e64382de018b9e578e09a0
Vladislav Dimitrov <Bones4pk@gmail.com>
2022-01-10 17:13:34 +0200

    Add new exercise

---
+++ C#Fundamentals/01.BasicSyntax/Login/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Login
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string username = Console.ReadLine();
+
+            char[] charArray = username.ToCharArray();
+            Array.Reverse(charArray);
+            string password = String.Join("",charArray);
+
+            string input = Console.ReadLine();
+            int counter = 0;
+
+            while (input!=password)
+            {
+                counter++;
+                if (counter == 4)
+                {
+                    Console.WriteLine("User sunny blocked!");
+                    return;
+                }
+
+                Console.WriteLine("Incorrect password. Try again.");
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine($"User {username} logged in.");
+        }
+    }
+}
