1cdcdd0995bb365d2d4e62cd446dd9ff1fb8a7af
Vladislav Dimitrov <Bones4pk@gmail.com>
2022-01-11 15:31:27 +0200

    Add new exercise

---
+++ C#Fundamentals/01.BasicSyntax/EnglishNameOfTheLastDigit/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace EnglishNameOfTheLastDigit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] numbers = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+            int num = int.Parse(Console.ReadLine());
+            int lastNum = num % 10;
+            Console.WriteLine(numbers[lastNum]);
+        }
+    }
+}
---
+++ C#Fundamentals/01.BasicSyntax/Sort Numbers/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace SortNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+
+            int[] numbers = { a, b, c };
+            int maxNumber = numbers.Max();
+            int minNumber = numbers.Min();
+            int avgNumber = (a + b + c) - (maxNumber + minNumber);
+
+            Console.WriteLine(maxNumber);
+            Console.WriteLine(avgNumber);
+            Console.WriteLine(minNumber);
+        }
+    }
+}
