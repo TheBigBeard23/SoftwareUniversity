fdbb4a0cb5d4159d13b1545572474bf0ff418864
Vladislav Dimitrov <Bones4pk@gmail.com>
2022-01-11 15:27:00 +0200

    Add new exercise

---
+++ C#Fundamentals/01.BasicSyntax/Orders/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Orders
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ordersCount = int.Parse(Console.ReadLine());
+            double totalPrice = 0;
+
+            for (int i = 0; i < ordersCount; i++)
+            {
+                double pricePerCapsule = double.Parse(Console.ReadLine());
+                int daysInMonth = int.Parse(Console.ReadLine());
+                int capsulesCount = int.Parse(Console.ReadLine());
+
+                double currentPrice = ((daysInMonth * capsulesCount) * pricePerCapsule);
+                totalPrice += currentPrice;
+                Console.WriteLine($"The price for the coffee is: ${currentPrice:f2}");
+            }
+
+            Console.WriteLine($"Total: ${totalPrice:f2}");
+        }
+    }
+}
