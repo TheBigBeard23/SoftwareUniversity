5e5c51d97167cb6a40fca9472a103dbd46bae66f
Vladislav Dimitrov <Bones4pk@gmail.com>
2022-01-11 17:22:47 +0200

    Add new exercise

---
+++ C#Fundamentals/01.BasicSyntax/Messages/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Messages
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+            string message = string.Empty;
+
+            for (int i = 0; i < count; i++)
+            {
+                string code = Console.ReadLine();
+                int digitLength = code.Length;
+                int digit = code[0] - '0';
+                int offset = (digit - 2) * 3;
+
+
+                if (digit == 0)
+                {
+                    message += (char)(digit + 32);
+                    continue;
+                }
+
+                if(digit==8 || digit == 9)
+                {
+                    offset++;
+                }
+                int letterIndex = offset + digitLength - 1;
+                message += (char)(letterIndex + 97);
+            }
+
+            Console.WriteLine(message);
+        }
+    }
+}
