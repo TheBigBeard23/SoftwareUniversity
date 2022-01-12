6e387d552ef6f2ace0877b05b909dddecb554d3b
Vladislav Dimitrov <Bones4pk@gmail.com>
2022-01-12 12:15:34 +0200

    Add new exercise

---
+++ C#Fundamentals/02.DataTypesAndVariables/CenturiesToMinutes/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CenturiesToMinutes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int centuries = int.Parse(Console.ReadLine());
+            int year = centuries * 100;
+            int days = (int)(year * 365.2422);
+            int hours = days * 24;
+            int minutes = hours * 60;
+            Console.WriteLine($"{centuries} centuries = {year} years = {days} days = {hours} hours = {minutes} minutes");
+        }
+    }
+}
---
+++ C#Fundamentals/02.DataTypesAndVariables/ExactSumOfRealNumbers/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ExactSumOfRealNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            decimal sum = 0;
+            for (int i = 1; i <= num; i++)
+            {
+                decimal number = decimal.Parse(Console.ReadLine());
+                sum += number;
+            }
+            Console.WriteLine($"{sum}");
+        }
+    }
+}
