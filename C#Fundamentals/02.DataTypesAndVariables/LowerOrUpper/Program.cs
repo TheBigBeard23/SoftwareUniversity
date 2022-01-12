3818f9d12bfc9d17947d8417c1c5fe283f57b733
Vladislav Dimitrov <Bones4pk@gmail.com>
2022-01-12 12:41:03 +0200

    Add new exercise

---
+++ C#Fundamentals/02.DataTypesAndVariables/LowerOrUpper/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace LowerOrUpper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            if (input.ToUpper() == input)
+            {
+                Console.WriteLine("upper-case");
+            }
+            else
+            {
+                Console.WriteLine("lower-case");
+            }
+        }
+    }
+}
