205d55e10cb5d8b28fd3b9ebf882a5767cc4b933
Vladislav Dimitrov <Bones4pk@gmail.com>
2022-01-12 12:24:07 +0200

    Add new exercise

---
+++ C#Fundamentals/02.DataTypesAndVariables/ReversedChars/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ReversedChars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] symbols = new char[3];
+
+            for (int i = 1; i <= 3; i++)
+            {
+                symbols[3 - i] = char.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine(String.Join(" ",symbols));
+        }
+    }
+}
