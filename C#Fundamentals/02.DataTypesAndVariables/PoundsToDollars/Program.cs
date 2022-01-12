6ebcd0aea91f78b033b48a02d6f2787b9c1bcdc4
Vladislav Dimitrov <Bones4pk@gmail.com>
2022-01-12 12:10:03 +0200

    Add new exercise

---
+++ C#Fundamentals/02.DataTypesAndVariables/PoundsToDollars/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace PoundsToDollars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal britishPound = decimal.Parse(Console.ReadLine());
+            decimal dollars = britishPound * 1.31M;
+            Console.WriteLine($"{dollars:f3}");
+        }
+    }
+}
