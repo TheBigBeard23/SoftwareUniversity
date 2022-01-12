74881639adebbc870a40a39f5c10c3c38bc35e7a
Vladislav Dimitrov <Bones4pk@gmail.com>
2022-01-12 12:33:18 +0200

    Add new exercise

---
+++ C#Fundamentals/02.DataTypesAndVariables/TownInfo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TownInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string tower = Console.ReadLine();
+            string population = Console.ReadLine();
+            string area = Console.ReadLine();
+            Console.WriteLine($"Town {tower} has population of {population} and area {area} square km.");
+        }
+    }
+}
