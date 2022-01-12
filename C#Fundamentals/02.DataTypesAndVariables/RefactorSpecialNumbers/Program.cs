835b19073cf921878a7e893f656f5fef268990cf
Vladislav Dimitrov <Bones4pk@gmail.com>
2022-01-12 13:03:10 +0200

    Add new exercise

---
+++ C#Fundamentals/02.DataTypesAndVariables/RefactorSpecialNumbers/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace RefactorSpecialNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberCount = int.Parse(Console.ReadLine());
+            int digitSum = 0;
+
+            bool isSpecialNum;
+            for (int i = 1; i <= numberCount; i++)
+            {
+                int currentNumber = i;
+                while (i > 0)
+                {
+                    digitSum += i % 10;
+                    i = i / 10;
+                }
+                isSpecialNum = (digitSum == 5) || (digitSum == 7) || (digitSum == 11);
+                Console.WriteLine("{0} -> {1} ", currentNumber, isSpecialNum);
+                digitSum = 0;
+                i = currentNumber;
+
+            }
+        }
+    }
+}
