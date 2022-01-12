62d0d5124f38c4a1c80f63c401defaa3b22be887
Vladislav Dimitrov <Bones4pk@gmail.com>
2022-01-12 12:28:58 +0200

    Add new exercise

---
+++ C#Fundamentals/02.DataTypesAndVariables/ConcatNames/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConcatNames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = new string[3];
+
+            for (int i = 0; i < 3; i++)
+            {
+                words[i] = Console.ReadLine();
+            }
+
+            Console.WriteLine($"{words[0]}{words[2]}{words[1]}");
+        }
+    }
+}
