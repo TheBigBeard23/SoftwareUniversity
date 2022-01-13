11719adaf85406f8c211548448b31d1cfd3c36e3
Vladislav Dimitrov <Bones4pk@gmail.com>
2022-01-13 20:24:07 +0200

    Add new exercise

---
+++ C#Fundamentals/02.DataTypesAndVariables/BeerKegs/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BeerKegs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int count = int.Parse(Console.ReadLine());
+            Dictionary<string, double> kegs = new Dictionary<string, double>();
+
+            for (int i = 0; i < count; i++)
+            {
+                string model = Console.ReadLine();
+                double radious = double.Parse(Console.ReadLine());
+                int height = int.Parse(Console.ReadLine());
+
+                double volume = Math.PI * Math.Pow(radious, 2) * height;
+
+                kegs[model] = volume;
+            }
+
+            var biggestKeg = kegs.OrderByDescending(x => x.Value).FirstOrDefault();
+
+            Console.WriteLine(biggestKeg.Key);
+        }
+    }
+}
