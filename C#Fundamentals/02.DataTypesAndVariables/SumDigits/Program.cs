14cadaf35b623948bce84765bfe6c41bcfc24f86
Vladislav Dimitrov <Bones4pk@gmail.com>
2022-01-13 13:21:42 +0200

    Add new exercise

---
+++ C#Fundamentals/02.DataTypesAndVariables/SumDigits/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SumDigits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int sum = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                sum += (int)input[i] - '0';
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
