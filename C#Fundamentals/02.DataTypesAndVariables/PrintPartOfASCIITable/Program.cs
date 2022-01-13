8f03d4f96c4fec99eedad0431fd407c1292f43e5
Vladislav Dimitrov <Bones4pk@gmail.com>
2022-01-13 13:39:24 +0200

    Add new exercise

---
+++ C#Fundamentals/02.DataTypesAndVariables/PrintPartOfASCIITable/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace PrintPartOfASCIITable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNum = int.Parse(Console.ReadLine());
+            int secondNum = int.Parse(Console.ReadLine());
+
+            for (int i = firstNum; i <= secondNum; i++)
+            {
+                Console.WriteLine((char)i);
+            }
+        }
+    }
+}
