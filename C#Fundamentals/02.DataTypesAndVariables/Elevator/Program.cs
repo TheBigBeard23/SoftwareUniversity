1d663f5e14524ba1b7947e87d2a9ada7e93ce81c
Vladislav Dimitrov <Bones4pk@gmail.com>
2022-01-13 13:30:49 +0200

    Add new exercise

---
+++ C#Fundamentals/02.DataTypesAndVariables/Elevator/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Elevator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int peopleCount = int.Parse(Console.ReadLine());
+            int capacity = int.Parse(Console.ReadLine());
+            int counter = 0;
+
+            while (peopleCount>0)
+            {
+                counter++;
+                peopleCount -= capacity;
+            }
+
+            Console.WriteLine(counter);
+        }
+    }
+}
