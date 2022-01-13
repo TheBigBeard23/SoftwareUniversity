690d5f0fa470f045e097d61fce66e8150e3ba5c0
Vladislav Dimitrov <Bones4pk@gmail.com>
2022-01-13 19:41:42 +0200

    Add new exercise

---
+++ C#Fundamentals/02.DataTypesAndVariables/WaterOverflow/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace WaterOverflow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+            int tankQuanity = 0;
+
+            for (int i = 0; i < count; i++)
+            {
+                int quanity = int.Parse(Console.ReadLine());
+
+                if (tankQuanity + quanity <= 255)
+                {
+                    tankQuanity += quanity;
+                }
+                else
+                {
+                    Console.WriteLine("Insufficient capacity!");
+                }
+            }
+
+            Console.WriteLine(tankQuanity);
+        }
+    }
+}
