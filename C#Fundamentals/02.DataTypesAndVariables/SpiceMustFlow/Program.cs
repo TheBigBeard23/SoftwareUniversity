580d3c242dc01766bbe41b4cc347af8af0976a5e
Vladislav Dimitrov <Bones4pk@gmail.com>
2022-01-14 13:49:22 +0200

    Add new exercise

---
+++ C#Fundamentals/02.DataTypesAndVariables/SpiceMustFlow/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace SpiceMustFlow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int startingYield = int.Parse(Console.ReadLine());
+            int daysCount = 0;
+            int yieldQuantity = 0;
+
+            while (startingYield>=100)
+            {
+                daysCount++;
+                yieldQuantity += startingYield - 26;
+                startingYield -= 10;
+            }
+            yieldQuantity -= 26;
+
+            Console.WriteLine($"{daysCount}");
+            Console.WriteLine($"{yieldQuantity}");
+        }
+    }
+}
