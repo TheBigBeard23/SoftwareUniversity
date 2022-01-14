4a4ea0a10b8bafc0ffbf1e657b3d8c5d5459462f
Vladislav Dimitrov <Bones4pk@gmail.com>
2022-01-15 00:17:37 +0200

    Add new exercise

---
+++ C#Fundamentals/02.DataTypesAndVariables/Pokemon/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Pokemon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int pokemonPower = int.Parse(Console.ReadLine());
+            int distance = int.Parse(Console.ReadLine());
+            int exhaustionFactor = int.Parse(Console.ReadLine());
+
+            int halfPower = pokemonPower / 2;
+            int targetCount = 0;
+            
+            while (pokemonPower>=distance)
+            {
+                pokemonPower -= distance;
+                targetCount++;
+
+                if (pokemonPower == halfPower && exhaustionFactor!=0)
+                {
+                    pokemonPower /= exhaustionFactor;
+                }
+                
+            }
+
+            Console.WriteLine($"{pokemonPower}");
+            Console.WriteLine($"{targetCount}");
+        }
+    }
+}
