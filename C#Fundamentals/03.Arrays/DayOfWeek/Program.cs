01c9efb98f5f11ecb5c1ddd21bc03d29224d6aec
Vladislav Dimitrov <Bones4pk@gmail.com>
2022-01-17 00:32:46 +0200

    Add new exercise

---
+++ C#Fundamentals/03.Arrays/DayOfWeek/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace DayOfWeek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int dayOfWeek = int.Parse(Console.ReadLine());
+            string[] days = new string[]
+            {
+                "Monday",
+                "Tuesday",
+                "Wednesday",
+                "Thursday",
+                "Friday",
+                "Saturday",
+                "Sunday"
+            };
+
+            if (dayOfWeek < 1 || dayOfWeek > days.Length)
+            {
+                Console.WriteLine("Invalid day!");
+            }
+            else
+            {
+                Console.WriteLine(days[dayOfWeek - 1]);
+            }
+        }
+    }
+}
---
+++ C#Fundamentals/03.Arrays/PrintNumbersInReverseOrder/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace PrintNumbersInReverseOrder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] nums = new int[n];
+
+            for (int i = n-1; i >= 0; i--)
+            {
+                nums[i] = int.Parse(Console.ReadLine());
+
+            }
+
+            Console.WriteLine(String.Join(" ",nums));
+        }
+    }
+}
