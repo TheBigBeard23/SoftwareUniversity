fad8b6f45e9a77c3a282bd0556cd9044375291b6
Vladislav Dimitrov <Bones4pk@gmail.com>
2022-01-16 20:51:58 +0200

    Add new exercise

---
+++ C#Fundamentals/02.DataTypesAndVariables/PrimeChecker/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace PrimeChecker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            for (int i = 2; i <= number; i++)
+            {
+                bool isPrime = true;
+                for (int k = 2; k < i; k++)
+                {
+                    if (i % k == 0)
+                    {
+                        takovalie = false;
+                        break;
+                    }
+                }
+                Console.WriteLine("{0} -> {1}", i, takovalie);
+            }
+
+        }
+    }
+}
