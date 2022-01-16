cf04bcd60b2fcb8efb8e4ca664dd6728abc9dced
Vladislav Dimitrov <Bones4pk@gmail.com>
2022-01-16 23:23:53 +0200

    Add new exercise

---
+++ C#Fundamentals/02.DataTypesAndVariables/BalancedBrackets/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace BalancedBrackets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lineCount = int.Parse(Console.ReadLine());
+            int brackets = 0;
+            
+
+            for (int i = 0; i < lineCount; i++)
+            {
+                string input = Console.ReadLine();
+
+                if (input == "(")
+                {
+                    brackets++;
+                }
+                else if (input == ")")
+                {
+                    brackets--;
+                }
+                if (brackets < 0)
+                {
+                    break;
+                }
+            }
+
+            if (brackets==0)
+            {
+                Console.WriteLine("BALANCED");
+            }
+            else
+            {
+                Console.WriteLine("UNBALANCED");
+            }
+        }
+    }
+}
