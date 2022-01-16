47fda49b2fa07869c77c8853b1e737738d0dde93
Vladislav Dimitrov <Bones4pk@gmail.com>
2022-01-16 19:58:11 +0200

    Add new exercise

---
+++ C#Fundamentals/02.DataTypesAndVariables/FromLeftToTheRight/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Numerics;
+
+namespace FromLeftToTheRight
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < count; i++)
+            {
+              
+                BigInteger biggestNumber = Console.ReadLine()
+                             .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                             .Select(x => BigInteger.Parse(x))
+                             .OrderByDescending(x => x)
+                             .FirstOrDefault();
+
+                int digitsSum = 0;
+                string numberAsString = biggestNumber.ToString();
+
+                for (int k = 0; k < numberAsString.Length; k++)
+                {
+                    digitsSum += int.Parse(numberAsString[k].ToString());
+                }
+
+                Console.WriteLine(Math.Abs(digitsSum));
+            }
+        }
+    }
+}
