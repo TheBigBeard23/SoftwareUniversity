204f6bc89b55e22bf87d451d68c7eb4e23e2f2b7
Vladislav Dimitrov <Bones4pk@gmail.com>
2022-01-18 16:30:43 +0200

    Add new exercise

---
+++ C#Fundamentals/03.Arrays/CondenseArrayToNumber/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace CondenseArrayToNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int numsLenght = nums.Length;
+
+            while (numsLenght > 1)
+            {
+                int[] condensed = new int[nums.Length - 1];
+
+                for (int i = 0; i < numsLenght - 1; i++)
+                {
+                    condensed[i] = nums[i] + nums[i + 1];
+                }
+
+                nums = new int[numsLenght - 1];
+
+                for (int i = 0; i < nums.Length; i++)
+                {
+                    nums[i] = condensed[i];
+                }
+                numsLenght--;
+            }
+
+            Console.WriteLine(nums[0]);
+        }
+    }
+}
