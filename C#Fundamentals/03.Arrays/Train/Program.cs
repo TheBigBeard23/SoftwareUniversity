462fe9180031eac8ef7641f6cec6f94b8537a5f4
Vladislav Dimitrov <Bones4pk@gmail.com>
2022-01-18 16:45:31 +0200

    Add new exercise

---
+++ C#Fundamentals/03.Arrays/Train/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Train
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int wagonCount = int.Parse(Console.ReadLine());
+            int[] wagonsPassengers = new int[wagonCount];
+            int totalPassengers = 0;
+
+            for (int i = 0; i < wagonCount; i++)
+            {
+                int currentPassengers = int.Parse(Console.ReadLine());
+                wagonsPassengers[i] = currentPassengers;
+                totalPassengers += currentPassengers;
+            }
+
+            foreach (var passenger in wagonsPassengers)
+            {
+                Console.Write($"{passenger} ");
+            }
+            Console.WriteLine();
+            Console.WriteLine(totalPassengers);
+        }
+    }
+}
