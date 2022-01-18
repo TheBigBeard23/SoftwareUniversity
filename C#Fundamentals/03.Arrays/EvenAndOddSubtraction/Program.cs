0b123c70e61c2c8c402bfbfb3ffe3cad13690cc2
Vladislav Dimitrov <Bones4pk@gmail.com>
2022-01-18 15:56:26 +0200

    Add new exercise

---
+++ C#Fundamentals/03.Arrays/EvenAndOddSubtraction/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace EvenAndOddSubtraction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int sumEvenNumbers = 0;
+            int sumOddNumbers = 0;
+            int result = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] % 2 == 0)
+                {
+                    sumEvenNumbers += numbers[i];
+                }
+                else
+                {
+                    sumOddNumbers += numbers[i];
+                }
+                result = sumEvenNumbers - sumOddNumbers;
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
