e1ab6858e51a42709a98c75ebac7168db4d3e60e
Vladislav Dimitrov <Bones4pk@gmail.com>
2022-01-19 23:37:12 +0200

    Add new exercise

---
+++ C#Fundamentals/03.Arrays/TopIntegers/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace TopIntegers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+      
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                bool isBigger = true;
+
+                for (int k = i+1; k < array.Length; k++)
+                {
+                    if (array[i] <= array[k])
+                    {
+                        isBigger = false;
+                        break;
+                    }
+                }
+                if (isBigger)
+                {
+                    Console.Write($"{array[i]} ");
+                }
+            }
+        }
+    }
+}
