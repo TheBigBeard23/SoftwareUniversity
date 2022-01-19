95404201295ae3aabd0bd1c1fef284e97c4a1f48
Vladislav Dimitrov <Bones4pk@gmail.com>
2022-01-19 23:24:15 +0200

    Add new exercise

---
+++ C#Fundamentals/03.Arrays/ArrayRotation/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace ArrayRotation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] array = Console.ReadLine()
+                .Split()
+                .ToArray();
+
+            int rotationIndex = int.Parse(Console.ReadLine());
+            int arrayLength = array.Length;
+
+            if (rotationIndex > arrayLength)
+            {
+                rotationIndex = rotationIndex - arrayLength;
+            }
+            
+            for (int i = 0; i < rotationIndex; i++)
+            {
+                string currentElement = array[0];
+
+                for (int k = 0; k < array.Length-1; k++)
+                {
+                    array[k] = array[k+1];
+                }
+
+                array[arrayLength - 1] = currentElement;
+            }
+
+            Console.WriteLine(string.Join(" ",array));
+        }
+    }
+}
