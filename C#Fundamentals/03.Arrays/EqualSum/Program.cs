1d047a0f7e25fc42ce569fefa8ddc3aada919c7b
Vladislav Dimitrov <Bones4pk@gmail.com>
2022-01-20 12:32:14 +0200

    Add new exercise

---
+++ C#Fundamentals/03.Arrays/EqualSum/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+
+namespace EqualSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+
+            bool isEqual = false;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int leftsum = 0;
+                int rightSum = 0;
+
+                for (int k = i + 1; k < numbers.Length; k++)
+                {
+                    rightSum += numbers[k];
+                }
+                for (int j = i - 1; j >= 0; j--)
+                {
+                    leftsum += numbers[j];
+                }
+
+                if (leftsum == rightSum)
+                {
+                    isEqual = true;
+                    Console.WriteLine(i);
+                    return;
+                }
+
+            }
+            if(!isEqual)
+            {
+                Console.WriteLine("no");
+            }
+
+        }
+    }
+}
