beae41ad7873ef8a54f56f025a519d4cace8aeae
Vladislav Dimitrov <Bones4pk@gmail.com>
2022-01-20 13:50:42 +0200

    Add new exercise

---
+++ C#Fundamentals/03.Arrays/MaxSequenceOfEqualElements/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+
+namespace MaxSequenceOfEqualElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] elements = Console.ReadLine()
+                .Split()
+                .ToArray();
+
+            
+            int arrayLength = 0;
+            string element = string.Empty;
+            
+            for (int i = 0; i < elements.Length; i++)
+            {
+                int lengthCounter = 1;
+
+                for (int j = i+1; j < elements.Length; j++)
+                {
+                    if (elements[i] == elements[j])
+                    {
+                        lengthCounter++;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+
+                if (lengthCounter > arrayLength)
+                {
+                    arrayLength = lengthCounter;
+                    element = elements[i];
+                }
+
+            }
+
+            for (int i = 0; i < arrayLength; i++)
+            {
+                Console.Write($"{element} ");
+            }
+           
+        }
+    }
+}
