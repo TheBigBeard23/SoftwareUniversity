c2f517dabb73a09ac8eed4ecea66b6ad97225693
TheBigBeard23 <Bones4pk@gmail.com>
2022-01-28 18:09:39 +0200

    Add new exercise

---
+++ C#Fundamentals/03.Arrays/RecursiveFibonacci/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace RecursiveFibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(GetFibonacci(number));
+        }
+
+        private static int GetFibonacci(int number)
+        {
+
+            if (number <= 1)
+            {
+                return number;
+            }
+            else
+            {
+                return GetFibonacci(number - 1) + GetFibonacci(number - 2);
+            }
+        }
+    }
+}
