d5081313ca5e8d86edcd4310ae9db337f029e675
TheBigBeard23 <Bones4pk@gmail.com>
2022-01-29 12:21:44 +0200

    Add new exercise

---
+++ C#Fundamentals/03.Arrays/FoldAndSum/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace FoldAndSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int k = numbers.Length / 4;
+            int[] newArray = new int[numbers.Length / 2];
+
+            for (int i = 0; i < k; i++)
+            {
+                newArray[i] = numbers[i + k] + numbers[k - 1 - i];
+                newArray[i + k] = numbers[i + 2 * k] + numbers[numbers.Length - 1 - i];
+            }
+
+            Console.WriteLine(string.Join(" ",newArray));
+        }
+    }
+}
