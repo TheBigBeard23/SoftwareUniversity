440988b92b468c85b2e726e663072791cc46bd9e
TheBigBeard23 <Bones4pk@gmail.com>
2022-01-31 11:31:53 +0200

    Add new exercise

---
+++ C#Fundamentals/03.Arrays/LongestIncreasingSubsequence/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LongestIncreasingSubsequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int maxLength = 0;
+            int lastIndex = -1;
+            int[] len = new int[array.Length];
+            int[] prev = new int[array.Length];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                len[i] = 1;
+                prev[i] = -1;
+
+                for (int k = 0; k < i; k++)
+                {
+                    if(array[k]<array[i] )
+                    {
+                        len[i] = 1 + len[k];
+                        prev[i] = k;
+                    }
+                }
+
+                if (len[i] > maxLength)
+                {
+                    maxLength = len[i];
+                    lastIndex = i;
+                }
+
+            }
+
+            int[] LIS = new int[maxLength];
+            int currentIndex = maxLength - 1;
+
+            while (lastIndex != -1)
+            {
+                LIS[currentIndex] = array[lastIndex];
+                currentIndex--;
+                lastIndex = prev[lastIndex];
+            }
+
+            Console.WriteLine(string.Join(" ", LIS));
+        }
+    }
+}
