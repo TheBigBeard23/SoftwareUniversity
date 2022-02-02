1aca348edb23c2c39bec5ec70620ee071c9b7ccc
TheBigBeard23 <Bones4pk@gmail.com>
2022-02-02 23:10:21 +0200

    Add new exercise

---
+++ C#Fundamentals/04.Methods/MathPower/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MathPower
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
