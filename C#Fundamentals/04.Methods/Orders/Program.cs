7c15eb7811ce6c42cb9786a721c2e8eb7960ae04
TheBigBeard23 <Bones4pk@gmail.com>
2022-02-02 13:12:59 +0200

    Add new exercise

---
+++ C#Fundamentals/04.Methods/Orders/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Orders
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string product = Console.ReadLine();
+            int quantity = int.Parse(Console.ReadLine());
+
+            TotalPrice(product, quantity);
+        }
+        static void TotalPrice(string product,int quantity)
+        {
+            double sum = double.Parse(Console.ReadLine());
+
+            switch (product)
+            {
+                case "coffee":
+                    sum = 1.5 * quantity;
+                    break;
+                case "water":
+                    sum = 1.0 * quantity;
+                    break;
+                case "coke":
+                    sum = 1.4 * quantity;
+                    break;
+                case "snacks":
+                    sum = 2.0 * quantity;
+                    break;  
+            }
+
+            Console.WriteLine($"{sum:f2}");
+
+        }
+    }
+}
