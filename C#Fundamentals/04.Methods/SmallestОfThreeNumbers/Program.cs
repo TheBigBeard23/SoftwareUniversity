0f5725517518fa4f0466296fa086069d1de891d8
TheBigBeard23 <Bones4pk@gmail.com>
2022-02-03 14:17:46 +0200

    Add new exercise

---
+++ C#Fundamentals/04.Methods/SmallestОfThreeNumbers/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace SmallestОfThreeNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+            int thirdNumber = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(GetSmallestNumber(firstNumber,secondNumber,thirdNumber));
+
+        }
+        static int GetSmallestNumber(int firstNumber,int secondNumber,int thirdNumber)
+        {
+            int[] numbers = new int[] { firstNumber, secondNumber, thirdNumber };
+
+            return numbers.OrderBy(x => x).FirstOrDefault();
+        }
+
+    }
+}
