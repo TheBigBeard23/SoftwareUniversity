7dcb7f3ad4afdf7fdfd4623ccbbedc05e7c3bd33
TheBigBeard23 <Bones4pk@gmail.com>
2022-02-03 23:17:50 +0200

    Add new exercise

---
+++ C#Fundamentals/04.Methods/MiddleCharacters/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MiddleCharacters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
