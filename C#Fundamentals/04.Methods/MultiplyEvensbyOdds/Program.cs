5c3437da49bd245b94964f5dff7985eec18bdba4
TheBigBeard23 <Bones4pk@gmail.com>
2022-02-03 11:50:32 +0200

    Add new exercise

---
+++ C#Fundamentals/04.Methods/MultiplyEvensbyOdds/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace MultiplyEvensbyOdds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = Math.Abs(int.Parse(Console.ReadLine()));
+
+            int sumOfEvenDigits = GetSumOfEvenDigits(number);
+            int sumOfOddDigits = GetSumOfOddDigits(number);
+
+            Console.WriteLine(sumOfEvenDigits + sumOfOddDigits); ;
+        }
+        static int GetSumOfEvenDigits(int number)
+        {
+            int sum = 0;
+            string numberAsString = number.ToString();
+
+            for (int i = 0; i < numberAsString.Length; i++)
+            {
+                int currentNumber = int.Parse(numberAsString[i].ToString());
+
+                if (currentNumber % 2 == 0)
+                {
+                    sum += currentNumber;
+                }
+            }
+
+            return sum;
+        }
+        static int GetSumOfOddDigits(int number)
+        {
+            int sum = 0;
+            string numberAsString = number.ToString();
+
+            for (int i = 0; i < numberAsString.Length; i++)
+            {
+                int currentNumber = int.Parse(numberAsString[i].ToString());
+
+                if (currentNumber % 2 == 1)
+                {
+                    sum += currentNumber;
+                }
+            }
+
+            return sum;
+        }
+    }
+}
