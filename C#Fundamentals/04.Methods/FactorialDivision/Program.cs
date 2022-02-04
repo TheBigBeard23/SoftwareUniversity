c7a64b689c6f3104a049a71001cedf3a6e03fb45
TheBigBeard23 <Bones4pk@gmail.com>
2022-02-04 14:13:16 +0200

    Add new exercise

---
+++ C#Fundamentals/04.Methods/FactorialDivision/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace FactorialDivision
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double firstNumber = double.Parse(Console.ReadLine());
+            double secondNumber = double.Parse(Console.ReadLine());
+
+            double firstFactorial = GetFactorial(firstNumber);
+            double secondFactorial = GetFactorial(secondNumber);
+            double result = firstFactorial / secondFactorial;
+
+            Console.WriteLine($"{result:f2}");
+
+        }
+        static double GetFactorial(double number)
+        {
+            double result = 1;
+
+            for (int i = 2; i <= number; i++)
+            {
+                result *= i;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ C#Fundamentals/04.Methods/PalindromeIntegers/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace PalindromeIntegers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string number = Console.ReadLine();
+            
+            while (number!="END")
+            {
+                Console.WriteLine(CheckDoesNumberArePalindrome(number).ToString().ToLower());
+
+                number = Console.ReadLine();
+            }
+        }
+        static bool CheckDoesNumberArePalindrome(string number)
+        {
+            string reversedNumber = string.Empty;
+
+            for (int i = number.Length-1; i >= 0; i--)
+            {
+                reversedNumber += number[i].ToString();
+            }
+
+            if (reversedNumber == number)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
