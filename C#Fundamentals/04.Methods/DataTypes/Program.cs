d49b30db2f14ea6df3c373113db7681cfae17ce5
TheBigBeard23 <Bones4pk@gmail.com>
2022-02-05 11:47:54 +0200

    Add new exercise

---
+++ C#Fundamentals/04.Methods/DataTypes/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace DataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dataType = Console.ReadLine();
+
+            switch (dataType)
+            {
+
+                case "int":
+
+                    int numberInt = int.Parse(Console.ReadLine());
+                    Console.WriteLine(CalculateInt(numberInt));
+
+                    break;
+
+                case "real":
+
+                    double numberDouble = double.Parse(Console.ReadLine());
+                    Console.WriteLine($"{CalculateDouble(numberDouble):f2}");
+
+                    break;
+
+                case "string":
+
+                    string word = Console.ReadLine();
+                    Console.WriteLine(ModificateString(word));
+
+                    break;
+            }
+        }
+        static int CalculateInt(int number)
+        {
+            return number * 2;
+        }
+        static double CalculateDouble(double number)
+        {
+            return number * 1.50;
+        }
+        static string ModificateString(string word)
+        {
+            return $"${word}$";
+        }
+    }
+}
