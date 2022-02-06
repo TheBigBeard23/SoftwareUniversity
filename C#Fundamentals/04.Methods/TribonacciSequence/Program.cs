2b9c21e998f7bb09b9b32928dba0912bded1da47
TheBigBeard23 <Bones4pk@gmail.com>
2022-02-06 09:08:00 +0200

    Add new exercise

---
+++ C#Fundamentals/04.Methods/TribonacciSequence/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace TribonacciSequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            PrintTribonacciSequence(n);
+        }
+
+        static void PrintTribonacciSequence(int n)
+        {
+            int n1 = 0;
+            int n2 = 1;
+            int n3 = 1;
+            int n4 = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                if (i == 0 || i == 1)
+                {
+                    Console.Write("1 ");
+                }
+                else
+                {
+                    n4 += n3 + n2 + n1;
+
+                    Console.Write($"{n4} ");
+
+                    n1 = n2;
+                    n2 = n3;
+                    n3 = n4;
+                    n4 = 0;
+                }
+
+            }
+        }
+    }
+}
