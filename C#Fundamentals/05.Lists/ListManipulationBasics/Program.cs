f557e2f1b00a33fca0bf05186b11af95337bd229
TheBigBeard23 <Bones4pk@gmail.com>
2022-02-08 23:48:59 +0200

    Add new exercise

---
+++ C#Fundamentals/05.Lists/ListManipulationBasics/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ListManipulationBasics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            string input = Console.ReadLine();
+
+            while (input != "end")
+            {
+                var commands = input
+                                    .Split()
+                                    .ToList();
+
+                string operation = commands[0];
+
+                switch (operation)
+                {
+                    case "Add":
+
+                        int number = int.Parse(commands[1]);
+                        numbers.Add(number);
+
+                        break;
+
+                    case "Remove":
+
+                        number = int.Parse(commands[1]);
+                        numbers.Remove(number);
+
+                        break;
+
+                    case "RemoveAt":
+
+                        number = int.Parse(commands[1]);
+                        numbers.RemoveAt(number);
+
+                        break;
+
+                    case "Insert":
+
+                        number = int.Parse(commands[1]);
+                        int index = int.Parse(commands[2]);
+                        numbers.Insert(index,number);
+
+                        break;
+
+                }
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine(string.Join(" ",numbers));
+        }
+       
+    }
+}
--- C#Fundamentals/05.Lists/RemoveNegativesAndReverse/Program.cs
+++ C#Fundamentals/05.Lists/RemoveNegativesAndReverse/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace RemoveNegativesAndReverse
 {
@@ -6,7 +8,21 @@ namespace RemoveNegativesAndReverse
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<int> numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .Where(x => x > 0)
+                .Reverse()
+                .ToList();
+
+            if (numbers.Count > 0)
+            {
+                Console.WriteLine(string.Join(" ",numbers));
+            }
+            else
+            {
+                Console.WriteLine("empty");
+            }
         }
     }
 }
