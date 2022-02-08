b0c1bd0de79fde62a12d6c0e90ed459d27e938c1
TheBigBeard23 <Bones4pk@gmail.com>
2022-02-08 23:15:44 +0200

    Add new exercise

---
+++ C#Fundamentals/05.Lists/ListOfProducts/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ListOfProducts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            List<string> ithems = new List<string>(n);
+
+            for (int i = 0; i < n; i++)
+            {
+                ithems.Add(Console.ReadLine());
+            }
+
+            PrintSortedItems(ithems);
+        }
+        static void PrintSortedItems(List<string> ithems)
+        {
+            int counter = 1;
+
+            foreach (var item in ithems.OrderBy(x=>x))
+            {
+                Console.WriteLine($"{counter}.{item}");
+                counter++;
+            }
+        }
+    }
+}
