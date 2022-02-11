b5cb3b349b763aee538dcbdafcc21af238e6726b
TheBigBeard23 <Bones4pk@gmail.com>
2022-02-12 00:07:42 +0200

    Add new exercise

---
+++ C#Fundamentals/05.Lists/AppendArrays/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AppendArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> sequences = Console.ReadLine()
+                                     .Split('|',StringSplitOptions.RemoveEmptyEntries)
+                                     .Reverse()
+                                     .ToList();
+
+       
+            string sequencesAsString = string.Join(" ", sequences);
+
+            List<string> sequence = sequencesAsString
+                                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                                    .ToList();
+
+            Console.WriteLine(string.Join(" ",sequence));
+
+
+
+
+        }
+    }
+}
