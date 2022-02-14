f6dad59326b4558075f017e4c6a7f63ac53ced61
TheBigBeard23 <Bones4pk@gmail.com>
2022-02-14 16:18:40 +0200

    Add new exercise

---
+++ C#Fundamentals/05.Lists/CarRace/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CarRace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                                .Split()
+                                .Select(int.Parse)
+                                .ToList();
+            
+            int startIndex = 0;
+            int endIndex = numbers.Count / 2;
+
+            double leftTime = CalculateTime(numbers, startIndex, endIndex);
+
+            startIndex = endIndex + 1;
+            endIndex = numbers.Count;
+
+            double rightTime = CalculateTime(numbers, startIndex, endIndex);
+
+            if (leftTime < rightTime)
+            {
+                Console.WriteLine($"The winner is left with total time: {leftTime}");
+            }
+            else
+            {
+                Console.WriteLine($"The winner is right with total time: {rightTime}");
+            }
+
+        }
+
+        private static double CalculateTime(List<int> numbers, int startIndex,int endIndex)
+        {
+            double time = 0.0;
+
+            for (int i = startIndex; i < endIndex; i++)
+            {
+                if (numbers[i] == 0)
+                {
+                    time *= 0.8;
+                }
+                else
+                {
+                    time += numbers[i];
+                }
+            }
+
+            return time;
+        }
+    }
+}
