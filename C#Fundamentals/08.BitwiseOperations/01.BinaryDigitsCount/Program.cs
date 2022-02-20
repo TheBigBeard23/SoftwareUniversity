c6ecbea30f557a202221283599176fd805501d1f
TheBigBeard23 <Bones4pk@gmail.com>
2022-02-20 20:29:07 +0200

    Add new exercise

---
+++ C#Fundamentals/08.BitwiseOperations/01.BinaryDigitsCount/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _01.BinaryDigitsCount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int binaryDigit = int.Parse(Console.ReadLine());
+            int counter = 0;
+
+            while (number>0)
+            {
+                int currentDigit = number % 2;
+
+                if (currentDigit == binaryDigit)
+                {
+                    counter++;
+                }
+
+                number /= 2;
+            }
+
+            Console.WriteLine(counter);
+        }
+    }
+}
