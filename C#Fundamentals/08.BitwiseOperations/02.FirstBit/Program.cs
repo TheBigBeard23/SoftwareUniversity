c113a75747ea716a3c8eb0956724658632263d05
TheBigBeard23 <Bones4pk@gmail.com>
2022-02-20 22:25:44 +0200

    Add new exercise

---
+++ C#Fundamentals/08.BitwiseOperations/02.FirstBit/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _02.FirstBit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            int firstBit = 0;
+
+            while (number>0)
+            {
+                firstBit = number % 2;
+                number /= 2;
+            }
+
+            Console.WriteLine(firstBit);
+
+            
+
+
+        }
+    }
+}
