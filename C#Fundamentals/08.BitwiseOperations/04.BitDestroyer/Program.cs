7eb0409b0a0b25067518cd37034ed3854f4c8535
TheBigBeard23 <Bones4pk@gmail.com>
2022-02-21 16:14:29 +0200

    add new exercise

---
+++ C#Fundamentals/08.BitwiseOperations/04.BitDestroyer/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _04.BitDestroyer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            int position = int.Parse(Console.ReadLine());
+
+            int mask = 1 << position;
+
+            mask = ~mask;
+
+            number = number & mask;
+
+            Console.WriteLine(number);
+        }
+    }
+}
