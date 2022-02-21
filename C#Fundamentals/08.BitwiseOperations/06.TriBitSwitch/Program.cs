8536ac2f9c127e9060aeb9b029ae7fb430cdae47
TheBigBeard23 <Bones4pk@gmail.com>
2022-02-21 16:47:47 +0200

    Add new exercise

---
+++ C#Fundamentals/08.BitwiseOperations/06.TriBitSwitch/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _06.TriBitSwitch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int position = int.Parse(Console.ReadLine());
+
+            int mask = 7 << position;
+            int result = number ^ mask;
+
+            Console.WriteLine(result);
+        }
+    }
+}
