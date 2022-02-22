51dc54243449807898d2a508da126f5baf498433
TheBigBeard23 <Bones4pk@gmail.com>
2022-02-22 19:28:56 +0200

    Add new exercise

---
+++ C#Fundamentals/09.ObjectsAndClasses/02.BigFactorial/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Numerics;
+
+namespace _02.BigFactorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Factorial factorial = new Factorial();
+            factorial.number = int.Parse(Console.ReadLine());
+            Console.WriteLine(factorial.Calculate());
+            
+        }
+
+    }
+    public class Factorial
+    {
+        public int number;
+        public BigInteger Calculate()
+        {
+            BigInteger result = number;
+
+            for (int i = 1; i < number; i++)
+            {
+                result *= i;
+            }
+
+            return result;
+
+        }
+
+    }
+}
