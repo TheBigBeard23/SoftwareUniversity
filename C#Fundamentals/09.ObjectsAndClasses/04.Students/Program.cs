4754caf59ffe57567f653340b275710d6332ed0c
TheBigBeard23 <Bones4pk@gmail.com>
2022-02-22 20:14:38 +0200

    Add new exercise

---
+++ C#Fundamentals/09.ObjectsAndClasses/04.Students/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04.Students
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            List<Student> students = new List<Student>();
+
+            while (input != "end")
+            {
+                string[] data = input.Split();
+
+                string firstName = data[0];
+                string lastName = data[1];
+                int age = int.Parse(data[2]);
+                string homeTown = data[3];
+
+                students.Add(new Student(firstName, lastName, age, homeTown));
+
+                input = Console.ReadLine();
+            }
+
+            string town = Console.ReadLine();
+
+            foreach (var student in students.Where(x => x.HomeTown == town))
+            {
+                Console.WriteLine($"{student.FirstName} {student.LastName} is {student.Age} years old.");
+            }
+        }
+    }
+    public class Student
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string HomeTown { get; set; }
+
+        public Student(string firstName, string lastName, int age, string homeTown)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            HomeTown = homeTown;
+        }
+    }
+}
