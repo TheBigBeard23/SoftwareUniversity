d3e80438f49455aa68618c81d34786196c8c5142
TheBigBeard23 <Bones4pk@gmail.com>
2022-02-27 00:18:51 +0200

    Add new exercise

---
+++ C#Fundamentals/09.ObjectsAndClasses/10.Articles2.0/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _10.Articles2._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+            Article article = new Article();
+
+            while (count > 0)
+            {
+                string[] input = Console.ReadLine()
+                                .Split(", ")
+                                .ToArray();
+
+                string title = input[0];
+                string content = input[1];
+                string author = input[2];
+
+                article.Articles.Add(new Article(title, content, author));
+
+                count--;
+
+            }
+
+            string order = Console.ReadLine();
+
+            if(order == "author")
+            {
+                foreach (var item in article.Articles.OrderBy(x => x.Author))
+                {
+                    Console.WriteLine(item.ToString());
+                }
+            }
+            else if(order=="content")
+            {
+                foreach (var item in article.Articles.OrderBy(x => x.Content))
+                {
+                    Console.WriteLine(item.ToString());
+                }
+            }
+            else
+            {
+                foreach (var item in article.Articles.OrderBy(x => x.Title))
+                {
+                    Console.WriteLine(item.ToString());
+                }
+            }
+
+        }
+    }
+    class Article
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string Author { get; set; }
+
+        public List<Article> Articles { get; set; }
+
+        public Article(string title, string content, string author)
+        {
+            Title = title;
+            Content = content;
+            Author = author;
+        }
+        public Article()
+        {
+            Articles = new List<Article>();
+        }
+
+        public override string ToString()
+        {
+            return $"{Title} - {Content}: {Author}";
+        }
+    }
+}
