fe3368c4dbcbcf68ba8391b100fb80647432e2da
TheBigBeard23 <Bones4pk@gmail.com>
2022-03-02 16:58:38 +0200

    Add new exercise

---
+++ C#Fundamentals/10.AssociativeArrays/01.CountRealNumbers/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.CountRealNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, int> numbersCount = new Dictionary<int, int>();
+
+            int[] numbers = Console.ReadLine()
+                                   .Split()
+                                   .Select(int.Parse)
+                                   .ToArray();
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbersCount.ContainsKey(numbers[i]))
+                {
+                    numbersCount[numbers[i]]++;
+                }
+                else
+                {
+                    numbersCount[numbers[i]] = 1;
+                }
+            }
+
+            foreach (var number in numbersCount.OrderBy(x=>x.Key))
+            {
+                Console.WriteLine($"{number.Key} -> {number.Value}");
+            }
+        }
+    }
+}
