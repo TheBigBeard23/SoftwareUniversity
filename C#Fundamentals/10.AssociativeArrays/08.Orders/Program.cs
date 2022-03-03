cfa64eb7fe717437b83f3186afd0d79c5cb4d1cc
TheBigBeard23 <Bones4pk@gmail.com>
2022-03-03 23:26:49 +0200

    Add new exercise

---
+++ C#Fundamentals/10.AssociativeArrays/08.Orders/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _08.Orders
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, decimal> productPrices = new Dictionary<string, decimal>();
+            Dictionary<string, int> productQuantities = new Dictionary<string, int>();
+
+            string input = Console.ReadLine();
+
+            while (input != "buy")
+            {
+                string[] data = input.Split(' ',StringSplitOptions.RemoveEmptyEntries)
+                                     .ToArray();
+
+                string name = data[0];
+                decimal price = decimal.Parse(data[1]);
+                int quantity = int.Parse(data[2]);
+
+                if (!productPrices.ContainsKey(name))
+                {
+                    productPrices[name] = price;
+                    productQuantities[name] = quantity;
+                }
+                else
+                {
+                    productPrices[name] = price;
+                    productQuantities[name] += quantity;
+
+                }
+        
+                input = Console.ReadLine();
+            }
+
+            foreach (var product in productPrices)
+            {
+                string productName = product.Key;
+                decimal productPrice = product.Value;
+                int quantity = productQuantities[productName];
+
+                Console.WriteLine($"{productName} -> {productPrice*quantity:f2}");
+            }
+            
+        }
+    }
+}
