4869ca4386d772ee02a28b1f14532e5f7d78a545
TheBigBeard23 <Bones4pk@gmail.com>
2022-03-04 12:01:11 +0200

    Add new exercise

---
+++ C#Fundamentals/10.AssociativeArrays/12.CompanyUsers/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _12.CompanyUsers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<string>> companyEmployees = new Dictionary<string, List<string>>(); 
+            string input = Console.ReadLine();
+
+            while (input != "End")
+            {
+                string[] data = input.Split(" -> ")
+                                     .ToArray();
+
+                string companyName = data[0];
+                string employeeId = data[1];
+
+                if (!companyEmployees.ContainsKey(companyName))
+                {
+                    companyEmployees[companyName] = new List<string>();
+                }
+                if (!companyEmployees[companyName].Contains(employeeId))
+                {
+                    companyEmployees[companyName].Add(employeeId);
+                }
+                
+
+                input = Console.ReadLine();
+            }
+
+            foreach (var company in companyEmployees)
+            {
+                Console.WriteLine($"{company.Key} \n-- {string.Join("\n-- ",company.Value)}");
+
+            }
+        }
+    }
+}
