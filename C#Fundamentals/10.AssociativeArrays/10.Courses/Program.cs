0d880ec4fcec2b212ff75094b0769479528adff3
TheBigBeard23 <Bones4pk@gmail.com>
2022-03-04 11:28:24 +0200

    Add new exercise

---
+++ C#Fundamentals/10.AssociativeArrays/10.Courses/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _10.Courses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<string>> courseStudents = new Dictionary<string, List<string>>();
+
+            string input = Console.ReadLine();
+
+            while (input!="end")
+            {
+                string[] data = input.Split(" : ")
+                                     .ToArray();
+
+                string courseName = data[0];
+                string studentName = data[1];
+
+                if (!courseStudents.ContainsKey(courseName))
+                {
+                    courseStudents[courseName] = new List<string>();
+                }
+
+                courseStudents[courseName].Add(studentName);
+
+                input = Console.ReadLine();
+            }
+
+            foreach (var course in courseStudents)
+            {
+                Console.WriteLine($"{course.Key}: {course.Value.Count}");
+                Console.WriteLine($"-- {string.Join("\n-- ",course.Value)}");
+            }
+        }
+    }
+}
