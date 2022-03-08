5db2e6074a28fbe4ca1cd4626f26f59b683eaf79
TheBigBeard23 <Bones4pk@gmail.com>
2022-03-08 10:14:04 +0200

    Add new exercise

---
+++ C#Fundamentals/11.TextProcessing/01.ReverseStrings/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01.ReverseStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> words = new List<string>();
+
+            string input = Console.ReadLine();
+
+            while (input != "end")
+            {
+                words.Add(input);
+
+                input = Console.ReadLine();
+            }
+
+            foreach (var word in words)
+            {
+                Console.WriteLine($"{word} = {Reverse(word)}");
+            }
+
+        }
+        static string Reverse(string text)
+        {
+            char[] cArray = text.ToCharArray();
+            string reverse = String.Empty;
+            for (int i = cArray.Length - 1; i > -1; i--)
+            {
+                reverse += cArray[i];
+            }
+            return reverse;
+        }
+    }
+}
---
+++ C#Fundamentals/11.TextProcessing/02.RepeatStrings/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _02.RepeatStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split();
+            string result = string.Empty;
+
+            foreach (var word in words)
+            {
+                result += Repeat(word);
+            }
+
+            Console.WriteLine(result);
+        }
+        static string Repeat(string word)
+        {
+            string newWord = string.Empty;
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                newWord += word;
+            }
+
+            return newWord;
+        }
+    }
+}
