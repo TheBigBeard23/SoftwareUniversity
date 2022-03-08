fcdeff8243b33be41d8f6a805a1c50f1a4c845dd
TheBigBeard23 <Bones4pk@gmail.com>
2022-03-08 11:07:16 +0200

    Add new exercise:

---
+++ C#Fundamentals/11.TextProcessing/06.ValidUsernames/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace _06.ValidUsernames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] usernames = Console.ReadLine().Split(", ");
+
+            foreach (var username in usernames)
+            {
+                if (CheckUsername(username))
+                {
+                    Console.WriteLine(username);
+                }
+            }
+        }
+        static bool CheckUsername(string username)
+        {
+            if (username.Length < 3 ||
+                username.Length > 16)
+            {
+                return false;
+            }
+
+            for (int i = 0; i < username.Length; i++)
+            {
+                char crnChar = username[i];
+
+                if (!char.IsDigit(crnChar)  &&
+                    !char.IsLetter(crnChar) &&
+                    crnChar != '_'          && 
+                    crnChar != '-')
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
