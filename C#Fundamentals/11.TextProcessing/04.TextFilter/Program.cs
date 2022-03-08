bafe105ddaf14aa7e9307e14a4967d39caba1752
TheBigBeard23 <Bones4pk@gmail.com>
2022-03-08 10:44:39 +0200

    Add new exercise

---
+++ C#Fundamentals/11.TextProcessing/04.TextFilter/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _04.TextFilter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] bannedWords = Console.ReadLine().Split(", ");
+            string text = Console.ReadLine();
+
+            for (int i = 0; i < bannedWords.Length; i++)
+            {
+                text = Replace(bannedWords[i], text);
+            }
+
+            Console.WriteLine(text);
+        }
+        static string Replace(string banWord, string text)
+        {
+            string replacement = new string('*', banWord.Length);
+
+            while (text.Contains(banWord))
+            {
+                text = text.Replace(banWord, replacement);
+            }
+
+            return text;
+        }
+    }
+}
