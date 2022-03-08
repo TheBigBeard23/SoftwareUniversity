9fa04519ea001e0398ef14f2bdc4c4fcc40f28cc
TheBigBeard23 <Bones4pk@gmail.com>
2022-03-08 10:19:00 +0200

    Add new exercise

---
+++ C#Fundamentals/11.TextProcessing/03.Substring/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _03.Substring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstWord = Console.ReadLine();
+            string secondWord = Console.ReadLine();
+
+            while (secondWord.Contains(firstWord))
+            {
+                secondWord = secondWord.Replace(firstWord, "");
+            }
+
+            Console.WriteLine(secondWord);
+        }
+    }
+}
