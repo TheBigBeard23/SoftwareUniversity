fc055778854452c6652bf7bcd1383a0c83142258
TheBigBeard23 <Bones4pk@gmail.com>
2022-03-09 12:17:56 +0200

    Add new exercise

---
+++ C#Fundamentals/11.TextProcessing/08.ExtractFile/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace _08.ExtractFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            string fileName = input.Split('\\').Last().Split('.').First();
+            string extension = input.Split('\\').Last().Split('.').Last();
+
+            Console.WriteLine($"File name: {fileName}");
+            Console.WriteLine($"File extension: {extension}");
+        }
+    }
+}
