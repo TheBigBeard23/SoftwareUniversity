3e913d02f94fdb1b946efa00033243beea7eab15
TheBigBeard23 <Bones4pk@gmail.com>
2022-03-10 23:07:42 +0200

    Add new exercise

---
+++ C#Fundamentals/11.TextProcessing/18.HTML/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+
+namespace _18.HTML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            string title = Console.ReadLine();
+            string content = Console.ReadLine();
+
+            sb.AppendLine($"<h1>");
+            sb.AppendLine($"    {title}");
+            sb.AppendLine($"</h1>");
+            sb.AppendLine($"<article>");
+            sb.AppendLine($"    {content}");
+            sb.AppendLine($"</article>");
+
+            string comment = Console.ReadLine();
+
+            while (comment != "end of comments")
+            {
+                sb.AppendLine($"<div>");
+                sb.AppendLine($"    {comment}");
+                sb.AppendLine($"</div>");
+
+                comment = Console.ReadLine();
+            }
+
+            Console.WriteLine(sb);
+        }
+    }
+}
