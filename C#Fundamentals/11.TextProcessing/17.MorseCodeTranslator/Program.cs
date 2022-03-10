3703b69c18b1b623b0d786280776b7ea4b683f16
TheBigBeard23 <Bones4pk@gmail.com>
2022-03-10 22:58:30 +0200

    Add new exercise

---
+++ C#Fundamentals/11.TextProcessing/17.MorseCodeTranslator/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _17.MorseCodeTranslator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, char> MorseCodeLetters = new Dictionary<string, char>()
+            {
+                {".-",  'A'},
+                {"-...",'B'},
+                {"-.-.",'C'},
+                {"-..", 'D'},
+                {".",   'E'},
+                {"..-.",'F'},
+                {"--.", 'G'},
+                {"....",'H'},
+                {"..",  'I'},
+                {".---",'J'},
+                {"-.-", 'K'},
+                {".-..",'L'},
+                {"--",  'M'},
+                {"-.",  'N'},
+                {"---", 'O'},
+                {".--.",'P'},
+                {"--.-",'Q'},
+                {".-.", 'R'},
+                {"...", 'S'},
+                {"-",   'T'},
+                {"..-", 'U'},
+                {"...-",'V'},
+                {".--", 'W'},
+                {"-..-",'X'},
+                {"-.--",'Y'},
+                {"--..",'Z'},
+                {"|",   ' '}
+            };
+
+            List<string> codes = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                                                   .ToList();
+
+            string message = string.Empty;
+
+            for (int i = 0; i < codes.Count; i++)
+            {
+                message += MorseCodeLetters[codes[i]];
+            }
+
+            Console.WriteLine(message);
+
+            //--------------------------------------
+            //---- Translate text to Morse code ----
+
+            //string text = Console.ReadLine();
+            //string message = string.Empty;
+
+            //for (int i = 0; i < text.Length; i++)
+            //{
+            //    message += MorseCodeLetters.Where(x => x.Value == text[i]).FirstOrDefault().Key;
+            //    message += ' ';
+            //}
+            //Console.WriteLine(message);
+
+        }
+    }
+}
