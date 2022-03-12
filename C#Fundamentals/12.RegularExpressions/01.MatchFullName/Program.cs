6439b39c906e61c63d16b98ea17173c1bd7370a0
TheBigBeard23 <Bones4pk@gmail.com>
2022-03-12 20:49:16 +0200

    Add new exercise

---
+++ C#Fundamentals/12.RegularExpressions/01.MatchFullName/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _01.MatchFullName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+            string pattern = @"\b[A-Z][a-z]+ [A-Z][a-z]+\b";
+
+            Regex regex = new Regex(pattern);
+
+            MatchCollection matches = regex.Matches(text);
+
+            foreach (var item in matches)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
