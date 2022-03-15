c4450f809394a86a27e36cb73297839d84e182f5
TheBigBeard23 <Bones4pk@gmail.com>
2022-03-15 18:08:02 +0200

    Add new exercise:

--- C#Fundamentals/12.RegularExpressions/06.SoftUniBarIncome/Program.cs
+++ C#Fundamentals/12.RegularExpressions/06.SoftUniBarIncome/Program.cs
@@ -9,10 +9,9 @@ namespace _06.SoftUniBarIncome
     {
         static void Main(string[] args)
         {
-            Dictionary<string, Dictionary<string, double>> customersBag = new Dictionary<string, Dictionary<string, double>>();
-
+  
             string pattern = @"^%(?<name>[A-Z][a-z]+)%[^|$%.]*<(?<product>\w+)>[^|$%.]*\|(?<quantity>\d+)\|[^|$%.]*?(?<price>[-+]?[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?)\$";
-            double sum = 0.0;
+            double totalSum = 0.0;
             string input = Console.ReadLine();
 
             while (input != "end of shift")
@@ -25,28 +24,19 @@ namespace _06.SoftUniBarIncome
                     string product = match.Groups["product"].Value;
                     int quantity = int.Parse(match.Groups["quantity"].Value);
                     double price = double.Parse(match.Groups["price"].Value);
+                    double sum = quantity * price;
 
-                    double crnSum = quantity * price;
-                    Console.WriteLine($"{name}: {product} - {crnSum:f2}");
-                    sum += crnSum;
+                    Console.WriteLine($"{name}: {product} - {sum:f2}");
 
-                    customersBag[name] = new Dictionary<string, double>() { { product, 0.0 } };
-                    customersBag[name][product] += quantity * price;
-                }
-               
+                    totalSum += sum;
+
+                }          
 
                 input = Console.ReadLine();
             }
-            //Console.WriteLine($"Total income: {sum:f2}");
-            //foreach (var customer in customersBag)
-            //{
-            //    foreach (var product in customer.Value)
-            //    {
-            //        Console.WriteLine($"{customer.Key}: {product.Key} - {product.Value:f2}");
-            //    }
-            //}
 
-            Console.WriteLine($"Total income: {customersBag.Values.Select(x=>x.Values.Sum()).Sum():f2}");
+            Console.WriteLine($"Total income: {totalSum:f2}");
+
         }
     }
 }
---
+++ C#Fundamentals/12.RegularExpressions/07.StarEnigma/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _07.StarEnigma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<string> messages = new List<string>();
+            Dictionary<string, List<string>> statusPlanets = new Dictionary<string, List<string>>()
+            {
+                {"Attacked",new List<string>()},
+                {"Destroyed",new List<string>()}
+            };
+
+            while (n>0)
+            {
+                string input = Console.ReadLine();
+                string lettersPattern = @"[STARstar]";
+                int lettersCount = Regex.Matches(input, lettersPattern).Count;
+                messages.Add(DecryptMessage(input, lettersCount));
+
+                n--;
+            }
+
+            string dataPattern = @"@(?<name>[A-Za-z]+)[^@:!\->]*:(?<population>\d+)[^@:!\->]*!(?<command>[A|D])![^@:!\->]*\->(?<soldier>\d+)[^@:!\->]*";
+
+            for (int i = 0; i < messages.Count; i++)
+            {
+                Match match = Regex.Match(messages[i], dataPattern);
+
+                if (match.Success)
+                {
+                    string planetName = match.Groups["name"].Value;
+                    string planetStatus = match.Groups["command"].Value;
+
+                    if (planetStatus == "A")
+                    {
+                        statusPlanets["Attacked"].Add(planetName);
+                    }
+                    else
+                    {
+                        statusPlanets["Destroyed"].Add(planetName);
+                    }
+                    
+                }
+            }
+
+            Console.WriteLine($"Attacked planets: {statusPlanets.Where(x => x.Key == "Attacked").FirstOrDefault().Value.Count()}");
+            List<string> attackedPlanets = statusPlanets.Where(x => x.Key == "Attacked").FirstOrDefault()
+                                                .Value
+                                                .OrderBy(x => x)
+                                                .ToList();
+            Print(attackedPlanets);
+
+            Console.WriteLine($"Destroyed planets: {statusPlanets.Where(x => x.Key == "Destroyed").FirstOrDefault().Value.Count()}");
+            List<string> destroyedPlanets = statusPlanets.Where(x => x.Key == "Destroyed").FirstOrDefault()
+                                               .Value
+                                               .OrderBy(x => x)
+                                               .ToList();
+            Print(destroyedPlanets);
+
+        }
+
+        static string DecryptMessage(string input, int lettersCount)
+        {
+            string message = string.Empty;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                char crnChar = input[i];
+                message += (char)(crnChar - lettersCount);
+            }
+
+            return message;
+        }
+        static void Print(List<string> planets)
+        {
+            
+            for (int i = 0; i < planets.Count; i++)
+            {
+                Console.WriteLine($"-> {planets[i]}");
+            }
+        }
+    }
+}
