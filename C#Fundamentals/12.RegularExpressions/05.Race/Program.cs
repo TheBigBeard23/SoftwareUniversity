510a3ff20ca7bd3fb75cbd68fe203ba2bcff4609
TheBigBeard23 <Bones4pk@gmail.com>
2022-03-15 11:37:28 +0200

    Add new exercise

---
+++ C#Fundamentals/12.RegularExpressions/05.Race/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _05.Race
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> namesDistance = Console.ReadLine().Split(", ")
+                                                   .ToDictionary(x => x, x => 0);
+           
+            string lettersPattern = "[A-Za-z]";
+            string digitsPattern = "[0-9]";
+            string input = Console.ReadLine();
+
+            while (input!="end of race")
+            {
+                MatchCollection letters = Regex.Matches(input, lettersPattern);
+                MatchCollection digits = Regex.Matches(input, digitsPattern);
+
+                string name = string.Join("", letters);
+                int distance = digits.Select(x => x.ToString())
+                                     .Select(int.Parse)
+                                     .Sum();
+
+                if (namesDistance.ContainsKey(name))
+                {
+                    namesDistance[name] += distance;
+                }
+
+                input = Console.ReadLine();
+            }
+
+            namesDistance = namesDistance.OrderByDescending(x => x.Value)
+                                         .ToDictionary(x => x.Key, y => y.Value);
+
+            Console.WriteLine(namesDistance[1]);
+ 
+
+
+
+        }
+    }
+}
