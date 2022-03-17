3c5196909b0a875759417bccc231a1fba8b0bd36
VladislavDim <Bones4pk@gmail.com>
2022-03-17 18:29:12 +0200

    Add new exercise

---
+++ C#Fundamentals/12.RegularExpressions/10.WinningTicket/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _10.WinningTicket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> tickets = Regex.Split(Console.ReadLine(), @" *,{1} *").ToList();
+
+            foreach (var ticket in tickets)
+            {
+                CheckTicket(ticket);
+            }
+        }
+
+        static void CheckTicket(string ticket)
+        {
+            string pattern = @"([@#$^])\1+";
+
+            if (ticket.Length == 20)
+            {
+                MatchCollection matches = Regex.Matches(ticket, pattern);
+
+                if (matches.Count == 1)
+                {
+                    Console.WriteLine($"ticket \"{ticket}\" - 10{ticket[0]} Jackpot!");
+                }
+                else
+                {
+                    PrintResult(matches, ticket);
+                }           
+            }
+            else
+            {
+                Console.WriteLine("invalid ticket");
+            }
+        }
+
+        static void PrintResult(MatchCollection matches,string ticket)
+        {
+            string firstSequence = matches.OrderByDescending(x => x.Length).First().ToString();
+            string secondSequence = matches.OrderByDescending(x => x.Length)
+                                           .Select(x => x.ToString())
+                                           .ToList()[1];
+
+            if (firstSequence[0] == secondSequence[0] &&
+                secondSequence.Length >= 6 && secondSequence.Length <= 9)
+            {
+                Console.WriteLine($"ticket \"{ticket}\" - {secondSequence.Length}{secondSequence[0]}");
+            }
+            else
+            {     
+                    Console.WriteLine($"ticket \"{ticket}\" - no match");         
+            }
+
+        }
+    }
+}
