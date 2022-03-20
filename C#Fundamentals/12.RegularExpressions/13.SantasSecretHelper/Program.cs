fdd7555a2f2047b3452a44cb8be18a0a50c4e20a
VladislavDim <Bones4pk@gmail.com>
2022-03-20 13:56:24 +0200

    Add new exercise

---
+++ C#Fundamentals/12.RegularExpressions/13.SantasSecretHelper/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _13.SantasSecretHelper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int key = int.Parse(Console.ReadLine());
+            string cryptedMessage = Console.ReadLine();
+
+            while (cryptedMessage!="end")
+            {
+                string message = Decrypt(cryptedMessage,key);
+                Match match = Regex.Match(message, @"@(?<name>[A-Za-z]+)[^-@!:>]+!(?<behavior>G)!");
+
+                if (match.Success)
+                {
+                    string name = match.Groups["name"].Value;
+
+                    Console.WriteLine(name);
+                }
+
+                cryptedMessage = Console.ReadLine();
+            }
+
+        }
+
+        static string Decrypt(string cryptedMessage,int key)
+        {
+            string message = string.Empty;
+
+            for (int i = 0; i < cryptedMessage.Length; i++)
+            {
+                char crnChar = (char)(cryptedMessage[i] - key);
+                message += crnChar;
+            }
+
+            return message;
+        }
+    }
+}
