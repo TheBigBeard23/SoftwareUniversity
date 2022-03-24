1b2c2ae274c1bbc68bf919da57417e21267c9ec0
VladislavDim <Bones4pk@gmail.com>
2022-03-24 22:30:21 +0200

    add new exercise

---
+++ C#Advanced/01.StacksAndQueues/1.ReverseStrings/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+
+namespace _1.ReverseStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack text = new Stack();
+            string input = Console.ReadLine();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                char crnChar = input[i];
+                text.Push(crnChar);
+            }
+
+            while (text.Count>0)
+            {
+                Console.Write(text.Pop());
+            }
+
+        }
+    }
+}
