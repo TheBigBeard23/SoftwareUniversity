11f65e61e60008e7ead24e7fba2d18376e8c50c0
VladislavDim <Bones4pk@gmail.com>
2022-04-06 16:02:47 +0300

    Add new exercise

--- C#Advanced/01.StacksAndQueues/4.MatchingBrackets/Program.cs
+++ C#Advanced/01.StacksAndQueues/4.MatchingBrackets/Program.cs
@@ -50,6 +50,7 @@ namespace _4.MatchingBrackets
                     
                 }
             }
+
             if (expression != "")
             {
                 Console.WriteLine(expression);
---
+++ C#Advanced/01.StacksAndQueues/5.PrintEvenNumbers/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _5.PrintEvenNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> numbers = new Queue<int>(Console.ReadLine().Split()
+                                                                  .Select(int.Parse)
+                                                                  .Where(x => x % 2 == 0));
+
+            Console.WriteLine(string.Join(", ", numbers));
+
+        }
+    }
+}
