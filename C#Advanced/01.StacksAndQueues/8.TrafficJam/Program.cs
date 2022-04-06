e7d8e51bec139c17564f2dac534ab8a0225b8f8c
VladislavDim <Bones4pk@gmail.com>
2022-04-06 17:09:24 +0300

    Add new exercise

---
+++ C#Advanced/01.StacksAndQueues/8.TrafficJam/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _8.TrafficJam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> cars = new Queue<string>();
+            int count = int.Parse(Console.ReadLine());
+            int passedCars = 0;
+            string input = Console.ReadLine();
+
+            while (input != "end")
+            {
+                if (input == "green")
+                {
+                    PrintPassedCars(cars, count, ref passedCars);
+                  
+                }
+
+                else
+                {
+                    cars.Enqueue(input);
+                }
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine($"{passedCars} cars passed the crossroads.");
+        }
+
+        static void PrintPassedCars(Queue<string> cars, int count,int passedCars)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                if (cars.Count == 0)
+                {
+                    break;
+                }
+
+                Console.WriteLine($"{cars.Dequeue()} passed!");
+                passedCars++;
+            }
+        }
+    }
+}
