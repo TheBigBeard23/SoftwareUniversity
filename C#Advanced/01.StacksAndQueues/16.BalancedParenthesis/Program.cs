7e97483ae0f5f761e3bfe9decaddf43493b0a526
VladislavDim <Bones4pk@gmail.com>
2022-04-10 12:41:27 +0300

    Add new exercise

---
+++ C#Advanced/01.StacksAndQueues/16.BalancedParenthesis/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _16.BalancedParenthesis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<string> openingBrackets = new Stack<string>();
+            string input = Console.ReadLine();
+            bool isBalanced = true;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                string bracket = input[i].ToString();
+
+                if (bracket == "[" ||
+                    bracket == "{" ||
+                    bracket == "(")
+                {
+                    openingBrackets.Push(bracket);
+                }
+                else if (openingBrackets.Count > 0)
+                {
+
+                    string brackets = openingBrackets.Peek() + bracket;
+
+                    if (brackets != "{}" &&
+                        brackets != "[]" &&
+                        brackets != "()")
+                    {
+                        isBalanced = false;
+                        break;
+                    }
+                    else
+                    {
+                        openingBrackets.Pop();
+                    }
+                }
+                else
+                {
+                    isBalanced = false;
+                    break;
+                }
+            }
+
+            if (isBalanced)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+
+        }
+    }
+}
