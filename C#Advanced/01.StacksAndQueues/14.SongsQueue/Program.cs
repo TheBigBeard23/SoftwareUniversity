a1f3b684f6ca22fd04af91bbc427f06b5db32606
VladislavDim <Bones4pk@gmail.com>
2022-04-10 11:26:21 +0300

    Add new exercise

--- C#Advanced/01.StacksAndQueues/14.SongsQueue/Program.cs
+++ C#Advanced/01.StacksAndQueues/14.SongsQueue/Program.cs
@@ -9,18 +9,14 @@ namespace _14.SongsQueue
         static void Main(string[] args)
         {
             Queue<string> songs = new Queue<string>(Console.ReadLine().Split(", "));
-            //string[] input = Console.ReadLine().Split().ToArray();
             string input = Console.ReadLine();
+
             while (songs.Count > 0)
             {
-                //string command = input[0];
 
                 if (input.Contains("Add"))
                 {
-                    var data = input.Split().ToArray();
-                    data[0] = "";
-                    string name = string.Join(" ", data.Where(x => x != ""));
-                    //string name = input.Substring(4, input.Length - 4);
+                    string name = input.Substring(4, input.Length - 4);
 
                     if (songs.Contains(name))
                     {
@@ -39,30 +35,6 @@ namespace _14.SongsQueue
                 {
                     songs.Dequeue();
                 }
-                //switch (command)
-                //{
-                //    case "Play":
-                //        songs.Dequeue();
-                //        break;
-
-                //    case "Add":
-                //        input[0] = "";
-                //        string name = string.Join(" ", input.Where(x => x != ""));
-
-                //        if (songs.Contains(name))
-                //        {
-                //            Console.WriteLine($"{name} is already contained!");
-                //        }
-                //        else
-                //        {
-                //            songs.Enqueue(name);
-                //        }
-                //        break;
-
-                //    case "Show":
-                //        Console.WriteLine(string.Join(", ", songs));
-                //        break;
-                //}
 
                 input = Console.ReadLine(); 
             }
---
+++ C#Advanced/01.StacksAndQueues/15.TruckTour/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _15.TruckTour
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+
+            Queue<string> data = new Queue<string>();
+
+            for (int i = 0; i < count; i++)
+            {
+                data.Enqueue(Console.ReadLine()+$" {i}");
+            }
+
+            int tankAmount = 0;
+
+            for (int i = 0; i < data.Count; i++)
+            {
+                string[] crnData = data.Dequeue().Split();
+                int fuel = int.Parse(crnData[0]);
+                int distance = int.Parse(crnData[1]);
+
+                tankAmount += fuel;
+
+                if (tankAmount >= distance)
+                {
+                    tankAmount -= distance;
+                }
+                else
+                {
+                    tankAmount = 0;
+                    i = -1;
+                }
+                data.Enqueue(string.Join(" ", crnData));
+            }
+
+            Console.WriteLine(data.Peek().Split().ToArray() [2]);
+        }
+    }
+}
