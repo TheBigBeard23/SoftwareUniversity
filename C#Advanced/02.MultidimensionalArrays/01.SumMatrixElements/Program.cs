fcb9ea933f4f3e5d94f835aed1ba79a273230f92
VladislavDim <Bones4pk@gmail.com>
2022-04-14 12:26:22 +0300

    Add new exercise

---
+++ C#Advanced/02.MultidimensionalArrays/01.SumMatrixElements/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace _01.SumMatrixElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] matrixData = Console.ReadLine().Split(", ")
+                                                 .Select(int.Parse)
+                                                 .ToArray();
+            int rows = matrixData[0];
+            int cols = matrixData[1];
+            int[,] matrix = new int[rows, cols];
+            int sum = 0;
+
+            for (int row = 0; row < rows; row++)
+            {
+                int[] numbers = Console.ReadLine().Split(", ")
+                                                  .Select(int.Parse)
+                                                  .ToArray();
+
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[row, col] = numbers[col];
+                    sum += numbers[col];
+                }
+            }
+
+            Console.WriteLine(rows);
+            Console.WriteLine(cols);
+            Console.WriteLine(sum);
+
+        }
+    }
+}
