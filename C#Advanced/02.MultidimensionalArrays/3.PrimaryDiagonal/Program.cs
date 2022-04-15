112c7b491d661f415e2ea5aa6729b36893bab7c2
VladislavDim <Bones4pk@gmail.com>
2022-04-15 12:57:14 +0300

    Add new exercise

---
+++ C#Advanced/02.MultidimensionalArrays/3.PrimaryDiagonal/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace _3.PrimaryDiagonal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int sum = 0;
+            int[,] matrix = new int[n, n];
+
+            for (int row = 0; row < n; row++)
+            {
+                int[] data = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+                for (int col = 0; col < n; col++)
+                {
+                    matrix[row, col] = data[col];
+                }
+                sum += matrix[row, row];
+            }
+
+            Console.WriteLine(sum);
+
+        }
+    }
+}
