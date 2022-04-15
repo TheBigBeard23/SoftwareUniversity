70ac60f60999640621f97efe7cf07f986f4d23de
VladislavDim <Bones4pk@gmail.com>
2022-04-15 13:11:16 +0300

    Add new exercise

---
+++ C#Advanced/02.MultidimensionalArrays/4.SymbolInMatrix/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _4.SymbolInMatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            char[,] matrix = new char[n, n];
+
+            for (int row = 0; row < n; row++)
+            {
+                string input = Console.ReadLine();
+
+                for (int col = 0; col < n; col++)
+                {
+                    matrix[row, col] = input[col];
+                }
+            }
+
+            char symbol = char.Parse(Console.ReadLine());
+
+            for (int row = 0; row < n; row++)
+            {
+                for (int col = 0; col < n; col++)
+                {
+                    if (matrix[row, col] == symbol)
+                    {
+                        Console.WriteLine($"({row}, {col})");
+                        return;
+                    }
+                }
+            }
+
+            Console.WriteLine($"{symbol} does not occur in the matrix");
+        }
+    }
+}
