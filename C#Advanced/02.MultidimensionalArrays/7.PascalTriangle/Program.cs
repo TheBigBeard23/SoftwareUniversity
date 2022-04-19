69f15120e56ce7afd48e88421563ac168b40f3dd
VladislavDim <Bones4pk@gmail.com>
2022-04-19 12:51:45 +0300

    Add new exercise

---
+++ C#Advanced/02.MultidimensionalArrays/7.PascalTriangle/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace _7.PascalTriangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            if (n == 0)
+            {
+                Console.WriteLine(n);
+                return;
+            }
+
+            long[][] matrix = new long[n][];
+
+            for (int row = 0; row < n; row++)
+            {
+                matrix[row] = new long[row + 1];
+
+                for (int col = 0; col <= row; col++)
+                {
+                    if (col == 0)
+                    {
+                        matrix[row][col] = 1;
+                    }
+                    else if (col == row)
+                    {
+                        matrix[row][col] = 1;
+                    }
+                    else
+                    {
+                        matrix[row][col] = matrix[row - 1][col] + matrix[row - 1][col - 1];
+                    }
+                }
+            }
+
+
+            for (int row = 0; row < n; row++)
+            {
+                for (int col = 0; col < matrix[row].Length; col++)
+                {
+                    Console.Write(matrix[row][col]+" ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
