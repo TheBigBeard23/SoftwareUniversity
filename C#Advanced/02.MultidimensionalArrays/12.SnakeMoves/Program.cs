09fb1fe02e9106cc3fcc1bd807610d6b9688e94f
VladislavDim <Bones4pk@gmail.com>
2022-04-21 22:15:58 +0300

    Add new exercise

---
+++ C#Advanced/02.MultidimensionalArrays/12.SnakeMoves/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Linq;
+
+namespace _12.SnakeMoves
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] parameters = Console.ReadLine().Split()
+                                                 .Select(int.Parse)
+                                                 .ToArray();
+            int rows = parameters[0];
+            int cols = parameters[1];
+
+            char[,] matrix = new char[rows, cols];
+
+            string word = Console.ReadLine();
+
+            for (int row = 0; row < rows; row++)
+            {
+                if (row % 2 == 0)
+                {
+                    for (int col = 0; col < cols; col++)
+                    {
+                        matrix[row, col] = word[0];
+                        word = word.Remove(0, 1);
+                        word += matrix[row, col];
+                    }
+                }
+
+                else
+                {
+                    for (int col = cols - 1; col >= 0; col--)
+                    {
+                        matrix[row, col] = word[0];
+                        word = word.Remove(0, 1);
+                        word += matrix[row, col];
+                    }
+                }
+
+            }
+
+
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < cols; col++)
+                {
+                    Console.Write(matrix[row,col]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
