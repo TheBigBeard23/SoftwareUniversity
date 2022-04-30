7261f496a7b5ac92bc3084355f9aa61629ea3168
VladislavDim <Bones4pk@gmail.com>
2022-04-30 15:05:48 +0300

    Add new exercise

--- C#Advanced/03.SetsAndDictionariesAdvanced/03.Largest3Numbers/Program.cs
+++ C#Advanced/03.SetsAndDictionariesAdvanced/03.Largest3Numbers/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace _03.Largest3Numbers
 {
@@ -6,7 +7,13 @@ namespace _03.Largest3Numbers
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] numbers = Console.ReadLine().Split()
+                                              .Select(int.Parse)
+                                              .OrderByDescending(x => x)
+                                              .Take(3)
+                                              .ToArray();
+
+            Console.WriteLine(string.Join(" ",numbers));
         }
     }
 }
---
+++ C#Advanced/03.SetsAndDictionariesAdvanced/04.ProductShop/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04.ProductShop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Dictionary<string, double>> shopProductsPrice = new Dictionary<string, Dictionary<string, double>>();
+
+            string[] input = Console.ReadLine().Split(", ");
+
+            while (input[0] != "Revision")
+            {
+                string shop = input[0];
+                string product = input[1];
+                double price = double.Parse(input[2]);
+
+                if (!shopProductsPrice.ContainsKey(shop))
+                {
+                    shopProductsPrice[shop] = new Dictionary<string, double>();
+                }
+
+                shopProductsPrice[shop][product] = price;
+
+                input = Console.ReadLine().Split(", ");
+            }
+
+            foreach (var shop in shopProductsPrice.OrderBy(x=>x.Key))
+            {
+                Console.WriteLine($"{shop.Key}->");
+
+                foreach (var product in shop.Value)
+                {
+                    Console.WriteLine($"Product: {product.Key}, Price: {product.Value}");
+                }
+
+            }
+        }
+    }
+}
