e987e519f0d673d8d058ea8af5e4ac334b3d6074
VladislavDim <Bones4pk@gmail.com>
2022-04-30 16:31:03 +0300

    Add new exercise

--- C#Advanced/03.SetsAndDictionariesAdvanced/07.ParkingLot/Program.cs
+++ C#Advanced/03.SetsAndDictionariesAdvanced/07.ParkingLot/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace _07.ParkingLot
 {
@@ -6,7 +7,32 @@ namespace _07.ParkingLot
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] input = Console.ReadLine().Split(", ");
+
+            HashSet<string> carsNumber = new HashSet<string>(); 
+
+            while (input[0] != "END")
+            {
+                if (input[0] == "IN")
+                {
+                    carsNumber.Add(input[1]);
+                }
+                else
+                {
+                    carsNumber.Remove(input[1]);
+                }
+
+                input = Console.ReadLine().Split(", ");
+            }
+
+            if (carsNumber.Count > 0)
+            {
+                Console.WriteLine(string.Join(Environment.NewLine,carsNumber));
+            }
+            else
+            {
+                Console.WriteLine("Parking Lot is Empty");
+            }
         }
     }
 }
---
+++ C#Advanced/03.SetsAndDictionariesAdvanced/08.SoftUniParty/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _08.SoftUniParty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            HashSet<string> guests = new HashSet<string>();
+
+            while (input != "PARTY")
+            {
+                guests.Add(input);
+                input = Console.ReadLine();
+            }
+
+            input = Console.ReadLine();
+
+            while (input != "END")
+            {
+                guests.Remove(input);
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine(guests.Count);
+
+            if (guests.Where(x => Char.IsDigit(x[0])).Count() > 0)
+            {
+                Console.WriteLine(string.Join(Environment.NewLine, guests.Where(x => Char.IsDigit(x[0]))));
+            }
+            if (guests.Where(x => Char.IsLetter(x[0])).Count() > 0)
+            {
+                Console.WriteLine(string.Join(Environment.NewLine, guests.Where(x => Char.IsLetter(x[0]))));
+            }
+
+        }
+
+    }
+}
