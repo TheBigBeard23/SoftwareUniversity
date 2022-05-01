66d9cc052f1f545b7e4c79cd07f4a4ffd665f1dc
VladislavDim <Bones4pk@gmail.com>
2022-05-01 11:12:17 +0300

    Add new exercise

---
+++ C#Advanced/03.SetsAndDictionariesAdvanced/09.UniqueUsernames/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _09.UniqueUsernames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
