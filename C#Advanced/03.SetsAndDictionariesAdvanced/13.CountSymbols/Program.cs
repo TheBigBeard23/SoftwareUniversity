9a42d4cba8ff0238129fe72bcb34438b1daee788
VladislavDim <Bones4pk@gmail.com>
2022-05-01 16:35:13 +0300

    Add new exercise

---
+++ C#Advanced/03.SetsAndDictionariesAdvanced/13.CountSymbols/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _13.CountSymbols
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<char, int> charsCount = new Dictionary<char, int>();
+            string text = Console.ReadLine();
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                char crnChar = text[i];
+
+                if (!charsCount.ContainsKey(crnChar))
+                {
+                    charsCount[crnChar] = 0;
+                }
+                charsCount[crnChar]++;
+            }
+
+            foreach (var symbol in charsCount.OrderBy(x => x.Key))
+            {
+                Console.WriteLine($"{symbol.Key}: {symbol.Value} time/s");
+            }
+        }
+    }
+}
