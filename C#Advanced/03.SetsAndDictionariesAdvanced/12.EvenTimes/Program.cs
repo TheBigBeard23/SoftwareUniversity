6abfbb4bee8798b10f74dc8b7b6e4fa45211b3fd
VladislavDim <Bones4pk@gmail.com>
2022-05-01 11:46:54 +0300

    Add new exercise

---
+++ C#Advanced/03.SetsAndDictionariesAdvanced/12.EvenTimes/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _12.EvenTimes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+            HashSet<int> numbers = new HashSet<int>();
+            int evenNum = 0;
+
+            for (int i = 0; i < count; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+
+                if (numbers.Contains(number))
+                {
+                    evenNum = number;
+                }
+
+                numbers.Add(number);
+            }
+
+            Console.WriteLine(evenNum);
+        }
+    }
+}
