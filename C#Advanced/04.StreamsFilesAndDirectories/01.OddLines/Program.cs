22664134a631f15339090bed0d17636efe3e8ba9
VladislavDim <Bones4pk@gmail.com>
2022-05-07 19:29:09 +0300

    Add new exercise

---
+++ C#Advanced/04.StreamsFilesAndDirectories/01.OddLines/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+
+namespace _01.OddLines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputFilePath = @"..\..\..\input.txt";
+            string outputFilePath = @"..\..\..\output.txt";
+
+
+            using (StreamWriter writer = new StreamWriter(inputFilePath))
+            {
+                string text = Console.ReadLine();
+
+                while (text != "")
+                {
+                    writer.WriteLine(text);
+                    text = Console.ReadLine();
+                }
+            }
+
+            using (StreamReader reader = new StreamReader(inputFilePath))
+            {
+                string text = reader.ReadLine();
+                int counter = 1;
+
+                while (text != null)
+                {
+                    if (counter % 2 == 0)
+                    {
+                        using (StreamWriter writer = new StreamWriter(outputFilePath))
+                        {
+                            writer.WriteLine(text);
+                        }
+                    }
+
+                    counter++;
+                    text = reader.ReadLine();
+                }
+            }
+
+            using (StreamReader reader = new StreamReader(outputFilePath))
+            {
+                Console.WriteLine(reader.ReadToEnd());
+            }
+        }
+    }
+}
