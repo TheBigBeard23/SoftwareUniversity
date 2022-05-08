52e29ba8736440ce8d9ebade54587759c29cf907
VladislavDim <Bones4pk@gmail.com>
2022-05-08 13:56:44 +0300

    Add new exercise

---
+++ C#Advanced/04.StreamsFilesAndDirectories/02.LineNumbers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace _02.LineNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            using (StreamReader reader = new StreamReader("../../../input.txt"))
+            {
+                using (StreamWriter writer = new StreamWriter("../../../output.txt"))
+                {
+                    string line = reader.ReadLine();
+                    int counter = 1;
+
+                    while (line != null)
+                    {
+                        writer.WriteLine($"{counter}. {line}");
+                        line = reader.ReadLine();
+                        counter++;
+                    }
+                }
+            }
+        }
+    }
+}
