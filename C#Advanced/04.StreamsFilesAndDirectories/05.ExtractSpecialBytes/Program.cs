a848dc7bedd58e40c05828bdfb55096bfe5f5bb7
VladislavDim <Bones4pk@gmail.com>
2022-05-12 12:06:03 +0300

    Add new exercise

---
+++ C#Advanced/04.StreamsFilesAndDirectories/05.ExtractSpecialBytes/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace _05.ExtractSpecialBytes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        
+            byte[] fileBytes = File.ReadAllBytes("../../../example.png");
+
+            List<string> bytesList = File.ReadAllText("../../../bytes.txt")
+                                         .Split(Environment.NewLine)
+                                         .ToList();
+            
+            using(StreamWriter writer = new StreamWriter("../../../output.bin"))
+            {
+                foreach (var item in fileBytes)
+                {
+                    if (bytesList.Contains(item.ToString()))
+                    {
+                        writer.WriteLine(item.ToString());
+                    }
+                }
+            }
+            
+        
+        }
+    }
+}
