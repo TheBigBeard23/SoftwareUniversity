e0e8e35241d4624fa325ac0c699778cd3f1a7912
VladislavDim <Bones4pk@gmail.com>
2022-05-12 16:31:17 +0300

    Add new exercise

---
+++ C#Advanced/04.StreamsFilesAndDirectories/06.SplitMergeBinaryFiles/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _06.SplitMergeBinaryFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SplitMergeBinaryFile.SplitBinaryFile("../../../example.png", "../../../part-1.bin", "../../../part-2.bin");
+            SplitMergeBinaryFile.MergeBinaryFiles("../../../part-1.bin", "../../../part-2.bin", "../../../example-joined.png");
+        }
+    }
+}
---
+++ C#Advanced/04.StreamsFilesAndDirectories/06.SplitMergeBinaryFiles/SplitMergeBinaryFile.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace _06.SplitMergeBinaryFiles
+{
+    class SplitMergeBinaryFile
+    {
+        public static void SplitBinaryFile(string sourceFilePath, string partOneFilePath, string partTwoFilePath)
+        {
+            byte[] sourceFileBytes = File.ReadAllBytes(sourceFilePath);
+            int firstFileLenght = 0;
+
+            using (StreamWriter writer = new StreamWriter(partOneFilePath))
+            {
+                if (sourceFileBytes.Length % 2 == 1)
+                {
+                    firstFileLenght = sourceFileBytes.Length / 2 + 1;
+                }
+                else
+                {
+                    firstFileLenght = sourceFileBytes.Length / 2;
+                }
+
+                for (int i = 0; i < firstFileLenght; i++)
+                {
+                    writer.Write(sourceFileBytes[i]);
+                }
+            }
+            using (StreamWriter writer = new StreamWriter(partTwoFilePath))
+            {
+                for (int i = 0; i < firstFileLenght - 1; i++)
+                {
+                    writer.Write(sourceFileBytes[i]);
+                }
+            }
+
+        }
+        public static void MergeBinaryFiles(string partOneFilePath, string partTwoFilePath, string joinedFilePath)
+        {
+            using (FileStream writerStream = new FileStream(joinedFilePath, FileMode.OpenOrCreate, FileAccess.Write))
+            {
+                using (FileStream readerStream = new FileStream(partOneFilePath, FileMode.OpenOrCreate, FileAccess.Read))
+                {
+                    readerStream.CopyTo(writerStream);
+                }
+                using (FileStream readerStream = new FileStream(partTwoFilePath, FileMode.OpenOrCreate, FileAccess.Read))
+                {
+                    readerStream.CopyTo(writerStream);
+                }
+            }
+
+        }
+    }
+}
