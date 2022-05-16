57e57e4868d66e2474ba328405c60028b5bec29d
VladislavDim <Bones4pk@gmail.com>
2022-05-16 17:48:56 +0300

    Add new exercise

---
+++ C#Advanced/04.StreamsFilesAndDirectories/10.CopyBinaryFile/CopyBinaryFile.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace _10.CopyBinaryFile
+{
+    public class CopyBinaryFile
+    {
+        public static void CopyFile(string inputFilePath, string outputFilePath)
+        {
+            using (FileStream reader = new FileStream(inputFilePath, FileMode.Open, FileAccess.Read))
+            {
+                byte[] buffer = new byte[4096];
+
+                using (FileStream writer = new FileStream(outputFilePath, FileMode.OpenOrCreate, FileAccess.Write))
+                {
+                    for (int i = 0; i < reader.Length / buffer.Length; i++)
+                    {
+                        reader.Read(buffer, 0, buffer.Length);
+                        writer.Write(buffer);
+                    }
+                }
+            }
+        }
+
+    }
+}
+
+
---
+++ C#Advanced/04.StreamsFilesAndDirectories/10.CopyBinaryFile/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _10.CopyBinaryFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CopyBinaryFile.CopyFile("../../../mario.png", "../../../mario-copy.png");
+        }
+    }
+}
