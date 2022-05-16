138307a81147c2528ccaca3495c6ddb68fd6c6aa
VladislavDim <Bones4pk@gmail.com>
2022-05-16 17:20:32 +0300

    Add new exercise

---
+++ C#Advanced/04.StreamsFilesAndDirectories/09.LineNumbers/LineNumbers.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace _09.LineNumbers
+{
+    public class LineNumbers
+    {
+        public static void ProcessLines(string inputFilePath,string outputFilePath)
+        {
+            string[] lines = File.ReadAllLines(inputFilePath);
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                string crnLine = lines[i];
+                MatchCollection marks = Regex.Matches(crnLine, "[-,.?!'\":]");
+                MatchCollection letters = Regex.Matches(crnLine, "[A-z]");
+
+                sb.AppendLine($"Line {i + 1}: {crnLine} ({letters.Count})({marks.Count})");
+            }
+
+            File.WriteAllText(outputFilePath, sb.ToString());
+        }
+    }
+}
---
+++ C#Advanced/04.StreamsFilesAndDirectories/09.LineNumbers/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _09.LineNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LineNumbers.ProcessLines("../../../text.txt", "../../../output.txt");
+        }
+    }
+}
