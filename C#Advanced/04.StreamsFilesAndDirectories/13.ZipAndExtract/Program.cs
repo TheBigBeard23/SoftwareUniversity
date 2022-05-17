12515eb75e59f4ba300b81ec9d2b85b66ae74a26
VladislavDim <Bones4pk@gmail.com>
2022-05-17 19:19:50 +0300

    Add new exercise

---
+++ C#Advanced/04.StreamsFilesAndDirectories/13.ZipAndExtract/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _13.ZipAndExtract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ZipAndExtract.ZipFileToArchive("../../../mario.png", "../../../archive.zip");
+            ZipAndExtract.ExtractFileFromArchive("../../../archive.zip", "picture.png", "../../../");
+        }
+    }
+}
---
+++ C#Advanced/04.StreamsFilesAndDirectories/13.ZipAndExtract/ZipAndExtract.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Text;
+
+namespace _13.ZipAndExtract
+{
+    class ZipAndExtract
+    {
+        public static void ZipFileToArchive(string inputFilePath, string zipArchiveFilePath)
+        {
+            using (var archive = ZipFile.Open(zipArchiveFilePath, ZipArchiveMode.Create))
+            {
+                archive.CreateEntryFromFile(inputFilePath, Path.GetFileName(inputFilePath));
+            }
+        }
+        public static void ExtractFileFromArchive(string zipArchiveFilePath, string fileName, string outputFilePath)
+        {
+            using (var archive = ZipFile.OpenRead(zipArchiveFilePath))
+            {
+                foreach (var entry in archive.Entries)
+                {
+                    entry.ExtractToFile(Path.Combine(outputFilePath, fileName));
+                }
+            }
+        }
+    }
+}
