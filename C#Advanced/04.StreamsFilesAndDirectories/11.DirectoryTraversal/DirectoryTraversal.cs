78ac598680c6483bdbf22dc4c48b763c7ab5b42b
VladislavDim <Bones4pk@gmail.com>
2022-05-17 11:09:57 +0300

    Add new exercise

---
+++ C#Advanced/04.StreamsFilesAndDirectories/11.DirectoryTraversal/DirectoryTraversal.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace _11.DirectoryTraversal
+{
+    public class DirectoryTraversal
+    {
+        public static string TraverseDirectory(string inputFolderPath)
+        {
+            Dictionary<string, Dictionary<string, double>> extensionsFilesSize = new Dictionary<string, Dictionary<string, double>>();
+            StringBuilder sb = new StringBuilder();
+
+            var files = Directory.GetFiles(inputFolderPath);
+
+            foreach (var file in files)
+            {
+                FileInfo fileInfo = new FileInfo(file);
+
+                string extension = fileInfo.Extension;
+                string name = fileInfo.Name;
+                double size = fileInfo.Length / 1024.0;
+
+                if (!extensionsFilesSize.ContainsKey(extension))
+                {
+                    extensionsFilesSize[extension] = new Dictionary<string, double>();
+                }
+                extensionsFilesSize[extension].Add(name, size);
+
+            }
+
+            foreach (var extension in extensionsFilesSize.OrderByDescending(x => x.Value.Count)
+                                                         .ThenBy(x => x.Key))
+            {
+                sb.AppendLine($"{extension.Key}");
+
+                foreach (var file in extension.Value.OrderBy(x => x.Value))
+                {
+                    sb.AppendLine($"--{file.Key} - {file.Value:f3}kb");
+                }
+
+            }
+
+            return sb.ToString();
+        }
+
+        public static void WriteReportToDesktop(string textContent,string reportFileName)
+        {
+            using (FileStream stream = new FileStream(@$"C:\Users\Bones\Desktop\{reportFileName}.txt", FileMode.OpenOrCreate, FileAccess.Write))
+            {
+                byte[] bytes = Encoding.ASCII.GetBytes(textContent);
+                stream.Write(bytes, 0, bytes.Length);
+            }
+        }
+    }
+}
---
+++ C#Advanced/04.StreamsFilesAndDirectories/11.DirectoryTraversal/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _11.DirectoryTraversal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = DirectoryTraversal.TraverseDirectory(@"C:\Users\Public\Desktop\");
+            DirectoryTraversal.WriteReportToDesktop(text, "report");
+        }
+    }
+}
