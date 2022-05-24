cff27bf897dc27e640fb1ff70c3062fde6d71432
VladislavDim <Bones4pk@gmail.com>
2022-05-24 09:38:22 +0300

    Add new exercise

---
+++ C#Advanced/05.FunctionalProgramming/08.CustomMinFunction/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace _08.CustomMinFunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                           .Split()
+                           .Select(int.Parse)
+                           .ToArray();
+
+            Func<int[], int> getMinValueDelegate = GetMinValue;
+
+            Console.WriteLine(getMinValueDelegate(numbers)); 
+        }
+        static int GetMinValue(int[] numbers)
+        {
+            int minValue = int.MaxValue;
+
+            foreach (var number in numbers)
+            {
+                if (minValue > number)
+                {
+                    minValue = number;
+                }
+            }
+
+            return minValue;
+        }
+    }
+}
