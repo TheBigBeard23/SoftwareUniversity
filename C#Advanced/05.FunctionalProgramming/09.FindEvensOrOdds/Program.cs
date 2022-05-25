0da78a27e5fa8220fbcc7e60fc0ffd5467924c62
VladislavDim <Bones4pk@gmail.com>
2022-05-25 11:35:10 +0300

    Add new exercise

---
+++ C#Advanced/05.FunctionalProgramming/09.FindEvensOrOdds/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _09.FindEvensOrOdds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+            int start = int.Parse(input[0]);
+            int end = int.Parse(input[1]);
+            string type = Console.ReadLine();
+
+            Predicate<int> checkEvenNumber = x => x % 2 == 0;
+            Predicate<int> checkOddNumber = x => x % 2 == 1;
+            Action<int,int,Predicate<int>> printerDelegate = Printer;
+
+            switch (type)
+            {
+                case "odd":
+                    printerDelegate(start, end, checkOddNumber);
+                    break;
+
+                case "even":
+                    printerDelegate(start, end, checkEvenNumber);
+                    break;
+            }
+        }
+        static void Printer(int start, int end, Predicate<int> predicate)
+        {
+            for (int i = start; i <= end; i++)
+            {
+                if (predicate(i))
+                {
+                    Console.Write($"{i} ");
+                }
+            }
+        }
+    }
+}
