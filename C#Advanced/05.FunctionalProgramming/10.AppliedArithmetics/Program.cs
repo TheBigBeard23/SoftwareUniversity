b73f3c07ffd3e08cbf1204818584bbf89e332a96
VladislavDim <Bones4pk@gmail.com>
2022-05-27 12:38:42 +0300

    add new exercise

---
+++ C#Advanced/05.FunctionalProgramming/10.AppliedArithmetics/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+
+namespace _10.AppliedArithmetics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                           .Split()
+                           .Select(int.Parse)
+                           .ToArray();
+
+            string command = Console.ReadLine();
+
+            while (command != "end")
+            {
+                GetOperation(command, numbers);
+
+                command = Console.ReadLine();
+            }
+        }
+
+        static void GetOperation(string command, int[] numbers)
+        {
+            switch (command)
+            {
+                case "add":
+                    numbers = numbers.Select(x => x + 1).ToArray();
+                    break;
+
+                case "multiply":
+                    numbers = numbers.Select(x => x * 1).ToArray();
+                    break;
+
+                case "subtract":
+                    numbers = numbers.Select(x => x - 1).ToArray();
+                    break;
+
+                case "print":
+                    Console.WriteLine(string.Join(" ", numbers));
+                    break;
+
+            }
+        }
+    }
+}
