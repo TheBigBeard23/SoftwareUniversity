15aa00b0993e39e4fd29e583538b8b8439510202
VladislavDim <Bones4pk@gmail.com>
2022-06-02 12:07:55 +0300

    Add new exercise

--- C#Advanced/05.FunctionalProgramming/12.PredicateForNames/Program.cs
+++ C#Advanced/05.FunctionalProgramming/12.PredicateForNames/Program.cs
@@ -7,9 +7,9 @@ namespace _12.PredicateForNames
         static void Main(string[] args)
         {
             int nameLength = int.Parse(Console.ReadLine());
-            string[] names = Console.ReadLine().Split();
+            string[] names = Console.ReadLine().Split(" ",StringSplitOptions.RemoveEmptyEntries);
 
-            Predicate<string> checkLength = n => n.Length == nameLength;
+            Predicate<string> checkLength = n => n.Length <= nameLength;
 
             foreach (var name in names)
             {
---
+++ C#Advanced/05.FunctionalProgramming/13.ListOfPredicates/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+
+namespace _13.ListOfPredicates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int end = int.Parse(Console.ReadLine());
+
+            int[] dividers = Console.ReadLine()
+                            .Split()
+                            .Select(int.Parse)
+                            .ToArray();
+
+            Func<int, int[], bool> CheckNumbersDelegate = CheckNumber;
+
+            for (int i = 1; i <= end; i++)
+            {
+                if (CheckNumbersDelegate(i, dividers))
+                {
+                    Console.Write($"{i} ");
+                }
+            }
+
+        }
+        static bool CheckNumber(int number,int[] dividers)
+        {
+            for (int i = 0; i < dividers.Length; i++)
+            {
+                int currentNum = dividers[i];
+
+                if (number % currentNum > 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
