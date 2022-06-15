99836c5dcf24076044fad0aa162c3420bf313be7
VladislavDim <Bones4pk@gmail.com>
2022-06-15 22:18:04 +0300

    Add new exercise

---
+++ C#Advanced/07.Generics/01.Box/Box.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BoxOfT
+{
+    class Box<T>
+    {
+        public Box()
+        {
+            Stack = new Stack<T>();
+        }
+        public Stack<T> Stack { get; set; }
+        public int Count => Stack.Count;
+        public void Add(T element)
+        {
+            Stack.Push(element);
+        }
+        public T Remove()
+        {
+            return Stack.Pop();
+        }
+
+    }
+}
