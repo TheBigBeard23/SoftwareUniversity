f9e87c8dbde062ac67dfcc1b6567bd78a2ad442e
VladislavDim <Bones4pk@gmail.com>
2022-06-19 23:56:18 +0300

    Add new exercise

---
+++ C#Advanced/07.Generics/10.Tuple/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _10.Tuple
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string[] firstInput = Console.ReadLine().Split();
+            var firstTuple = new Tuple<string, string>($"{firstInput[0]} {firstInput[1]}", firstInput[2]);
+
+            string[] secondInput = Console.ReadLine().Split();
+            var secondTuple = new Tuple<string, double>(secondInput[0], double.Parse(secondInput[1]));
+
+            string[] thirdInput = Console.ReadLine().Split();
+            var thirdTuple = new Tuple<int, double>(int.Parse(thirdInput[0]), double.Parse(thirdInput[1]));
+
+            Console.WriteLine(firstTuple.ToString()); 
+            Console.WriteLine(secondTuple.ToString());
+            Console.WriteLine(thirdTuple.ToString());
+
+        }
+    }
+}
---
+++ C#Advanced/07.Generics/10.Tuple/Tuple.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _10.Tuple
+{
+    public class Tuple<ithem1, ithem2>
+    {
+        public ithem1 FirstIthem { get; set; }
+        public ithem2 SecondIthem { get; set; }
+
+        public Tuple(ithem1 firstIthem,ithem2 secondIthem)
+        {
+            FirstIthem = firstIthem;
+            SecondIthem = secondIthem;
+        }
+
+        public override string ToString()
+        {
+            return $"{FirstIthem} -> {SecondIthem}";
+        }
+    }
+}
