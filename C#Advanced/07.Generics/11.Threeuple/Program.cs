90e5c2fc50ec5dd7c427491b866eeeeb88debe3d
VladislavDim <Bones4pk@gmail.com>
2022-06-20 00:29:34 +0300

    Add new exercise

---
+++ C#Advanced/07.Generics/11.Threeuple/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _11.Threeuple
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string[] firstInput = Console.ReadLine().Split();
+            var firstTuple = new Threeuple<string, string, string>($"{firstInput[0]} {firstInput[1]}", firstInput[2], firstInput[3]);
+
+            string[] secondInput = Console.ReadLine().Split();
+            var secondTuple = new Threeuple<string, string, bool>(secondInput[0],secondInput[1], secondInput[2] == "drunk" ? true : false);
+
+            string[] thirdInput = Console.ReadLine().Split();
+            var thirdTuple = new Threeuple<string, double, string>(thirdInput[0], double.Parse(thirdInput[1]), thirdInput[2]);
+
+            Console.WriteLine(firstTuple.ToString());
+            Console.WriteLine(secondTuple.ToString());
+            Console.WriteLine(thirdTuple.ToString());
+        }
+    }
+}
---
+++ C#Advanced/07.Generics/11.Threeuple/Threeuple.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _11.Threeuple
+{
+    public class Threeuple<ithem1, ithem2, ithem3>
+    {
+        public ithem1 FirstElement { get; set; }
+        public ithem2 SecondElement { get; set; }
+        public ithem3 ThirdElement { get; set; }
+
+        public Threeuple(ithem1 firstElement, ithem2 secondElement, ithem3 thirdElement)
+        {
+            FirstElement = firstElement;
+            SecondElement = secondElement;
+            ThirdElement = thirdElement;
+        }
+
+        public override string ToString()
+        {
+            return $"{FirstElement} -> {SecondElement} -> {ThirdElement}";
+        }
+    }
+}
