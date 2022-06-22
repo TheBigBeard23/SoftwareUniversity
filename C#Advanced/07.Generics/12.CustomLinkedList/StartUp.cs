4ea8b117cf9eb59d34c362ab7610ef0beab01c3e
VladislavDim <Bones4pk@gmail.com>
2022-06-22 11:34:28 +0300

    Add new exercise

--- C#Advanced/07.Generics/12.CustomLinkedList/StartUp.cs
+++ C#Advanced/07.Generics/12.CustomLinkedList/StartUp.cs
@@ -4,7 +4,7 @@ namespace CustomLinkedList
 {
     public class StartUp
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             var customLinkedList = new CustomLinkedList<string>();
         }
