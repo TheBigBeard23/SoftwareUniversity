a4831270d855966c9dd82e27fcab03fdf9a1fa1b
VladislavDim <Bones4pk@gmail.com>
2022-06-23 11:42:15 +0300

    Add new exercise

---
+++ C#Advanced/08.LinkedList/CustomLinkedList/CustomLinkedList/LinkedList.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomLinkedList
+{
+    public class LinkedList
+    {
+        private Node head;
+        private Node tail;
+
+        public int Count { get; private set; }
+
+        public void AddFirst(int element)
+        {
+            var newNode = new Node(element);
+
+            if (Count == 0)
+            {
+                head = tail = newNode;
+            }
+            else
+            {
+                newNode.NextNode = head;
+                head.PreviousNode = newNode;
+                head = newNode;
+            }
+
+            Count++;
+        }
+        public void AddLast(int element)
+        {
+            var newNode = new Node(element);
+
+            if (Count == 0)
+            {
+                head = tail = newNode;
+            }
+            else
+            {
+                tail.NextNode = newNode;
+                newNode.PreviousNode = tail;
+                tail = newNode;
+            }
+
+            Count++;
+        }
+        public int RemoveFirst()
+        {
+            CheckIfEmptyThrowException();
+
+            var value = head.Value;
+            head = head.NextNode;
+
+            if (head == null)
+            {
+                tail = null;
+            }
+            else
+            {
+                head.PreviousNode = null;
+            }
+            Count--;
+            return value;
+        }
+        public int RemoveLast()
+        {
+            CheckIfEmptyThrowException();
+
+            var value = tail.Value;
+            tail = tail.PreviousNode;
+
+            if (tail == null)
+            {
+                head = null;
+            }
+            else
+            {
+                tail.NextNode = null;
+            }
+            Count--;
+            return value;
+        }
+        public void ForEach(Action<int> action)
+        {
+            var crnNode = head;
+
+            while (crnNode!=null)
+            {
+                action(crnNode.Value);
+                crnNode = crnNode.NextNode;
+            }
+
+        }
+        public int[] ToArray()
+        {
+            int[] array = new int[Count];
+
+            var crnNode = head;
+            int counter = 0;
+
+            while (crnNode != null)
+            {
+                array[counter] = crnNode.Value;
+                crnNode = crnNode.NextNode;
+                counter++;
+            }
+
+            return array;
+
+        }
+        private void CheckIfEmptyThrowException()
+        {
+            if (Count == 0)
+            {
+                throw new InvalidOperationException("LinkedList is empty!");
+            }
+        }
+    }
+}
---
+++ C#Advanced/08.LinkedList/CustomLinkedList/CustomLinkedList/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace CustomLinkedList
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            LinkedList list = new LinkedList();
+
+            for (int i = 1; i <= 10; i++)
+            {
+                list.AddLast(i);
+            }
+
+            Console.WriteLine(string.Join(" ", list.ToArray()));
+
+            Console.WriteLine(list.RemoveFirst());
+            Console.WriteLine(list.RemoveLast());
+
+            list.ForEach(x => Console.Write($"{x}, "));
+            Console.WriteLine();
+
+            while (list.Count > 0)
+            {
+                Console.WriteLine(list.RemoveFirst());
+            }
+
+            list.RemoveFirst();
+        }
+    }
+}
