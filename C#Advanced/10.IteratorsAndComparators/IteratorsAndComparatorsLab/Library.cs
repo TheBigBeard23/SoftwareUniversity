362a0e35934efc3c3f5c62496e7a7ebb0522a8fe
VladislavDim <Bones4pk@gmail.com>
2022-06-26 15:11:43 +0300

    New exercise

---
+++ C#Advanced/10.IteratorsAndComparators/IteratorsAndComparatorsLab/Library.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IteratorsAndComparators
+{
+    public class Library : IEnumerable<Book>
+    {
+        private SortedSet<Book> books;
+        public Library(IEnumerable<Book> books)
+        {
+            var comparator = new BookComparator();
+            this.books = new SortedSet<Book>(books,comparator);
+        }
+        public IEnumerator<Book> GetEnumerator()
+        {
+            return new LibraryIterator(books);
+        }
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+
+    public class LibraryIterator : IEnumerator<Book>
+    {
+        private readonly List<Book> books;
+        private int currentIndex;
+
+        public LibraryIterator(IEnumerable<Book> books)
+        {
+            Reset();
+            this.books = new List<Book>(books);
+        }
+        public Book Current => books[currentIndex];
+
+        object IEnumerator.Current => Current;
+
+        public void Dispose() { }
+
+        public bool MoveNext() => ++currentIndex < books.Count;
+
+        public void Reset() => currentIndex = -1;
+
+    }
+}
---
+++ C#Advanced/10.IteratorsAndComparators/IteratorsAndComparatorsLab/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace IteratorsAndComparators
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Book[] books = new Book[]
+            {
+                new Book("The Documents in the Case", 2002, "Dorothy Sayers", "Robert Eustace"),
+                new Book("Animal Farm", 2003, "George Orwell"),
+                new Book("The Documents in the Case", 1930)
+            };
+
+
+            Library libraryTwo = new Library(books);
+
+            foreach (var book in libraryTwo)
+            {
+                Console.WriteLine(book);
+            }
+        }
+    }
+}
