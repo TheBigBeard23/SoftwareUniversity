0f01edf5bacf1d9fc67122469580a48c692cb2b3
VladislavDim <Bones4pk@gmail.com>
2022-06-26 21:09:04 +0300

    Add new exercise

---
+++ C#Advanced/10.IteratorsAndComparators/ListyIterator/ListyIterator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ListyIterator
+{
+    public class ListyIterator<T>
+    {
+        private List<T> items;
+        private int currentIndex = 0;
+        public ListyIterator()
+        {
+            items = new List<T>();
+        }
+        public ListyIterator(List<T> items)
+        {
+            this.items = items;
+        }
+        public bool Move() => ++currentIndex < items.Count;
+        public bool HasNext() => currentIndex + 1 < items.Count ? true : false;
+        public void Print()
+        {
+            if (currentIndex < items.Count)
+            {
+                Console.WriteLine(items[currentIndex]);
+            }
+            else
+            {
+                Console.WriteLine("Invalid Operation!");
+            }
+        }
+
+    }
+}
---
+++ C#Advanced/10.IteratorsAndComparators/ListyIterator/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ListyIterator
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string[] data = Console.ReadLine().Split();
+            List<string> list= new List<string>();
+
+            if (data.Length > 1)
+            {
+                for (int i = 1; i < data.Length; i++)
+                {
+                    list.Add(data[i]);
+                }
+            }
+
+            int number;
+            List<int> numberList = new List<int>();
+            if (int.TryParse(list[0], out number))
+            {
+                numberList = list.Select(int.Parse).ToList();
+                var listyIterator = new ListyIterator<int>(numberList);
+                ExecuteCommands(listyIterator);
+            }
+            else
+            {
+                var listyIterator = new ListyIterator<string>(list);
+                ExecuteCommands(listyIterator);
+            }
+
+        }
+        static void ExecuteCommands<T>(ListyIterator<T> list)
+        {
+            while (true)
+            {
+                string command = Console.ReadLine();
+
+                switch (command)
+                {
+                    case "Move":
+                        Console.WriteLine(list.Move());
+                        break;
+                    case "Print":
+                        list.Print();
+                        break;
+                    case "HasNext":
+                        Console.WriteLine(list.HasNext());
+                        break;
+                    case "END":
+                        return;
+                }
+            }
+        }
+    }
+}
