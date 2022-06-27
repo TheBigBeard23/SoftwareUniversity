8d6cf45c694c9a5a7f1cbfd7e4d7addde9923087
VladislavDim <Bones4pk@gmail.com>
2022-06-27 13:46:55 +0300

    Add new exercise:

---
+++ C#Advanced/10.IteratorsAndComparators/CustomStack/CustomStack.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ImplementCustomStack
+{
+    public class CustomStack<T> : IEnumerable<T>
+    {
+        private List<T> items;
+
+        public CustomStack()
+        {
+            items = new List<T>();
+        }
+        public CustomStack(List<T> items)
+        {
+            this.items = items;
+        }
+        public T Pop()
+        {
+            if (items.Count > 0)
+            {
+                T element = items[items.Count - 1];
+                items.RemoveAt(items.Count - 1);
+                return element;
+            }
+            else
+            {
+                Console.WriteLine("No elements");
+                return default;
+            }
+        }
+        public void Push(T element)
+        {
+            items.Add(element);
+        }
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = items.Count - 1; i >= 0; i--)
+            {
+                yield return items[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
---
+++ C#Advanced/10.IteratorsAndComparators/CustomStack/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ImplementCustomStack
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            CustomStack<int> stack = new CustomStack<int>();
+
+            var input = Console.ReadLine();
+
+            while (input != "END")
+            {
+                if (input.Contains("Pop"))
+                {
+                    stack.Pop();
+                }
+                else if (input.Contains("Push"))
+                {
+                    List<int> list = input
+                                 .Split(new string[] { "Push", ", " }, StringSplitOptions.RemoveEmptyEntries)
+                                 .Select(int.Parse)
+                                 .ToList();
+
+                    for (int i = 0; i < list.Count; i++)
+                    {
+                        stack.Push(list[i]);
+                    }
+                }
+
+                input = Console.ReadLine();
+            }
+
+            Print(stack);
+            Print(stack);
+
+        }
+        private static void Print(CustomStack<int> stack)
+        {
+            foreach (var element in stack)
+            {
+                Console.WriteLine(element);
+            }
+        }
+    }
+}
