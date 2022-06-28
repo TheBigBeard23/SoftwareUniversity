733732dc8c65701db3f47c2f63e56882d5839002
VladislavDim <Bones4pk@gmail.com>
2022-06-28 11:24:14 +0300

    Add new exercise

---
+++ C#Advanced/10.IteratorsAndComparators/Froggy/Lake.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Froggy
+{
+    public class Lake : IEnumerable<int>
+    {
+        private int[] stones;
+        public Lake(IEnumerable<int> stones)
+        {
+            this.stones = (int[])stones;
+        }
+        public IEnumerator<int> GetEnumerator()
+        {
+            for (int i = 0; i < stones.Length; i++)
+            {
+                yield return stones[i];
+            }
+        }
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+  
+    }
+}
---
+++ C#Advanced/10.IteratorsAndComparators/Froggy/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace Froggy
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var stones = Console.ReadLine()
+                         .Split(", ")
+                         .Select(int.Parse)
+                         .ToArray();
+
+            Lake lake = new Lake(stones);
+
+            int[] path = new int[stones.Length];
+            int counter = 0;
+
+            foreach (var stone in lake)
+            {
+                if (stone % 2 == 1)
+                {
+                    path[counter] = stone;
+                    counter++;
+                }
+                else
+                {
+                    path[(stones.Length - 1) - (counter - 1)] = stone;
+                }
+            }
+
+            Console.WriteLine(string.Join(", ", path));
+        }
+    }
+}
