e78e73f4d9b5eeaa8ea57e8fd5b4e97f2f251ea7
VladislavDim <Bones4pk@gmail.com>
2022-06-28 13:55:50 +0300

    Add new exercise

---
+++ C#Advanced/10.IteratorsAndComparators/EqualityLogic/Person.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace EqualityLogic
+{
+    public class Person : IComparable<Person>
+    {
+        readonly string name;
+        readonly int age;
+
+        public Person(string name,int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+
+        public int CompareTo(Person other)
+        {
+            if (this.name.CompareTo(other.name) == 0)
+            {
+                return this.age.CompareTo(other.age);
+            }
+            return this.name.CompareTo(other.name);
+        }
+
+        public override bool Equals(object obj)
+        {
+            return this.name == ((Person)obj).name && this.age == ((Person)obj).age;
+        }
+        public override int GetHashCode()
+        {
+            return this.name.GetHashCode() + this.age.GetHashCode() + (257 * 53);
+        }
+    }
+}
---
+++ C#Advanced/10.IteratorsAndComparators/EqualityLogic/StartUp.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EqualityLogic
+{
+   public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var hashSet = new HashSet<Person>();
+            var sortedSet = new SortedSet<Person>();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] personInfo = Console.ReadLine().Split();
+
+                string name = personInfo[0];
+                int age = int.Parse(personInfo[1]);
+
+                var person = new Person(name, age);
+
+                hashSet.Add(person);
+                sortedSet.Add(person);
+
+            }
+
+            Console.WriteLine(hashSet.Count);
+            Console.WriteLine(sortedSet.Count);
+
+        }
+    }
+}
