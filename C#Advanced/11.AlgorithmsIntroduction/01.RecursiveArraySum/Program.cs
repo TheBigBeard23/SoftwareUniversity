ed7e50e67906e916112d798939c9e2addf35d8d6
VladislavDim <Bones4pk@gmail.com>
2022-06-29 23:59:47 +0300

    Add new exercise

---
+++ C#Advanced/11.AlgorithmsIntroduction/01.RecursiveArraySum/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace _01.RecursiveArraySum
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console.ReadLine()
+                          .Split()
+                          .Select(int.Parse)
+                          .ToArray();
+
+            Console.WriteLine(Sum(array));
+        }
+        public static int Sum(int[] array, int index = 0)
+        {
+            if (index == array.Length)
+            {
+                return 0;
+            }
+
+            int sum = array[index];
+            index++;
+
+            sum += Sum(array,index);
+
+            return sum;
+        }
+    }
+}
