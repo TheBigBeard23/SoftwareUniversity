8fdcff995181272ff0c41c8bb4f8cec76b3d8082
VladislavDim <Bones4pk@gmail.com>
2022-06-30 00:14:35 +0300

    Add new exercise

---
+++ C#Advanced/11.AlgorithmsIntroduction/02.RecursiveFactorial/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _02.RecursiveFactorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long n = long.Parse(Console.ReadLine());
+
+            Console.WriteLine(Factorial(n));
+        }
+        static long Factorial(long n)
+        {
+            return n != 1 ? n * Factorial(n - 1) : 1;
+        }
+    }
+}
