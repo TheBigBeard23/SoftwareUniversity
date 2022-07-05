f759b28b2e4f822f5ad08079d762686475d08515
VladislavDim <Bones4pk@gmail.com>
2022-07-05 15:56:28 +0300

    Add new exercise

---
+++ C#OOP/01.Inheritance/04.RandomList/StartUp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CustomRandomList
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            RandomList list = new RandomList();
+
+            for (int i = 1; i <= 10; i++)
+            {
+                list.Add(i.ToString());
+            }
+
+            Console.WriteLine(list.RandomString());
+        }
+    }
+}
