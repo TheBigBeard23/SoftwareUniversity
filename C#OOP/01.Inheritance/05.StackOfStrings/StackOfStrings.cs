55f5c4fae157b31452eb49abf731f38e593a9fd7
VladislavDim <Bones4pk@gmail.com>
2022-07-06 20:44:56 +0300

    Add new exercise

---
+++ C#OOP/01.Inheritance/05.StackOfStrings/StackOfStrings.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomStack
+{
+    public class StackOfStrings : Stack<string>
+    {
+        private Stack<string> stack;
+        public StackOfStrings()
+        {
+            stack = new Stack<string>();
+        }
+        public StackOfStrings(string[] array)
+        {
+            stack = new Stack<string>(array);
+        }
+        public bool IsEmpty()
+        {
+            if (stack.Count != 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public Stack<string> AddRange(IEnumerable<string> collection)
+        {
+            foreach (var item in collection)
+            {
+                stack.Push(item);
+            }
+
+            return stack;
+        }
+    }
+}
---
+++ C#OOP/01.Inheritance/05.StackOfStrings/StartUp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CustomStack
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string[] array = new string[] { "gogo", "pepe", "jose" };
+
+            StackOfStrings stack = new StackOfStrings(array);
+            Console.WriteLine(stack.IsEmpty());
+
+            string[] newArr = new string[] { "pesho", "gogi", "koko" };
+
+            Console.WriteLine(string.Join(", ", stack.AddRange(newArr)));
+
+        }
+    }
+}
