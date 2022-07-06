0eec051413de0ac0ecf4f1d2b74582af1b90835d
VladislavDim <Bones4pk@gmail.com>
2022-07-06 23:09:32 +0300

    Add new exercise

---
+++ C#OOP/01.Inheritance/06.Person/Child.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _06.Person
+{
+    public class Child : Person
+    {
+        public Child(string name, int age)
+            : base(name, age)
+        {
+        }
+    }
+}
