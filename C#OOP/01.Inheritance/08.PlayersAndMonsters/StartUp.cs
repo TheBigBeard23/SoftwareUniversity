dd3ddb7abc5eee959f2c833d7102eab21ef2d8f5
VladislavDim <Bones4pk@gmail.com>
2022-07-08 12:20:51 +0300

    Add new exercise

---
+++ C#OOP/01.Inheritance/08.PlayersAndMonsters/StartUp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace PlayersAndMonsters
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var darkKnight = new DarkKnight("Jose", 33);
+
+            Console.WriteLine(darkKnight);
+        }
+    }
+}
