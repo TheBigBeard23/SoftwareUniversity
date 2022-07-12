a12bd56ef35fa097f02c7ef227d3b66b1ae034a6
VladislavDim <Bones4pk@gmail.com>
2022-07-12 14:59:24 +0300

    Edit

--- C#OOP/02.Encapsulation/07.ShoppingSpree/StartUp.cs
+++ C#OOP/02.Encapsulation/07.ShoppingSpree/StartUp.cs
@@ -16,14 +16,14 @@ namespace ShoppingSpree
 
             for (int i = 0; i < peopleData.Length; i++)
             {
+                string[] personData = peopleData[i]
+                    .Split("=", StringSplitOptions.RemoveEmptyEntries);
+
+                string name = personData[0];
+                decimal money = decimal.Parse(personData[1]);
+
                 try
                 {
-                    string[] personData = peopleData[i]
-                        .Split("=", StringSplitOptions.RemoveEmptyEntries);
-
-                    string name = personData[0];
-                    decimal money = decimal.Parse(personData[1]);
-
                     people.Add(new Person(name, money));
                 }
                 catch (Exception ex)
@@ -38,14 +38,14 @@ namespace ShoppingSpree
 
             for (int i = 0; i < productsData.Length; i++)
             {
+                string[] productData = productsData[i]
+                    .Split("=", StringSplitOptions.RemoveEmptyEntries);
+
+                string name = productData[0];
+                decimal price = decimal.Parse(productData[1]);
+
                 try
                 {
-                    string[] productData = productsData[i]
-                        .Split("=", StringSplitOptions.RemoveEmptyEntries);
-
-                    string name = productData[0];
-                    decimal price = decimal.Parse(productData[1]);
-
                     products.Add(new Product(name, price));
                 }
                 catch (Exception ex)
@@ -59,12 +59,12 @@ namespace ShoppingSpree
 
             while ((input = Console.ReadLine()) != "END")
             {
+                string[] crnArgs = input.Split();
+                string personName = crnArgs[0];
+                string productName = crnArgs[1];
+
                 try
                 {
-                    string[] crnArgs = input.Split();
-                    string personName = crnArgs[0];
-                    string productName = crnArgs[1];
-
                     people.Where(x => x.Name == personName)
                           .FirstOrDefault()
                           .Buy(products.Where(x => x.Name == productName)
