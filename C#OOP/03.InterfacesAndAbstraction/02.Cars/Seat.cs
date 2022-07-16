9bfdea15f0a8b28b63442b0c6944661a02308228
VladislavDim <Bones4pk@gmail.com>
2022-07-16 20:02:24 +0300

    Add new exercise

---
+++ C#OOP/03.InterfacesAndAbstraction/02.Cars/Seat.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cars
+{
+    public class Seat : ICar
+    {
+        public Seat(string model, string color)
+        {
+            Model = model;
+            Color = color;
+        }
+        public string Model { get; set; }
+        public string Color { get; set; }
+        public string Start()
+        {
+            return $"{Color} Seat {Model}";
+        }
+        public string Stop()
+        {
+            return "Breaaak!";
+        }
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine(Start());
+            sb.AppendLine(Stop());
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ C#OOP/03.InterfacesAndAbstraction/02.Cars/Tesla.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cars
+{
+    public class Tesla : ICar, IElectricCar
+    {
+        public Tesla(string model, string color, int battry)
+        {
+            Model = model;
+            Color = color;
+            Battery = battry;
+        }
+        public string Model { get; set; }
+        public string Color { get; set; }
+        public int Battery { get; set; }
+
+        public string Start()
+        {
+            return $"{Color} Tesla {Model} with {Battery} Batteries";
+        }
+
+        public string Stop()
+        {
+            return "Breaaak!";
+        }
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine(Start());
+            sb.AppendLine(Stop());
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
