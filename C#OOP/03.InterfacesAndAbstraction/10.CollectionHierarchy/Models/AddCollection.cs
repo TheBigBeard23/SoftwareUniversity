a760dc09f97a8a6bf8a80ecf16f4075fa5b24ea9
VladislavDim <Bones4pk@gmail.com>
2022-07-19 23:13:41 +0300

    Add new exercise

---
+++ C#OOP/03.InterfacesAndAbstraction/10.CollectionHierarchy/Models/AddCollection.cs
@@ -0,0 +1,24 @@
+﻿using CollectionHierarchy.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CollectionHierarchy.Models
+{
+    public class AddCollection : ICollection
+    {
+        public AddCollection()
+        {
+            collection = new string[100];
+        }
+        public AddCollection(string[] array)
+            : base()
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                collection[i] = array[i];
+            }
+        }
+        public string[] collection { get; private set; }
+    }
+}
