87850462b43f248a00fadc8729e9524ba60179a6
VladislavDim <Bones4pk@gmail.com>
2022-07-21 09:05:20 +0300

    Add new exercise

--- C#OOP/03.InterfacesAndAbstraction/10.CollectionHierarchy/StartUp.cs
+++ C#OOP/03.InterfacesAndAbstraction/10.CollectionHierarchy/StartUp.cs
@@ -1,6 +1,8 @@
-﻿using CollectionHierarchy.Models;
+﻿using CollectionHierarchy.Contracts;
+using CollectionHierarchy.Models;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace CollectionHierarchy
 {
@@ -13,16 +15,33 @@ namespace CollectionHierarchy
             int count = int.Parse(Console.ReadLine());
 
             collections.Add(new AddCollection());
-            collections.Add(new AddRemoveCollection());
             collections.Add(new MyList());
+            collections.Add(new AddRemoveCollection());
 
             foreach (var collection in collections)
             {
-                for (int i = 0; i < count; i++)
+                for (int i = 0; i < data.Length; i++)
                 {
                     Console.Write($"{collection.Add(data[i])} ");
                 }
                 Console.WriteLine();
+            }
+            foreach (var collection in collections)
+            {
+                try
+                {
+                    var removable = (IAddRemoveCollection)(collection);
+
+                    for (int i = 0; i < count; i++)
+                    {
+                        Console.Write($"{removable.Remove()} ");
+                    }
+                    Console.WriteLine();
+                }
+                catch (Exception)
+                {
+                    continue;
+                }
             }
         }
     }
