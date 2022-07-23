ecc12dc67bffa1ca68bc749b9f7eb2a6e6cda1a9
VladislavDim <Bones4pk@gmail.com>
2022-07-23 14:11:57 +0300

    Add new exercise

---
+++ C#OOP/04.Polymorphism/03.Shapes/StartUp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Shapes
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Shape rectangle = new Rectangle(2, 3);
+            Shape cirle = new Circle(3);
+
+            Console.WriteLine(rectangle.CalculateArea());
+            Console.WriteLine(rectangle.CalculatePerimeter());
+
+            Console.WriteLine(cirle.CalculateArea());
+            Console.WriteLine(cirle.CalculatePerimeter());
+        }
+    }
+}
