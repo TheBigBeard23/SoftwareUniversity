0d1071820fb5b9bce6780623ee21a97c5b109c05
VladislavDim <Bones4pk@gmail.com>
2022-08-13 12:48:51 +0300

    Add new exercise

--- C#OOP/06.Reflection/02.HighQualityMistakes/Spy.cs
+++ C#OOP/06.Reflection/02.HighQualityMistakes/Spy.cs
@@ -8,29 +8,6 @@ namespace Stealer
 {
     public class Spy
     {
-        public string StealFieldInfo(string typeName, params string[] fieldsNames)
-        {
-            var classType = Type.GetType($"Stealer.{typeName}");
-            var fields = classType
-              .GetFields(BindingFlags.Instance |
-                         BindingFlags.Static |
-                         BindingFlags.Public |
-                         BindingFlags.NonPublic)
-
-              .Where(x => fieldsNames.Contains(x.Name));
-
-            var classInstance = Activator.CreateInstance(classType);
-            var sb = new StringBuilder();
-
-            sb.AppendLine($"Class under investigation: {classType}");
-
-            foreach (var field in fields)
-            {
-                sb.AppendLine($"{field.Name} = {field.GetValue(classInstance)}");
-            }
-
-            return sb.ToString().TrimEnd();
-        }
         public string AnalyzeAccessModifiers(string className)
         {
             var classType = Type.GetType($"Stealer.{className}");
---
+++ C#OOP/06.Reflection/03.MissionPrivateImpossible/Spy.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Stealer
+{
+    public class Spy
+    {
+        public string RevealPrivateMethods(string investigatedClass)
+        {
+            Type classType = Type.GetType($"Stealer.{investigatedClass}");
+
+            MethodInfo[] classMethods = classType
+                .GetMethods(BindingFlags.Instance |
+                            BindingFlags.NonPublic);
+
+            var sb = new StringBuilder();
+
+            sb.AppendLine($"All Private Methods of Class: {investigatedClass}");
+            sb.AppendLine($"Base Class: {classType.BaseType.Name}");
+
+            foreach (var method in classMethods)
+            {
+                sb.AppendLine(method.Name);
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
