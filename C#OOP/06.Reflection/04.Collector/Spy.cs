456302640c8b894a68da0deac1689c31357b328d
VladislavDim <Bones4pk@gmail.com>
2022-08-13 13:01:04 +0300

    Add new exercise

---
+++ C#OOP/06.Reflection/04.Collector/Spy.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Stealer
+{
+    public class Spy
+    {
+        public string CollectGettersAndSetters(string className)
+        {
+            Type classType = Type.GetType($"Stealer.{className}");
+
+            MethodInfo[] classMethods = classType
+                .GetMethods(BindingFlags.Instance |
+                            BindingFlags.Public |
+                            BindingFlags.NonPublic);
+
+            var sb = new StringBuilder();
+
+            foreach (var method in classMethods.Where(m => m.Name.StartsWith("get")))
+            {
+                sb.AppendLine($"{method.Name} will return {method.ReturnType}");
+            }
+            foreach (var method in classMethods.Where(m => m.Name.StartsWith("set")))
+            {
+                sb.AppendLine($"{method.Name} will set field of {method.GetParameters().First().ParameterType}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
