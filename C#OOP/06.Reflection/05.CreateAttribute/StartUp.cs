5717cfa4641d1620c650e99d172110c9912374c7
VladislavDim <Bones4pk@gmail.com>
2022-08-13 13:14:01 +0300

    Add new exercise

---
+++ C#OOP/06.Reflection/05.CreateAttribute/StartUp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AuthorProblem
+{
+    [Author("Vlad")]
+    class StartUp
+    {
+        [Author("Jon")]
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
