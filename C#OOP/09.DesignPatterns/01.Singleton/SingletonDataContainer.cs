e3775b1111a46077553b6eca1ec20d8c0d4eeba4
VladislavDim <Bones4pk@gmail.com>
2022-09-11 10:32:10 +0300

    Add new exercise

---
+++ C#OOP/09.DesignPatterns/01.Singleton/SingletonDataContainer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace SingletonDemo
+{
+    public class SingletonDataContainer
+    {
+        private Dictionary<string, int> capitalsPopulation = new Dictionary<string, int>();
+
+        private static SingletonDataContainer instance = new SingletonDataContainer();
+        public static SingletonDataContainer Instance => instance;
+        public SingletonDataContainer()
+        {
+            Console.WriteLine("Initializing singleton object");
+
+            var elements = File.ReadAllLines("../../../capitals.txt");
+            for (int i = 0; i < elements.Length; i += 2)
+            {
+                capitalsPopulation.Add(elements[i], int.Parse(elements[i + 1]));
+            }
+        }
+
+        public int GetPopulation(string name)
+        {
+            return capitalsPopulation[name];
+        }
+    }
+}
---
+++ C#OOP/09.DesignPatterns/01.Singleton/StartUp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace SingletonDemo
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var db = SingletonDataContainer.Instance;
+            var db2 = SingletonDataContainer.Instance;
+            var db3 = SingletonDataContainer.Instance;
+            var db4 = SingletonDataContainer.Instance;
+
+            Console.WriteLine(db.GetPopulation("Sofia"));
+            Console.WriteLine(db2.GetPopulation("Sofia"));
+            Console.WriteLine(db3.GetPopulation("Sofia"));
+            Console.WriteLine(db4.GetPopulation("Sofia"));
+
+        }
+    }
+}
