b51dc14705824d7d148ec4e69bf2de5e75931aee
VladislavDim <Bones4pk@gmail.com>
2022-09-12 12:17:42 +0300

    Add new exercise

---
+++ C#OOP/09.DesignPatterns/03.CommandPattern/ICommand.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommandPatternDemo
+{
+    public interface ICommand
+    {
+        void ExecuteAction();
+    }
+}
---
+++ C#OOP/09.DesignPatterns/03.CommandPattern/StartUp.cs
@@ -0,0 +1,22 @@
+﻿namespace CommandPatternDemo
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var modifyPrice = new ModifyPrice();
+            var product = new Product("Izvara", 4);
+
+            Execute(product,modifyPrice, new ProductCommand(product, PriceAction.Increase, 1));
+            Execute(product, modifyPrice, new ProductCommand(product, PriceAction.Increase, 2));
+            Execute(product, modifyPrice, new ProductCommand(product, PriceAction.Decrease, 4));
+
+            System.Console.WriteLine(product);
+        }
+        private static void Execute(Product product, ModifyPrice modifyPrice, ICommand productCommand)
+        {
+            modifyPrice.SetCommand(productCommand);
+            modifyPrice.Invoke();
+        }
+    }
+}
