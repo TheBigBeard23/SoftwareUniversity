23ee820bf5245e6ef629269450a20ef78e618569
VladislavDim <Bones4pk@gmail.com>
2022-09-13 14:48:01 +0300

    Add new exercise

---
+++ C#OOP/09.DesignPatterns/04.Prototype/SandwichMenu.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PrototypeDemo
+{
+    public class SandwichMenu
+    {
+        private Dictionary<string, SandwichPrototype> sandwiches =
+            new Dictionary<string, SandwichPrototype>();
+
+        public SandwichPrototype this[string name]
+        {
+            get => sandwiches[name];
+            set => sandwiches[name] = value;
+        }
+
+
+    }
+}
---
+++ C#OOP/09.DesignPatterns/04.Prototype/StartUp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace PrototypeDemo
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var sandwichMenu = new SandwichMenu();
+
+            sandwichMenu["BLT"] = new Sandwich("Wheat", "Bacon", "", "Lettuce, Tomato");
+            sandwichMenu["PB&J"] = new Sandwich("White", "", "", "Peanut Butter, Jelly");
+            sandwichMenu["Turkey"] = new Sandwich("Rye", "Turkey", "Swiss", "Lettuce, Onion, Tomato");
+
+            Sandwich sandwichBLT = sandwichMenu["BLT"].Clone() as Sandwich;
+            Sandwich sandwichPBJ = sandwichMenu["PB&J"].Clone() as Sandwich;
+            Sandwich sandwichTurkey = sandwichMenu["Turkey"].Clone() as Sandwich;
+
+        }
+    }
+}
