440a4021dbd728f845a555ea3680ecc74b651640
VladislavDim <Bones4pk@gmail.com>
2022-09-13 22:59:50 +0300

    Add new exercise

---
+++ C#OOP/09.DesignPatterns/06.Template/StartUp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TemplateDemo
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var sourdough = new Sourdough();
+            sourdough.Make();
+
+            var twelveGrain = new TwelveGrain();
+            twelveGrain.Make();
+
+            var wholeWheat = new WholeWheat();
+            wholeWheat.Make();
+        }
+    }
+}
---
+++ C#OOP/09.DesignPatterns/06.Template/TwelveGrain.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TemplateDemo
+{
+    public class TwelveGrain : Bread
+    {
+        public override void MixIngredients()
+        {
+            Console.WriteLine("Gathering Ingreadients for 12-Grain Bread.");
+        }
+        public override void Bake()
+        {
+            Console.WriteLine("Baking the 12-Grain Bread. (25 minutes)");
+        }
+
+    }
+}
