816d366211e7aba1d5d1bed6961927ddfb0b4491
VladislavDim <Bones4pk@gmail.com>
2022-09-19 11:54:02 +0300

    Add new exercise

---
+++ JSAdvanced/01.SyntaxFunctionsAndStatements/aggregateElements.js
@@ -0,0 +1,17 @@
+function solve(numbers) {
+    console.log(aggregate(numbers, x => x));
+    console.log(aggregate(numbers, x => 1 / x));
+    console.log(aggregate(numbers, x => x.toString()).substring(1));
+
+    function aggregate(numbers, func) {
+        let result = 0;
+
+        for (let i = 0; i < numbers.length; i++) {
+
+            result += func(numbers[i]);
+        }
+
+        return result;
+    }
+}
+solve([2, 4, 8, 16])
