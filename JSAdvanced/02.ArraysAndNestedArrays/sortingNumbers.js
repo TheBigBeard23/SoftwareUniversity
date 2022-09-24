49ba40c57a495e4ca73977c2cd46bd51579dff8a
VladislavDim <Bones4pk@gmail.com>
2022-09-24 23:22:51 +0300

    Add new exercise

---
+++ JSAdvanced/02.ArraysAndNestedArrays/sortingNumbers.js
@@ -0,0 +1,13 @@
+function sort(arr) {
+
+    let result = [];
+    arr.sort((a, b) => b - a);
+
+    while (arr.length > 0) {
+        result.push(arr.pop());
+        result.push(arr.shift());
+    }
+
+    return result;
+}
+console.log(sort([1, 65, 3, 52, 48, 63, 31, -3, 18, 56]));
