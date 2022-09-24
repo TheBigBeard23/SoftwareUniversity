86e5914c106bc2f36bc65c095e2fd59cb207af19
VladislavDim <Bones4pk@gmail.com>
2022-09-24 14:23:20 +0300

    Add new exercise

--- JSAdvanced/02.ArraysAndNestedArrays/extractSubset.js
+++ JSAdvanced/02.ArraysAndNestedArrays/extractSubset.js
@@ -1,13 +1,13 @@
 function extract(arr) {
-    let n = 0;
+    let n = arr[0];
     let result = [];
     for (let i = 0; i < arr.length; i++) {
-        if (n < arr[i]) {
+        if (n <= arr[i]) {
             n = arr[i];
             result.push(n);
         }
     }
-    return arr.slice(0, n)
+    return result;
 }
 console.log(extract([
     1,
