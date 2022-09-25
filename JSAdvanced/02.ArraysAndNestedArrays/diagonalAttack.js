5b61633f9641b6e5099a7f41e7742a1fe00c3fe0
VladislavDim <Bones4pk@gmail.com>
2022-09-25 21:19:44 +0300

    Add new exercise

---
+++ JSAdvanced/02.ArraysAndNestedArrays/diagonalAttack.js
@@ -0,0 +1,14 @@
+function magicMatrices(arr) {
+
+    let sumRow = arr.map((col => col.reduce((a, b) => a + b)));
+
+    let sumCol = arr.reduce((r, a) => r.map((b, i) => a[i] + b));
+
+    let allEqual = array => array.every( v => v === array[0]);
+
+    console.log(allEqual(sumRow) && sumRow.toString() === sumCol.toString());
+
+}
+magicMatrices([[4, 5, 6],
+    [6, 5, 4],
+    [5, 5, 5]]);
---
+++ JSAdvanced/02.ArraysAndNestedArrays/ticTacToe.js
@@ -0,0 +1,74 @@
+function play(arr) {
+
+    let matrix = [
+        [false, false, false],
+        [false, false, false],
+        [false, false, false]];
+
+    let playerWin = false;
+    let player = 'X';
+    let fields = 9;
+
+    while (arr.length > 0 && !playerWin && fields > 0) {
+
+        let [row, col] = arr.shift().split(' ').map(Number);
+
+        if (!matrix[row][col]) {
+            fields--;
+            matrix[row][col] = player;
+            if (checkForWin(player)) {
+                playerWin = true;
+                break;
+            }
+            player = player == 'X' ? 'O' : 'X';
+        }
+        else {
+            console.log("This place is already taken. Please choose another!");
+        }
+
+    }
+    if (playerWin) {
+        console.log(`Player ${player} wins!`);
+    }
+    else {
+        console.log(`The game ended! Nobody wins :(`);
+    }
+    matrix.forEach(line => {
+        console.log(line.join('\t'));
+    });
+    function checkForWin(symbol) {
+
+        for (let i = 0; i < 3; i++) {
+            if (matrix[i][0] === symbol &&
+                matrix[i][1] === symbol &&
+                matrix[i][2] === symbol) {
+                return true;
+            }
+            if (matrix[0][i] === symbol &&
+                matrix[1][i] === symbol &&
+                matrix[2][i] === symbol) {
+                return true;
+            }
+        }
+
+        if ((matrix[0][0] === symbol &&
+            matrix[1][1] === symbol &&
+            matrix[2][2] === symbol) ||
+            (matrix[2][0] === symbol &&
+                matrix[1][1] === symbol &&
+                matrix[0][2] === symbol)) {
+            return true;
+        }
+    }
+}
+play([
+    "0 1",
+    "0 0",
+    "0 2",
+    "2 0",
+    "1 0",
+    "1 2",
+    "1 1",
+    "2 1",
+    "2 2",
+    "0 0"]);
