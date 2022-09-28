0d0250ddfb9212e701e6e623c7ad079139c87db4
VladislavDim <Bones4pk@gmail.com>
2022-09-28 21:52:00 +0300

    Add new exercise

---
+++ JSAdvanced/03.ObjectsAndComposition/heroicInventory.js
@@ -0,0 +1,21 @@
+function solve(arr) {
+    let result = [];
+    for (let line of arr) {
+        let [name, level, items] = line.split(' / ');
+        level = Number(level);
+        items == undefined ? items = [] : items = items.split(', ');
+        result.push({
+            name: name,
+            level: level,
+            items: items
+        })
+    }
+    console.log(JSON.stringify(result));
+}
+console.log(
+    solve([
+        'Isacc / 25 ',
+        'Derek / 12 / BarrelVest, DestructionSword',
+        'Hes / 1 / Desolator, Sentinel, Antara']
+    )
+)
