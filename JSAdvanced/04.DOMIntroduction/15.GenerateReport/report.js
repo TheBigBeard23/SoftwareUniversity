0c6d3de69c8f9ea42402f3793e1769d912454e5d
VladislavDim <Bones4pk@gmail.com>
2022-10-06 14:18:44 +0300

    Add new exercise

--- JSAdvanced/04.DOMIntroduction/15.GenerateReport/report.js
+++ JSAdvanced/04.DOMIntroduction/15.GenerateReport/report.js
@@ -1,4 +1,22 @@
 function generateReport() {
-    let tableRows = Array.from(document.getElementsByTagName('tr'));
-    console.log(tableRows);
+    let headCols = document.querySelectorAll('thead tr th');
+    let tableRows = document.querySelectorAll('tbody tr');
+    let result = [];
+    
+    for (let r = 0; r < tableRows.length; r++) {
+        let tableRowInfo = tableRows[r].cells;
+        let crnObj = {};
+
+        for (let c = 0; c < tableRowInfo.length; c++) {
+            let infoHeader = headCols[c].childNodes;
+            if (infoHeader[1].checked) {
+                crnObj[infoHeader[0].textContent.trim().toLocaleLowerCase()] = tableRowInfo[c].textContent;
+            }
+
+        }
+        result.push(crnObj);
+    }
+
+    let jsonResult = JSON.stringify(result);
+    document.getElementById('output').textContent = jsonResult;
 }
