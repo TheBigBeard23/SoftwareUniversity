0f417fd89f6bbcdc88df365f3320d956cfbf79c9
VladislavDim <Bones4pk@gmail.com>
2022-10-07 20:23:15 +0300

    Add new exercise

--- JSAdvanced/05.DOMManipulationsAndEvents/02.DeleteFromTable/app.js
+++ JSAdvanced/05.DOMManipulationsAndEvents/02.DeleteFromTable/app.js
@@ -1,3 +1,17 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+    let input = document.querySelector('label input').value;
+    let cells = document.querySelectorAll('tbody tr td');
+    let isDeleted = false;
+
+    for (let cell of cells) {
+        if (cell.textContent == input) {
+            cell.parentElement.remove();
+            document.getElementById('result').textContent = "Deleted.";
+            isDeleted = true;
+        }
+    }
+    if (!isDeleted) {
+        document.getElementById('result').textContent = "Not found.";
+    }
+    document.querySelector('label input').value = '';
 }
