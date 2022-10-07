1d770af12e3fabb9a555c446aa15bc00b9ca1425
VladislavDim <Bones4pk@gmail.com>
2022-10-07 21:33:57 +0300

    edit exercise

--- JSAdvanced/05.DOMManipulationsAndEvents/03.AddDelete/app.js
+++ JSAdvanced/05.DOMManipulationsAndEvents/03.AddDelete/app.js
@@ -1,3 +1,17 @@
 function addItem() {
-    //TODO...
+    let items = document.getElementById('items');
+    let input = document.getElementById('newItemText').value;
+
+    let a = document.createElement('a');
+    a.href = '#';
+    a.textContent = '[Delete]';
+
+    a.addEventListener('click',  (event) => {
+        event.target.parentElement.remove();
+    });
+
+    let li = document.createElement('li');
+    li.textContent = input;
+    li.appendChild(a);
+    items.appendChild(li);
 }
