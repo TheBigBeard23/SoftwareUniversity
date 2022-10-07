0500249d9462f636ee0bd699c10a6b130c4c29e3
VladislavDim <Bones4pk@gmail.com>
2022-10-07 21:44:40 +0300

    Edit exercise

--- JSAdvanced/05.DOMManipulationsAndEvents/04.MouseGradient/app.js
+++ JSAdvanced/05.DOMManipulationsAndEvents/04.MouseGradient/app.js
@@ -1,3 +1,9 @@
 function attachGradientEvents() {
-    console.log('TODO:...');
+    let gradient = document.getElementById('gradient-box');
+    let result = document.getElementById('result');
+
+    gradient.addEventListener('mousemove', (event) => {
+        let percentage = Math.trunc(event.offsetX / 300 * 100);
+        result.textContent = percentage + '%';
+    })
 }
