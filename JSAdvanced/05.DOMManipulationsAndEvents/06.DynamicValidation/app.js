2a93d1b24d11e31544884cac57a83a4118805b3b
VladislavDim <Bones4pk@gmail.com>
2022-10-08 22:00:23 +0300

    Add new exercise

--- JSAdvanced/05.DOMManipulationsAndEvents/06.DynamicValidation/app.js
+++ JSAdvanced/05.DOMManipulationsAndEvents/06.DynamicValidation/app.js
@@ -1,3 +1,16 @@
 function validate() {
-    console.log('TODO:...');
+    let input = document.getElementById('email');
+
+    input.addEventListener('change', (event) => {
+        
+        let email = event.target.value;
+        let regex = /^[a-z]+@[a-z]+\.[a-z]+$/;
+
+        if (!regex.test(email)) {
+            event.target.classList.add('error');
+        }
+        else {
+            event.target.classList.remove('error');
+        }
+    });
 }
