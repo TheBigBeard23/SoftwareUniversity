8ff21adcfa92835f928abbc7d5582a486ab9b67b
VladislavDim <Bones4pk@gmail.com>
2022-10-09 13:55:12 +0300

    Add new exercise

--- JSAdvanced/05.DOMManipulationsAndEvents/09.TimeConverter/solution.js
+++ JSAdvanced/05.DOMManipulationsAndEvents/09.TimeConverter/solution.js
@@ -1,4 +1,37 @@
 function attachEventsListeners() {
+    let daysValue = document.getElementById('days');
+    let hoursValue = document.getElementById('hours');
+    let minutesValue = document.getElementById('minutes');
+    let secondsValue = document.getElementById('seconds');
 
-    console.log('TODO:...');
+    let buttons = document.querySelector('main').addEventListener('click', (event) => {
+
+        if (event.target.type == 'button') {
+            let value = event.target.parentElement.children[1].value;
+            let typeOfInput = event.target.parentElement.children[1].id;
+            let timeInSeconds = 0;
+
+            switch (typeOfInput) {
+                case 'days':
+                    timeInSeconds = value * 86400;
+                    break;
+                case 'hours':
+                    timeInSeconds = value * 3600;
+                    break;
+                case 'minutes':
+                    timeInSeconds = value * 60;
+                    break;
+                case 'seconds':
+                    timeInSeconds = Number(value);
+                    break;
+
+            }
+
+            daysValue.value = timeInSeconds / 86400;
+            hoursValue.value = timeInSeconds / 3600;
+            minutesValue.value = timeInSeconds / 60;
+            secondsValue.value = timeInSeconds;
+        }
+
+    });
 }
--- JSAdvanced/05.DOMManipulationsAndEvents/10.LockedProfile/app.js
+++ JSAdvanced/05.DOMManipulationsAndEvents/10.LockedProfile/app.js
@@ -1,3 +1,26 @@
 function lockedProfile() {
-    console.log('TODO...')
+    document.getElementById('main').addEventListener('click', showInformation);
+
+    function showInformation(event) {
+
+        let type = event.target.type;
+
+        if (type == 'submit') {
+
+            if (event.target.parentElement.children[4].checked) {
+
+                let hiddenElement = event.target.parentElement.children[9];
+
+                if (hiddenElement.style.display == '') {
+                    hiddenElement.style = 'display:block'
+                    event.target.textContent = 'Hide it'
+                }
+                else {
+                    hiddenElement.style = 'display:'
+                    event.target.textContent = 'Show more'
+                }
+            }
+        }
+
+    }
 }
