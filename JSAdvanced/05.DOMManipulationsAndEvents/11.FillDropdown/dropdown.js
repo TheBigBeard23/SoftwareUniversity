e3e74a55dec8436d4c09edf12a05216e80ba18ae
VladislavDim <Bones4pk@gmail.com>
2022-10-09 15:21:21 +0300

    Add new exercise

--- JSAdvanced/05.DOMManipulationsAndEvents/11.FillDropdown/dropdown.js
+++ JSAdvanced/05.DOMManipulationsAndEvents/11.FillDropdown/dropdown.js
@@ -1,3 +1,15 @@
 function addItem() {
-    console.log('TODO:...');
+    let list = document.getElementById('menu');
+
+    let text = document.getElementById('newItemText');
+    let value = document.getElementById('newItemValue');
+
+    let option = document.createElement('option');
+    option.textContent = text.value;
+    option.value = value.value;
+
+    list.appendChild(option);
+    text.value = '';
+    value.value = '';
+
 }
