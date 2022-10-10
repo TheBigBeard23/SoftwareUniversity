f8d5c071f30f308b2fa074552a064b04a752f519
VladislavDim <Bones4pk@gmail.com>
2022-10-10 23:56:24 +0300

    Edit exercise

--- JSAdvanced/05.DOMManipulationsAndEvents/14.DistanceConverter/distanceConverter.js
+++ JSAdvanced/05.DOMManipulationsAndEvents/14.DistanceConverter/distanceConverter.js
@@ -1,3 +1,28 @@
 function attachEventsListeners() {
-    console.log('TODO:...');
+
+    let input = document.getElementById('inputDistance');
+    let output = document.getElementById('outputDistance');
+    let from = document.getElementById('inputUnits');
+    let to = document.getElementById('outputUnits');
+
+    let button = document.getElementById('convert').addEventListener('click', convertDistance);
+
+    function convertDistance() {
+
+        let metricUnits = {
+            km: 1000,
+            m: 1,
+            cm: 0.01,
+            mm: 0.001,
+            mi: 1609.34,
+            yrd: 0.9144,
+            ft: 0.3048,
+            in: 0.0254
+        };
+
+        let inputInMeters = input.value * metricUnits[from.value];
+        output.value = inputInMeters / metricUnits[to.value];
+
+    }
+
 }
--- JSAdvanced/05.DOMManipulationsAndEvents/15.Sudomu/solution.js
+++ JSAdvanced/05.DOMManipulationsAndEvents/15.Sudomu/solution.js
@@ -1,4 +1,70 @@
 function solve() {
+    let buttons = document.querySelector('tfoot').addEventListener('click', operations);
+    let table = document.querySelector('tbody');
+    let result = document.querySelector('#check p');
 
-    //TODO...
+    function operations(event) {
+        if (event.target.textContent == 'Quick Check') {
+
+            if (checkMatrix()) {
+                result.style.color = 'green';
+                table.parentElement.style.border = "2px solid green";
+                result.textContent = "You solve it! Congratulations!";
+            }
+            else {
+                result.style.color = 'red';
+                table.parentElement.style.border = "2px solid red";
+                result.textContent = "NOP! You are not done yet...";
+            }
+
+        } else if (event.target.textContent == 'Clear') {
+
+            let cells = table.querySelectorAll('tr td input');
+            for (let cell of cells) {
+                cell.value = '';
+            }
+            result.textContent = '';
+            table.parentElement.style.border = 'none';
+
+        }
+
+        function checkMatrix() {
+
+            let rows = [
+                [0, 0, 0],
+                [0, 0, 0],
+                [0, 0, 0]
+            ];
+            let cols = [
+                [0, 0, 0],
+                [0, 0, 0],
+                [0, 0, 0]
+            ];
+
+            let matrix = table.querySelectorAll('tr');
+
+            for (row = 0; row < matrix.length; row++) {
+
+                let crnCol = matrix[row].querySelectorAll('td input');
+
+                for (col = 0; col < crnCol.length; col++) {
+                    rows[row][col] = crnCol[col].value;
+                    cols[col][row] = crnCol[col].value;
+                }
+            }
+
+            for (let row = 0; row < rows.length; row++) {
+
+                for (let col = 0; col < rows[row].length; col++) {
+
+                    if (rows[row].filter(x => x == rows[row][col]).length > 1 ||
+                        cols[row].filter(x => x == cols[row][col]).length > 1) {
+                        return false;
+                    }
+                }
+            }
+            return true;
+
+        }
+    }
 }
