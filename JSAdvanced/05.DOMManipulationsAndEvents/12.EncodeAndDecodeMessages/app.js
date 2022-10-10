a6d622683ed655a92cd875182b55218c5a6d3a87
VladislavDim <Bones4pk@gmail.com>
2022-10-10 19:05:56 +0300

    Add new exercise

--- JSAdvanced/05.DOMManipulationsAndEvents/12.EncodeAndDecodeMessages/app.js
+++ JSAdvanced/05.DOMManipulationsAndEvents/12.EncodeAndDecodeMessages/app.js
@@ -1,3 +1,35 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+    let divs = document.querySelectorAll('div');
+
+    let sender = divs[1].querySelector('textarea');
+    let receiver = divs[2].querySelector('textarea');
+
+    document.getElementById('container').addEventListener('click', sendMessage);
+
+    function sendMessage(event) {
+        if (event.target.type == 'submit' &&
+            event.target.innerText.startsWith('Encode')) {
+
+            let message = sender.value;
+            sender.value = '';
+            message = encodeMessage(1, message);
+            receiver.value = message;
+
+        }
+        else {
+            let message = receiver.value;
+            message = encodeMessage(-1, message);
+            receiver.value = message;
+        }
+    }
+
+    function encodeMessage(encodingValue, message) {
+        let codedMessage = '';
+        for (let i = 0; i < message.length; i++) {
+            codedMessage += String.fromCharCode(message.charCodeAt(i) + encodingValue);
+        }
+        return codedMessage;
+    }
+
+
 }
--- JSAdvanced/05.DOMManipulationsAndEvents/13.Furniture/solution.js
+++ JSAdvanced/05.DOMManipulationsAndEvents/13.Furniture/solution.js
@@ -1,4 +1,47 @@
 function solve() {
+  let [generateBtn, buyBtn] = document.querySelectorAll('button');
 
-  //TODO...
+  generateBtn.addEventListener('click', generate);
+  buyBtn.addEventListener('click', buy);
+
+  function generate(event) {
+    let items = JSON.parse(document.querySelector('textarea').value);
+    let table = document.querySelector('tbody');
+
+    for (let item of items) {
+      let newItem = document.querySelector('tbody tr').cloneNode(true);
+
+      newItem.children[0].innerHTML = `<img src="${item.img}">`;
+      newItem.children[1].innerHTML = `<p>${item.name}</p>`;
+      newItem.children[2].innerHTML = `<p>${item.price}</p>`;
+      newItem.children[3].innerHTML = `<p>${item.decFactor}</p>`;
+      newItem.children[4].innerHTML = `<input type="checkbox" />`
+
+      table.appendChild(newItem);
+    }
+
+  }
+  function buy(event) {
+    let checkboxes = document.querySelectorAll('tbody input');
+    let items = [];
+    let totalPrice = 0;
+    let DecFactor = 0;
+
+    for (let checkbox of checkboxes) {
+      if (checkbox.checked) {
+        let data = checkbox.parentElement.parentElement.querySelectorAll('p');
+
+        items.push(data[0].textContent);
+        totalPrice += Number(data[1].textContent);
+        DecFactor += Number(data[2].textContent);
+      }
+    }
+
+    let output = document.querySelectorAll('textarea')[1];
+
+    output.textContent += `Bought furniture: ${items.join(', ')}\n`
+    output.textContent += `Total price: ${totalPrice.toFixed(2)}\n`
+    output.textContent += `Average decoration factor: ${DecFactor / items.length}`
+  }
+
 }
