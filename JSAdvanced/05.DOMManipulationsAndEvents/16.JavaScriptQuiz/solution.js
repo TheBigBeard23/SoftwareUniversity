2cf250fe79ad9100c9c69d2230d1831db1e06f96
VladislavDim <Bones4pk@gmail.com>
2022-10-11 19:34:46 +0300

    edit exercise

--- JSAdvanced/05.DOMManipulationsAndEvents/16.JavaScriptQuiz/solution.js
+++ JSAdvanced/05.DOMManipulationsAndEvents/16.JavaScriptQuiz/solution.js
@@ -1,3 +1,43 @@
 function solve() {
-  //TODO...
+  let questions = document.querySelectorAll('section');
+  let questionAnswer = {
+    'Question #1: Which event occurs when the user clicks on an HTML element?': 'onclick',
+    'Question #2: Which function converting JSON to string?': 'JSON.stringify()',
+    'Question #3: What is DOM?': 'A programming API for HTML and XML documents'
+  }
+  let rightAnswers = 0;
+  let questionsCounter = 0;
+
+  let buttons = document.querySelectorAll('section ul');
+  for (let button of buttons) {
+    button.addEventListener('click', next);
+  }
+
+  function next(event) {
+    if (event.target.classList.contains('answer-text')) {
+      let question = questions[questionsCounter].querySelector('.question').textContent.trim();
+      let answer = event.target.textContent;
+
+      if (questionAnswer[question] == answer) {
+        rightAnswers++;
+      }
+      questions[questionsCounter].style.display = 'none';
+      questionsCounter++;
+
+      if (questionsCounter == Object.keys(questionAnswer).length) {
+
+        document.getElementById('results').style.display = 'block';
+        let result = document.querySelector('.results-inner h1');
+
+        if (rightAnswers == Object.keys(questionAnswer).length) {
+          result.textContent = 'You are recognized as top JavaScript fan!';
+        }
+        else {
+          result.textContent = `You have ${rightAnswers} right answers`;
+        }
+        return;
+      }
+      questions[questionsCounter].style.display = 'block';
+    }
+  }
 }
