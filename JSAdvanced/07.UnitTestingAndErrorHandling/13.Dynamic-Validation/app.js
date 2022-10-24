85a00d07bed7b40cc20a874308369415e970af28
VladislavDim <Bones4pk@gmail.com>
2022-10-25 00:47:37 +0300

    Add new exercise

--- JSAdvanced/07.UnitTestingAndErrorHandling/13.Dynamic-Validation/app.js
+++ JSAdvanced/07.UnitTestingAndErrorHandling/13.Dynamic-Validation/app.js
@@ -1,3 +1,11 @@
 function validate() {
-    // TODO
+
+    let input = document.querySelector('#email').addEventListener('change', (event) => {
+        if (/^[a-z]+@[a-z]+.[a-z]+$/.test(event.target.value)) {
+            event.target.classList.remove('error');
+        }
+        else {
+            event.target.classList.add('error');
+        }
+    });
 }
