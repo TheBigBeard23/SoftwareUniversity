30f1aa45bef1b41eb426f4a73d716d719274f24c
VladislavDim <Bones4pk@gmail.com>
2022-10-24 23:26:46 +0300

    Add new exercise

---
+++ JSAdvanced/07.UnitTestingAndErrorHandling/12.Notification/app.js
@@ -0,0 +1,11 @@
+function notify(message) {
+
+  let div = document.querySelector('#notification');
+  div.textContent = message;
+  div.style.display = 'block'
+
+  div.addEventListener('click', () => {
+    div.style.display = 'none'
+  })
+
+}
