3d05d30471f84d662a7e17be99e3d511a8f29e65
VladislavDim <Bones4pk@gmail.com>
2022-10-29 13:42:03 +0300

    Add new exercise

--- JSAdvanced/08.ClassesAndAttributes/11. View-Model/app.js
+++ JSAdvanced/08.ClassesAndAttributes/11. View-Model/app.js
@@ -1,8 +1,29 @@
 class Textbox {
-    //TODO
+    constructor(selector, regex) {
+        this._elements = document.querySelectorAll(selector);
+        this._invalidSymbols = regex;
+        [...this._elements].forEach(x => x.addEventListener('change', () => this.value = x.value));
+    }
+
+    get value() {
+        return this._value;
+    }
+    set value(crnValue) {
+        this._value = crnValue;
+        Array.from(this._elements).forEach(x => x.value = crnValue);
+    }
+
+    get elements() {
+        return this._elements;
+    }
+
+    isValid() {
+        return !this._invalidSymbols.test(this.value);
+    }
 }
 
-let textbox = new Textbox(".textbox",/[^a-zA-Z0-9]/);
-let inputs = document.getElementsByClassName('.textbox');
+let textbox = new Textbox(".textbox", /[^a-zA-Z0-9]/);
+let inputs = document.getElementsByClassName('textbox');
 
-inputs.addEventListener('click',function(){console.log(textbox.value);});
+Array.from(inputs)
+  .forEach(el => el.addEventListener('click', function () { console.log(el.value); }));
