9b27752e74abb9ca5c9764683d93472de73945f8
VladislavDim <Bones4pk@gmail.com>
2022-11-04 23:33:41 +0200

    Add new exercise

--- JSApplications/01.AsynchronousProgramming/01.XHR/scripts/app.js
+++ JSApplications/01.AsynchronousProgramming/01.XHR/scripts/app.js
@@ -1,3 +1,12 @@
 function loadRepos() {
-   console.log("TODO...");
+   const url = 'https://api.github.com/users/testnakov/repos';
+   const httpRequest = new XMLHttpRequest();
+
+   httpRequest.addEventListener('readystatechange', () => {
+      if (httpRequest.readyState == 4) {
+         document.getElementById('res').textContent = httpRequest.responseText;
+      }
+   });
+   httpRequest.open('GET', url);
+   httpRequest.send();
 }
