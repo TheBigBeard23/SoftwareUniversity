6272b45eec8fa8c0600589c66c4b9505fc367448
VladislavDim <Bones4pk@gmail.com>
2022-11-05 00:04:07 +0200

    Add new exercise

--- JSApplications/01.AsynchronousProgramming/02.Github-Repos/scripts/app.js
+++ JSApplications/01.AsynchronousProgramming/02.Github-Repos/scripts/app.js
@@ -1,3 +1,25 @@
 function loadRepos() {
-	console.log("TODO...");
+	const username = document.getElementById('username').value;
+	const list = document.getElementById('repos');
+
+	let response = fetch(`https://api.github.com/users/${username}/repos`);
+
+	response
+		.then(res => res.json())
+		.then(repos => {
+
+			list.innerHTML = '';
+
+			for (const repo of repos) {
+				list.innerHTML += `<li>
+						<a href="${repo.html_url}" target="_blank">
+								${repo.full_name}
+						</a>
+						</li>`;
+			}
+		})
+		.catch(err => {
+			list.innerHTML = err;
+		});
+
 }
