b648f9b154273a813c090bf2b7a3a3814a4bb748
VladislavDim <Bones4pk@gmail.com>
2022-11-08 11:07:00 +0200

    Add new exercis

--- JSApplications/02.AsynchronousProgramming/08.Locked-Profile/app.js
+++ JSApplications/02.AsynchronousProgramming/08.Locked-Profile/app.js
@@ -1,3 +1,44 @@
 function lockedProfile() {
-    console.log('TODO...')
+
+    const main = document.querySelector('#main');
+    const div = document.querySelector('.profile');
+    const url = `http://localhost:3030/jsonstore/advanced/profiles`;
+
+    fetch(url)
+        .then(res => res.json())
+        .then(res => {
+
+            for (let obj of Object.values(res)) {
+
+                let newDiv = div.cloneNode(true);
+                let [id, username, email, age] = Object.values(obj);
+
+                newDiv.querySelector('[name="user1Username"]').value = username;
+                newDiv.querySelector('[name="user1Email"]').value = email;
+                newDiv.querySelector('[name="user1Age"]').value = age;
+                newDiv.querySelector('[name="user1Age"]').type = 'email';
+                newDiv.querySelector('input[value="lock"]').name = id;
+                newDiv.querySelector('input[value="unlock"]').name = id;
+
+                let moreInfo = newDiv.querySelector('.user1Username');
+                let btn = newDiv.querySelector('button');
+                moreInfo.style.display = 'none';
+
+                btn.addEventListener('click', (event) => {
+
+                    const unlock = newDiv.querySelector('input[value="unlock"]').checked;
+
+                    if (unlock && btn.textContent === 'Show more') {
+                        moreInfo.style.display = 'block'
+                        btn.textContent = 'Hide it';
+                    } else {
+                        moreInfo.style.display = 'none'
+                        btn.textContent = 'Show more';
+                    }
+                })
+                main.appendChild(newDiv);
+            }
+            div.remove();
+        })
+        .catch();
 }
