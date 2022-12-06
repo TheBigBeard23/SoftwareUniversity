8da7c9a909cb8dd15a9beef7377873c45ac5d395
VladislavDim <Bones4pk@gmail.com>
2022-12-06 23:35:28 +0200

    Add new exercie

---
+++ JSAdvanced/09.PrototypesAndInheritance/stringExtension.js
@@ -0,0 +1,39 @@
+(function stringExtension() {
+    String.prototype.ensureStart = function (str) {
+        if (!this.startsWith(str)) {
+            return str + this.toString();
+        }
+        return this.toString();
+    }
+    String.prototype.ensureEnd = function (str) {
+        if (!this.endsWith(str)) {
+            return this.toString() + str;
+        }
+        return this.toString();
+    }
+    String.prototype.isEmpty = function () {
+        return this.length === 0;
+    }
+    String.prototype.truncate = function (n) {
+
+        if (this.length <= n) {
+            return this.toString();
+        }
+        if (n < 4) {
+            return '.'.repeat(n);
+        }
+
+        const lastIndex = this.substring(0, n - 2).lastIndexOf(' ');
+
+        return lastIndex
+            ? this.substring(0, lastIndex) + '...'
+            : this.substring(0, n - 3) + '...';
+    }
+    String.format = function (string, ...params) {
+        while (params.length) {
+            string = string.replace(/{\d+}/, params.shift());
+        }
+        return string;
+    };
+})();
+
