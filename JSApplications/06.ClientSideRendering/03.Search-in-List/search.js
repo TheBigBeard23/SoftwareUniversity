1949cc6c15099765bbc5fc069ad7e9d942b7bb02
VladislavDim <Bones4pk@gmail.com>
2023-01-04 22:52:43 +0200

    Add new exercise

--- JSApplications/06.ClientSideRendering/03.Search-in-List/search.js
+++ JSApplications/06.ClientSideRendering/03.Search-in-List/search.js
@@ -1,3 +1,25 @@
-function search() {
-   // TODO
+import { html, render } from '../node_modules/lit-html/lit-html.js';
+import { towns } from './towns.js';
+
+const list = document.querySelector('#towns');
+let searchText;
+
+const listTamplate = (towns) => html`
+<ul>
+   ${towns.map(town => town.includes(searchText) && searchText
+      ? html`<li class="active">${town}</li>`
+      : html`<li>${town}</li>`)}
+</ul>
+`;
+
+render(listTamplate(towns), list);
+
+document.querySelector('article').addEventListener('click', search);
+
+function search(event) {
+   event.preventDefault();
+   if (event.target.tagName == 'BUTTON') {
+      searchText = document.querySelector('#searchText').value;
+      render(listTamplate(towns), list);
+   }
 }
