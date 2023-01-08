231c00015fd4c1e53f4f1bc937ee224d9c2365f8
VladislavDim <Bones4pk@gmail.com>
2023-01-08 22:44:16 +0200

    Add new exercise

--- JSApplications/06.ClientSideRendering/05.Table-Search/solution.js
+++ JSApplications/06.ClientSideRendering/05.Table-Search/solution.js
@@ -1,8 +1,52 @@
-function solve() {
-   document.querySelector('#searchBtn').addEventListener('click', onClick);
+import { html, render } from '../node_modules/lit-html/lit-html.js';
 
-   function onClick() {
-      //   TODO:
+const input = document.querySelector('#searchField');
+const tbody = document.querySelector('tbody');
 
+const tableRowTemplate = (item) =>
+html`
+${input.value && checkItem(item) 
+? html`<tr class="select">
+   <td>${item.firstName} ${item.lastName}</td>
+   <td>${item.email}</td>
+   <td>${item.course}</td>
+</tr>`
+: html `
+<tr>
+   <td>${item.firstName} ${item.lastName}</td>
+   <td>${item.email}</td>
+   <td>${item.course}</td>
+</tr>`}
+
+`;
+
+
+update();
+document.querySelector('#searchBtn').addEventListener('click', onClick);
+
+
+function onClick() {
+   if (input.value) {
+      update();
    }
+}
+function checkItem(item) {
+   let isIncludes = false;
+   Object.values(item).map(x => {
+      if (x.toLowerCase().includes(input.value.toLowerCase())) {
+         isIncludes = true;
+      }
+   });
+   
+   return isIncludes;
+}
+async function update() {
+   const data = await getData();
+   render(Object.values(data).map(tableRowTemplate), tbody);
+   input.value='';
+}
+async function getData() {
+   const res = await fetch('http://localhost:3030/jsonstore/advanced/table');
+   const data = await res.json();
+   return data;
 }
