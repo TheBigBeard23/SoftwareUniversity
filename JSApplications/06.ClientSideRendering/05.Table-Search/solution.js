dc45193ba847ffdb5b876255939833016e0f9b1f
VladislavDim <Bones4pk@gmail.com>
2023-01-09 17:06:16 +0200

    Add new exercise

--- JSApplications/06.ClientSideRendering/05.Table-Search/solution.js
+++ JSApplications/06.ClientSideRendering/05.Table-Search/solution.js
@@ -3,33 +3,25 @@ import { html, render } from '../node_modules/lit-html/lit-html.js';
 const input = document.querySelector('#searchField');
 const tbody = document.querySelector('tbody');
 
-const tableRowTemplate = (item) =>
-html`
-${input.value && search(item) 
-? html`<tr class="select">
-   <td>${item.firstName} ${item.lastName}</td>
-   <td>${item.email}</td>
-   <td>${item.course}</td>
-</tr>`
-: html `
-<tr>
-   <td>${item.firstName} ${item.lastName}</td>
-   <td>${item.email}</td>
-   <td>${item.course}</td>
-</tr>`}
+const cellsTamplate = (item) => html`
+<td>${item.firstName} ${item.lastName}</td>
+<td>${item.email}</td>
+<td>${item.course}</td>
 `;
 
+const tableRowTemplate = (item) =>
+   input.value && search(item)
+      ? html`<tr class="select">${cellsTamplate(item)}</tr>`
+      : html`<tr>${cellsTamplate(item)}</tr>`;
 
 update();
 document.querySelector('#searchBtn').addEventListener('click', onClick);
-
 
 function onClick() {
    if (input.value) {
       update();
    }
 }
-
 function search(item) {
 
    let isIncludes = false;
@@ -39,19 +31,17 @@ function search(item) {
          isIncludes = true;
       }
    });
-   
+
    return isIncludes;
 }
-
 async function update() {
 
    const data = await getData();
 
    render(Object.values(data).map(tableRowTemplate), tbody);
 
-   input.value='';
+   input.value = '';
 }
-
 async function getData() {
 
    const res = await fetch('http://localhost:3030/jsonstore/advanced/table');
