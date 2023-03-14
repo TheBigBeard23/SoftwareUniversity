2ae94078bbbdb676eaf66f4b27ab73eaf471f1da
VladislavDim <Bones4pk@gmail.com>
2023-03-14 16:41:11 +0200

    like functionality

--- JSApplications/ExamPrep/Music Library/src/api/data.js
+++ JSApplications/ExamPrep/Music Library/src/api/data.js
@@ -5,7 +5,10 @@ const endpoints = {
     'createAlbum': 'data/albums',
     'albumById': (id) => `data/albums/${id}`,
     'editAlbum': (id) => `data/albums/${id}`,
-    'deleteAlbum': (id) => `data/albums/${id}`
+    'deleteAlbum': (id) => `data/albums/${id}`,
+    'likesCount': (id) => `data/likes?where=albumId%3D%22${id}%22&distinct=_ownerId&count`,
+    'userLikes': (albumId, userId) => `data/likes?where=albumId%3D%22${albumId}%22%20and%20_ownerId%3D%22${userId}%22&count`,
+    'addLike': 'data/likes'
 }
 
 export async function getAllAlbums() {
@@ -22,4 +25,13 @@ export async function editAlbum(id, data) {
 }
 export async function deleteAlbum(id) {
     return await api.del(endpoints.deleteAlbum(id));
+}
+export async function getLikesCount(id) {
+    return await api.get(endpoints.likesCount(id));
+}
+export async function getUserLikes(albumId, userId) {
+    return await api.get(endpoints.userLikes(albumId, userId));
+}
+export async function addLike(albumId) {
+    return await api.post(endpoints.addLike, { albumId });
 }
--- JSApplications/ExamPrep/Music Library/src/views/detailsView.js
+++ JSApplications/ExamPrep/Music Library/src/views/detailsView.js
@@ -1,10 +1,10 @@
-import { deleteAlbum, getAlbum } from "../api/data.js";
+import { addLike, deleteAlbum, getAlbum, getLikesCount, getUserLikes } from "../api/data.js";
 import { html, nothing } from "../lit.js";
 
 let ctx;
 let album;
 
-const detailsTemplate = () => html`
+const detailsTemplate = async () => html`
     <section id="details">
         <div id="details-wrapper">
             <p id="details-title">Album Details</p>
@@ -20,16 +20,14 @@ const detailsTemplate = () => html`
                 <p><strong>Label:</strong><span id="details-label">${album.label}</span></p>
                 <p><strong>Sales:</strong><span id="details-sales">${album.sales}</span></p>
             </div>
-            <div id="likes">Likes: <span id="likes-count">0</span></div>
+            <div id="likes">Likes: <span id="likes-count">${await getLikesCount(album._id)}</span></div>
     
             <!--Edit and Delete are only for creator-->
             ${ctx.user
         ? html`
             <div id="action-buttons">
                 ${ctx.user._id !== album._ownerId
-                ? html`
-                <a href="" id="like-btn">Like</a>
-                `
+                ? await likeOption()
                 : html`
                 <a href="/edit/${album._id}" id="edit-btn">Edit</a>
                 <a @click=${onDelete} href="javascript:void(0)" id="delete-btn">Delete</a>
@@ -44,9 +42,20 @@ const detailsTemplate = () => html`
 export async function showDetails(context) {
     ctx = context;
     album = await getAlbum(ctx.params.id)
-    ctx.render(detailsTemplate());
+    ctx.render(await detailsTemplate());
+}
+async function likeOption() {
+    const like = await getUserLikes(album._id, ctx.user._id);
+    console.log(like);
+    if (like == 0) {
+        return html`<a @click=${onLike} href="javascript:void(0)" id="like-btn">Like</a>`;
+    }
 }
 async function onDelete() {
     await deleteAlbum(album._id);
     ctx.page.redirect('/catalog');
+}
+function onLike() {
+    addLike(album._id);
+    ctx.page.redirect(ctx.page.current);
 }
