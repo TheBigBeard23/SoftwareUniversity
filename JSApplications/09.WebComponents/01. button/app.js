31b9d7c83e8e52cc52043dacc49bfae61e5ddc2f
VladislavDim <Bones4pk@gmail.com>
2023-04-01 18:31:21 +0300

    add new exercise

---
+++ JSApplications/09.WebComponents/01. button/app.js
@@ -0,0 +1,19 @@
+
+const tmp = document.getElementById('app-button');
+
+class MyButton extends HTMLElement {
+    constructor() {
+        super();
+
+        this.attachShadow({ mode: "open" });
+
+    }
+
+    connectedCallback() {
+        this.shadowRoot.appendChild(tmp.content.cloneNode(true));
+        this.shadowRoot.querySelector('button').textContent = this.getAttribute('text');
+        this.shadowRoot.querySelector('button').classList.add(this.getAttribute('type'));
+    }
+}
+
+window.customElements.define('app-button', MyButton);
