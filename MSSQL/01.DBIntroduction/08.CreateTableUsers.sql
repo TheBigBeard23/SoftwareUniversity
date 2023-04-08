25f6a65b5a382e4d9569dde357362d7bd557448b
VladislavDim <Bones4pk@gmail.com>
2023-04-08 14:02:05 +0300

    Add new exercise

---
+++ MSSQL/01.DBIntroduction/08.CreateTableUsers.sql
@@ -0,0 +1,10 @@
+
+CREATE TABLE [Users](
+[Id] BIGINT PRIMARY KEY IDENTITY,
+[Username] VARCHAR(30) UNIQUE NOT NULL,
+[Password] VARCHAR(26) NOT NULL,
+[ProfilePicture] VARBINARY(MAX),
+CHECK (DATALENGTH([ProfilePicture]) <= 900000),
+[LastLoginTime] DATETIME2,
+[IsDeleted] BIT NOT NULL
+)
