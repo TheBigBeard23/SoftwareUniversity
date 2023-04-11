6b697929e707b5ac476d9661c2a07e4b1efc6325
VladislavDim <Bones4pk@gmail.com>
2023-04-12 00:02:36 +0300

    add new exercise

--- MSSQL/01.DBIntroduction/14.CarRentalDatabase.sql
+++ MSSQL/01.DBIntroduction/14.CarRentalDatabase.sql
@@ -6,11 +6,26 @@ USE [CarRental]
 
 GO
 
-CREATE TABLE [Directors] (
+CREATE TABLE [Categories] (
 [Id]			INT PRIMARY KEY IDENTITY (1, 1),
 [CategoryName]  NVARCHAR(50) NOT NULL,
 [DailyRate]		DECIMAL NOT NULL,
 [WeeklyRate]	DECIMAL NOT NULL,
 [MonthlyRate]	DECIMAL NOT NULL,
 [WeekendRate]	DECIMAL NOT NULL
+)
+
+GO
+
+CREATE TABLE [Cars] (
+[Id]			INT PRIMARY KEY IDENTITY (1, 1),
+[PlateNumber]   NVARCHAR(10) NOT NULL,
+[Manufacturer]	NVARCHAR(20) NOT NULL,
+[Model]			NVARCHAR(10) NOT NULL,
+[CarYear]		DATETIME2 NOT NULL,
+[CategoryId]	INT FOREIGN KEY REFERENCES [Categories](Id),
+[Doors]			INT,
+[Picture]		VARBINARY(MAX),
+[Condition]		NVARCHAR(10),
+[Available]		BIT
 )
