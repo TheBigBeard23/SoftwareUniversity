f80f287cad9834105bb56a824b2b2ce25318c6ce
VladislavDim <Bones4pk@gmail.com>
2023-04-19 19:04:08 +0300

    ADd new exercise

--- MSSQL/02.CRUD/06.FindEmailAddressOfEachEmployee.sql
+++ MSSQL/02.CRUD/06.FindEmailAddressOfEachEmployee.sql
@@ -1,5 +1,6 @@
 USE [SoftUni]
 
 SELECT 
-CONCAT([FirstName], '.', [LastName], '@softuni.bg') AS [Full Email Address]
-FROM [Employees]
+CONCAT([FirstName], '.', [LastName], '@softuni.bg') 
+	AS [Full Email Address]
+  FROM [Employees]
