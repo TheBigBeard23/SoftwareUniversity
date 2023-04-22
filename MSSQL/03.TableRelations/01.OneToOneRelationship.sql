73d94e9a174ea734b7bce518e95add44574b306c
VladislavDim <Bones4pk@gmail.com>
2023-04-22 18:12:02 +0300

    Add new exercise

---
+++ MSSQL/03.TableRelations/01.OneToOneRelationship.sql
@@ -0,0 +1,27 @@
+
+
+CREATE TABLE Passports(
+PassportID INT PRIMARY KEY,
+PassportNumber VARCHAR(10) UNIQUE NOT NULL)
+
+GO
+
+CREATE TABLE Persons(
+PersonID    INT PRIMARY KEY IDENTITY,
+FirstName   NVARCHAR(10) NOT NULL,
+Salary	    DECIMAL(10,2) NOT NULL,
+PassportID	INT UNIQUE FOREIGN KEY REFERENCES Passports(PassportID))
+
+GO
+
+INSERT INTO Passports(PassportID, PassportNumber)
+VALUES      (101, 'N34FG21B'),
+            (102, 'K65LO4R7'),
+	        (103, 'ZE657QP2')
+
+GO
+
+INSERT INTO Persons(FirstName, Salary, PassportID)
+VALUES      ('Roberto', 43300.00, 102),
+            ('Tom', 56100.00, 103),
+            ('Yana', 60200.00, 101)
