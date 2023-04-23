ec3a8a06043592e6f1073f0b9b4a765d623bb88e
VladislavDim <Bones4pk@gmail.com>
2023-04-23 22:42:57 +0300

    Add new exercise

--- MSSQL/03.TableRelations/03.ManyToManyRelationship.sql
+++ MSSQL/03.TableRelations/03.ManyToManyRelationship.sql
@@ -15,3 +15,27 @@ CREATE TABLE StudentsExams(
 [StudentID] INT FOREIGN KEY REFERENCES Students([StudentID]) NOT NULL,
 [ExamID]	INT FOREIGN KEY REFERENCES Exams([ExamID]) NOT NULL,
 PRIMARY KEY(StudentID, ExamID))
+
+GO
+
+INSERT INTO Students([Name])
+VALUES ('Mila'),
+	   ('Toni'),
+	   ('Ron')
+
+GO
+
+INSERT INTO Exams([Name])
+VALUES ('SpringMVC'),
+	   ('Neo4j'),
+	   ('Oracle 11g')
+
+GO
+
+INSERT INTO StudentsExams([StudentID],[ExamID])
+VALUES		(1, 101),
+			(1, 102),
+			(2, 101),
+			(3, 103),
+			(2, 102),
+			(2, 103)
