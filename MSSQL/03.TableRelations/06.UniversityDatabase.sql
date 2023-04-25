cac031c300578484ce77679f2155214f4422cde3
VladislavDim <Bones4pk@gmail.com>
2023-04-25 19:34:11 +0300

    Add new exercise

---
+++ MSSQL/03.TableRelations/06.UniversityDatabase.sql
@@ -0,0 +1,44 @@
+
+CREATE DATABASE University
+
+GO
+
+USE University
+
+GO
+
+CREATE TABLE Majors(
+MajorID		INT PRIMARY KEY IDENTITY,
+MajorName	VARCHAR(50)	NOT NULL)
+
+GO
+
+CREATE TABLE Students(
+StudentID		INT PRIMARY KEY IDENTITY (101, 1),
+StudentNumber	INT			NOT NULL,
+StudentName		VARCHAR(50) NOT NULL,
+MajorID			INT FOREIGN KEY REFERENCES Majors(MajorID) NOT NULL)
+
+GO
+
+CREATE TABLE Payments(
+PaymentID		INT PRIMARY KEY IDENTITY (1001, 1),
+PaymentDate		DATETIME2	   NOT NULL,
+PaymentAmount	DECIMAL(10, 2) NOT NULL,
+StudentID		INT FOREIGN KEY 
+				REFERENCES Students(StudentID) NOT NULL)
+
+GO
+
+CREATE TABLE Subjects(
+SubjectID		INT PRIMARY KEY IDENTITY (10001, 1),
+SubjectName		VARCHAR(50)	NOT NULL)
+
+GO
+
+CREATE TABLE Agenda(
+StudentID		INT FOREIGN KEY 
+				REFERENCES Students(StudentID) NOT NULL,
+SubjectID		INT FOREIGN KEY
+				REFERENCES Subjects(SubjectID) NOT NULL,
+				PRIMARY KEY(StudentID, SubjectID))
