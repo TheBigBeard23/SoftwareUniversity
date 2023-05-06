db29543c426aaddc62a3926636323078d444f724
VladislavDim <Bones4pk@gmail.com>
2023-05-06 12:44:43 +0300

    Add new exercise

--- MSSQL/05.SubqueriesAndJoins/08.Employee24.sql
+++ MSSQL/05.SubqueriesAndJoins/08.Employee24.sql
@@ -1,13 +1,13 @@
-Use SoftUni
-SELECT e.EmployeeID
-		,e.FirstName
-		,CASE
-			WHEN p.StartDate >= CAST('2005' AS DATE) THEN NULL
-			ELSE p.Name 
-		END AS ProjectName
-FROM Employees AS e
+      Use SoftUni
+   SELECT e.EmployeeID
+         ,e.FirstName
+         ,CASE
+              WHEN p.StartDate >= CAST('2005' AS DATE) THEN NULL
+              ELSE p.Name 
+          END AS ProjectName
+     FROM Employees AS e
 LEFT JOIN EmployeesProjects AS ep
-ON e.EmployeeID = ep.EmployeeID
+       ON e.EmployeeID = ep.EmployeeID
 LEFT JOIN Projects AS p
-ON ep.ProjectID = p.ProjectID
-WHERE e.EmployeeID = 24
+       ON ep.ProjectID = p.ProjectID
+    WHERE e.EmployeeID = 24
