69e85d6667c2825d4225278d2c5dd5601347eadf
VladislavDim <Bones4pk@gmail.com>
2023-05-06 12:53:59 +0300

    Add new exercise

---
+++ MSSQL/05.SubqueriesAndJoins/09.EmployeeManager.sql
@@ -0,0 +1,11 @@
+
+
+  SELECT e.EmployeeID
+         ,e.FirstName
+	     ,e.ManagerID
+	     ,em.FirstName
+    FROM Employees AS e
+    JOIN Employees AS em
+      ON em.EmployeeID = e.ManagerID
+   WHERE e.ManagerID IN (3, 7)
+ORDER BY e.EmployeeID
