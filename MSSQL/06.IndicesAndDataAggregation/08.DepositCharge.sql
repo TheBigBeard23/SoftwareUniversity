07b37e7a1a151f78131c94be941ec74a6f89e6ea
VladislavDim <Bones4pk@gmail.com>
2023-05-13 20:57:41 +0300

    Add new exercise

---
+++ MSSQL/06.IndicesAndDataAggregation/08.DepositCharge.sql
@@ -0,0 +1,10 @@
+     USE Gringotts
+  SELECT wd.DepositGroup
+         ,wd.MagicWandCreator
+	     ,MIN(wd.DepositCharge)
+      AS MinDepositCharge
+    FROM WizzardDeposits AS wd
+GROUP BY wd.DepositGroup
+         ,wd.MagicWandCreator
+ORDER BY wd.MagicWandCreator
+         ,wd.DepositGroup
