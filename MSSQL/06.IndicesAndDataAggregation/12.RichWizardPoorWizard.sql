01bc1560fbb533c2647d6f1b3440b90b193ee5f3
Dimitrov Vladislav <64010169+VladislavDim@users.noreply.github.com>
2023-05-14 11:34:22 +0300

    Update 12.RichWizardPoorWizard.sql

--- MSSQL/06.IndicesAndDataAggregation/12.RichWizardPoorWizard.sql
+++ MSSQL/06.IndicesAndDataAggregation/12.RichWizardPoorWizard.sql
@@ -1,8 +1,8 @@
 SELECT SUM(d.[Difference])
   FROM (SELECT wd.DepositAmount - (SELECT DepositAmount
                                      FROM WizzardDeposits 
-									   AS wdn
-						            WHERE wdn.id = wd.id + 1) 
-									   AS [Difference]
+				       AS wdn
+				    WHERE wdn.id = wd.id + 1) 
+				       AS [Difference]
           FROM WizzardDeposits AS wd) 
-		    AS d
+            AS d
