35a5c2ce504f0ff3c2bcb6771667d145869d3bc8
Dimitrov Vladislav <64010169+VladislavDim@users.noreply.github.com>
2023-05-14 23:44:10 +0300

    edit formatting

--- MSSQL/06.IndicesAndDataAggregation/19.SalaryChallenge.sql
+++ MSSQL/06.IndicesAndDataAggregation/19.SalaryChallenge.sql
@@ -1,16 +1,16 @@
 SELECT TOP(10) e.FirstName
               ,e.LastName
-	          ,e.DepartmentID
+	      ,e.DepartmentID
       FROM (SELECT e.DepartmentID
-	              ,AVG(e.Salary) 
-				AS AvgSalary
-	          FROM Employees 
-			    AS e
-	      GROUP BY e.DepartmentID) 
-		AS a,
-	       Employees 
-		AS e
-	 WHERE e.DepartmentID = a.DepartmentID
-	   AND e.Salary > a.AvgSalary
+	          ,AVG(e.Salary) 
+		AS AvgSalary
+	      FROM Employees 
+      	        AS e
+	  GROUP BY e.DepartmentID) 
+	AS a,
+	   Employees 
+        AS e
+     WHERE e.DepartmentID = a.DepartmentID
+       AND e.Salary > a.AvgSalary
   ORDER BY e.DepartmentID
 	            
