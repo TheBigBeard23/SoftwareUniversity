e4c2b8697977e11a103c24e9451983cdf797cdc1
VladislavDim <Bones4pk@gmail.com>
2023-05-14 11:13:51 +0300

    Add new exercise

---
+++ MSSQL/06.IndicesAndDataAggregation/11.AverageInterest.sql
@@ -0,0 +1,12 @@
+
+
+  SELECT wd.DepositGroup
+        ,wd.IsDepositExpired
+        ,AVG(wd.DepositInterest)
+    FROM WizzardDeposits 
+	  AS wd
+   WHERE wd.DepositStartDate > '01/01/1985'
+GROUP BY wd.DepositGroup
+        ,wd.IsDepositExpired
+ORDER BY wd.DepositGroup DESC
+        ,wd.IsDepositExpired
