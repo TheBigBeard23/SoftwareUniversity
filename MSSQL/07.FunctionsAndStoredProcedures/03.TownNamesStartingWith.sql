259358aabf4c2f8e997ccd3287b81ccc14204851
VladislavDim <Bones4pk@gmail.com>
2023-05-15 14:02:30 +0300

    Add new exercise

---
+++ MSSQL/07.FunctionsAndStoredProcedures/03.TownNamesStartingWith.sql
@@ -0,0 +1,10 @@
+CREATE OR ALTER PROC usp_GetTownsStartingWith(@str VARCHAR(50))
+    AS
+ BEGIN
+	  SELECT [Name] 
+	    FROM Towns
+	   WHERE [Name] LIKE @str + '%'
+  END
+   GO
+
+ EXEC usp_GetTownsStartingWith S
