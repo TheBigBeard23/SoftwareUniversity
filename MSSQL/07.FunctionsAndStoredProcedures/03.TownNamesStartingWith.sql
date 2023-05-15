46513c717e54f8579d28a11c8d1968120c941ea0
VladislavDim <Bones4pk@gmail.com>
2023-05-15 14:04:15 +0300

    Add new exercise

--- MSSQL/07.FunctionsAndStoredProcedures/03.TownNamesStartingWith.sql
+++ MSSQL/07.FunctionsAndStoredProcedures/03.TownNamesStartingWith.sql
@@ -1,10 +1,10 @@
 CREATE OR ALTER PROC usp_GetTownsStartingWith(@str VARCHAR(50))
     AS
  BEGIN
-	  SELECT [Name] 
-	    FROM Towns
-	   WHERE [Name] LIKE @str + '%'
-  END
-   GO
+	   SELECT [Name] 
+	     FROM Towns
+	    WHERE [Name] LIKE @str + '%'
+   END
+    GO
 
- EXEC usp_GetTownsStartingWith S
+  EXEC usp_GetTownsStartingWith S
