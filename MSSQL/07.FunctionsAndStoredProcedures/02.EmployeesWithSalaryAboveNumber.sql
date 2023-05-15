0593c64f0be8dd245a195fbf63c829ae5123dd88
VladislavDim <Bones4pk@gmail.com>
2023-05-15 13:51:02 +0300

    Add new exercise

---
+++ MSSQL/07.FunctionsAndStoredProcedures/02.EmployeesWithSalaryAboveNumber.sql
@@ -0,0 +1,13 @@
+CREATE OR ALTER PROC usp_GetEmployeesSalaryAboveNumber(@number DECIMAL(18,4))
+    AS
+ BEGIN
+      SELECT e.FirstName
+	        ,e.LastName
+	    FROM Employees 
+	      AS e
+	   WHERE e.Salary > @number
+   END
+    GO
+
+  EXEC usp_GetEmployeesSalaryAboveNumber 80000
+
