31fc8acb3ebfe272c1a487df422f1912436bd3aa
VladislavDim <Bones4pk@gmail.com>
2023-05-17 15:12:55 +0300

    Add new exercise

---
+++ MSSQL/07.FunctionsAndStoredProcedures/12.CalculateInterest.sql
@@ -0,0 +1,17 @@
+CREATE OR ALTER PROC usp_CalculateFutureValueForAccount(@accountId INT, @iRate FLOAT)
+AS
+BEGIN
+		SELECT  a.Id AS [Account Id]
+			   ,ah.FirstName AS [First Name]
+			   ,ah.LastName AS [Last Name]
+			   ,a.Balance AS [Current Balance]
+			   ,dbo.ufn_CalculateFutureValue(a.Balance, @iRate, 5) AS [Balance in 5 years]
+	      FROM Accounts AS a
+          JOIN AccountHolders AS ah
+            ON a.AccountHolderId = ah.Id
+		 WHERE a.Id = @accountId
+
+END
+GO
+
+EXEC usp_CalculateFutureValueForAccount 1, 0.1
