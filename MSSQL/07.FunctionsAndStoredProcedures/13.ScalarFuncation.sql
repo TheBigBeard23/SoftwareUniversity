5928e6afed0dfdcb44a8afa9df6e1d2081db9282
VladislavDim <Bones4pk@gmail.com>
2023-05-17 15:36:35 +0300

    Add new exericise

---
+++ MSSQL/07.FunctionsAndStoredProcedures/13.ScalarFuncation.sql
@@ -0,0 +1,17 @@
+CREATE FUNCTION ufn_CashInUsersGames(@gameName)
+RETURNS TABLE
+AS
+RETURN
+	SELECT SUM(r.Cash) AS SumCash
+	FROM
+		(SELECT g.Name
+			  ,ug.Cash
+			  ,ROW_NUMBER() OVER(PARTITION BY @gameName ORDER BY ug.Cash DESC) AS RowNumber
+		FROM Games AS g
+		JOIN UsersGames AS ug
+		ON g.Id = ug.GameId
+		WHERE g.Name = @gameName) AS r
+	WHERE r.RowNumber % 2 != 0
+
+
+	
