d4f30cd34538b62742848dbf41ec36eed326334b
Dimitrov Vladislav <64010169+VladislavDim@users.noreply.github.com>
2023-05-17 12:18:22 +0300

    Update 06.EmployeesBySalaryLevel.sql

--- MSSQL/07.FunctionsAndStoredProcedures/06.EmployeesBySalaryLevel.sql
+++ MSSQL/07.FunctionsAndStoredProcedures/06.EmployeesBySalaryLevel.sql
@@ -2,7 +2,7 @@ CREATE OR ALTER PROC usp_EmployeesBySalaryLevel(@level VARCHAR(10))
     AS
  BEGIN
       SELECT e.FirstName
-	        ,e.LastName
+	    ,e.LastName
 	    FROM Employees 
 		  AS e
 	   WHERE dbo.ufn_GetSalaryLevel(e.Salary) = @level
