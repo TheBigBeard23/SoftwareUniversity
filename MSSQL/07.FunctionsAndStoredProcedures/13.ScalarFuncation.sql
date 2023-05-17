4bdcd9731fd437d87db83f9438035b35f7022aa4
Dimitrov Vladislav <64010169+VladislavDim@users.noreply.github.com>
2023-05-17 15:37:59 +0300

    Update 13.ScalarFuncation.sql

--- MSSQL/07.FunctionsAndStoredProcedures/13.ScalarFuncation.sql
+++ MSSQL/07.FunctionsAndStoredProcedures/13.ScalarFuncation.sql
@@ -3,15 +3,15 @@ RETURNS TABLE
 AS
 RETURN
 	SELECT SUM(r.Cash) AS SumCash
-	FROM
+	  FROM
 		(SELECT g.Name
-			  ,ug.Cash
-			  ,ROW_NUMBER() OVER(PARTITION BY @gameName ORDER BY ug.Cash DESC) AS RowNumber
-		FROM Games AS g
-		JOIN UsersGames AS ug
-		ON g.Id = ug.GameId
-		WHERE g.Name = @gameName) AS r
-	WHERE r.RowNumber % 2 != 0
+		       ,ug.Cash
+		       ,ROW_NUMBER() OVER(PARTITION BY @gameName ORDER BY ug.Cash DESC) AS RowNumber
+		   FROM Games AS g
+		   JOIN UsersGames AS ug
+		     ON g.Id = ug.GameId
+		  WHERE g.Name = @gameName) AS r
+	 WHERE r.RowNumber % 2 != 0
 
 
 	
