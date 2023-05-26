34818a2a6950372e1211b5db0943fb6cea261de4
VladislavDim <Bones4pk@gmail.com>
2023-05-26 20:39:39 +0300

    Add new exercise

---
+++ EntityFrameworkCore/01.AdoDotNet/01.InitialSetup/Configuration.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.InitialSetup
+{
+    public static class Configuration
+    {
+         public const string ConnectionString = "";
+    }
+}
