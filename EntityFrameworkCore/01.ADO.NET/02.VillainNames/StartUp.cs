5e76664e844e8c5f74b296429af7080b3c567b3a
VladislavDim <Bones4pk@gmail.com>
2023-05-28 16:04:40 +0300

    Add new exercise

---
+++ EntityFrameworkCore/01.ADO.NET/02.VillainNames/StartUp.cs
@@ -0,0 +1,26 @@
+﻿using _01.InitialSetup;
+using Microsoft.Data.SqlClient;
+
+namespace _02.VillainNames
+{
+    public class StartUp
+    {
+        static async Task Main(string[] args)
+        {
+            await using SqlConnection connection =
+                new SqlConnection(Configuration.GetConnectionString("MinionsDB"));
+            await connection.OpenAsync();
+
+            SqlCommand sqlCommand = 
+                new SqlCommand(SqlQueries.GetVillainAndMinionsCount,connection);    
+            SqlDataReader reader = await sqlCommand.ExecuteReaderAsync();
+
+            while (reader.Read())
+            {
+                string name = (string)reader["Name"];
+                int minionsCount = (int)reader["MinionsCount"];
+                Console.WriteLine($"{name} - {minionsCount}");
+            }
+        }
+    }
+}
