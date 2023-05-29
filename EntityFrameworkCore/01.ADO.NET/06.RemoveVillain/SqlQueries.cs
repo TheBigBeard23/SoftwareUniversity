3cb6eb411eaeeecd030373486b6ed156b26584aa
VladislavDim <Bones4pk@gmail.com>
2023-05-29 19:56:58 +0300

    Add new exercise

---
+++ EntityFrameworkCore/01.ADO.NET/06.RemoveVillain/SqlQueries.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06.RemoveVillain
+{
+    public static class SqlQueries
+    {
+        public const string GetVillain = @"
+        SELECT Name 
+          FROM Villains 
+         WHERE Id = @villainId";
+
+        public const string DeleteMinionsFromVillain = @"
+        DELETE FROM MinionsVillains 
+         WHERE VillainId = @villainId";
+
+        public const string DeleteVillain = @"
+        DELETE FROM Villains
+         WHERE Id = @villainId"; 
+    }
+}
---
+++ EntityFrameworkCore/01.ADO.NET/06.RemoveVillain/StartUp.cs
@@ -0,0 +1,59 @@
+﻿using _01.InitialSetup;
+using Microsoft.Data.SqlClient;
+
+namespace _06.RemoveVillain
+{
+    public class StartUp
+    {
+        static async Task Main(string[] args)
+        {
+            SqlConnection connection =
+                new SqlConnection(Configuration.GetConnectionString("MinionsDB"));
+            await connection.OpenAsync();
+
+            int villainId = int.Parse(Console.ReadLine());
+
+            string? villain = await GetVillain(villainId, connection);
+
+            if (villain != null)
+            {
+                await DeleteVillainMinions(villain, villainId, connection);
+            }
+            else
+            {
+                Console.WriteLine("No such villain was found.");
+            }
+
+        }
+
+        private static async Task DeleteVillainMinions(string villain, int villainId, SqlConnection connection)
+        {
+            SqlCommand deleteVillainCmd = new SqlCommand(SqlQueries.DeleteMinionsFromVillain, connection);
+            deleteVillainCmd.Parameters.AddWithValue("@villainId", villainId);
+
+            int minionsCount = await deleteVillainCmd.ExecuteNonQueryAsync();
+
+            await DeleteVillain(villainId, connection);
+
+            Console.WriteLine($"{villain} was deleted.");
+            Console.WriteLine($"{minionsCount} minions were released.");
+        }
+
+        private static async Task DeleteVillain(int villainId, SqlConnection connection)
+        {
+            SqlCommand deleteVillainCmd = new SqlCommand(SqlQueries.DeleteVillain, connection);
+            deleteVillainCmd.Parameters.AddWithValue("villainId", villainId);
+
+            await deleteVillainCmd.ExecuteNonQueryAsync();
+        }
+
+        private static async Task<string?> GetVillain(int villainId, SqlConnection connection)
+        {
+            SqlCommand getVillainCmd = new SqlCommand(SqlQueries.GetVillain, connection);
+            getVillainCmd.Parameters.AddWithValue("@villainId", villainId);
+
+            return (string?)await getVillainCmd.ExecuteScalarAsync();
+
+        }
+    }
+}
