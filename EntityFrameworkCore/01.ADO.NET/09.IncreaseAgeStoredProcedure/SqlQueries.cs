6c745620b2f2c35f922d9f5a4c8b984cc54d1c25
VladislavDim <Bones4pk@gmail.com>
2023-05-29 22:26:34 +0300

    Add new exercise:

---
+++ EntityFrameworkCore/01.ADO.NET/09.IncreaseAgeStoredProcedure/SqlQueries.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09.IncreaseAgeStoredProcedure
+{
+    public static class SqlQueries
+    {
+        public const string GetMinion = @"SELECT Name, Age FROM Minions WHERE Id = @Id";
+
+        public const string IncreaseMinionAge = "EXEC usp_GetOlder @Id";
+    }
+}
---
+++ EntityFrameworkCore/01.ADO.NET/09.IncreaseAgeStoredProcedure/StartUp.cs
@@ -0,0 +1,45 @@
+﻿using _01.InitialSetup;
+using Microsoft.Data.SqlClient;
+
+namespace _09.IncreaseAgeStoredProcedure
+{
+    public class StartUp
+    {
+        static async Task Main(string[] args)
+        {
+            SqlConnection connection =
+                new SqlConnection(Configuration.GetConnectionString("MinionsDB"));
+            await connection.OpenAsync();
+
+            int minionId = int.Parse(Console.ReadLine());
+
+            await IncreaseMinionAge(minionId, connection);
+            await PrintMinionInfo(connection, minionId);
+
+        }
+
+        private static async Task PrintMinionInfo(SqlConnection connection, int minionId)
+        {
+            SqlCommand getMinionCmd = new SqlCommand(SqlQueries.GetMinion, connection);
+            getMinionCmd.Parameters.AddWithValue("@Id", minionId);
+
+            SqlDataReader reader = await getMinionCmd.ExecuteReaderAsync();
+
+            reader.Read();
+
+            string name = (string)reader["Name"];
+            int age = (int)reader["Age"];
+
+            Console.WriteLine($"{name} – {age} years old");
+        }
+
+        private static async Task IncreaseMinionAge(int minionId, SqlConnection connection)
+        {
+            SqlCommand increaseMinionAgeCmd =
+                new SqlCommand(SqlQueries.IncreaseMinionAge, connection);
+            increaseMinionAgeCmd.Parameters.AddWithValue("@Id", minionId);
+
+            await increaseMinionAgeCmd.ExecuteNonQueryAsync();
+        }
+    }
+}
