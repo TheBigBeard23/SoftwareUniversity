13f5ae7ca37c4551fc934356a0faf9cca527abd5
VladislavDim <Bones4pk@gmail.com>
2023-06-07 22:56:37 +0300

    Add new exercise

---
+++ EntityFrameworkCore/03.EntityFrameworkIntro/04.EmployeesWithSalaryOver50000/StartUp.cs
@@ -0,0 +1,37 @@
+﻿using _01.DatabaseFirst.Data;
+using _01.DatabaseFirst.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Text;
+
+namespace _04.EmployeesWithSalaryOver50000;
+
+public class StartUp
+{
+    static async Task Main(string[] args)
+    {
+        SoftUniContext context = new SoftUniContext();
+        string result = await GetEmployeesWithSalaryOver50000(context);
+        Console.WriteLine(result);
+
+    }
+    public static async Task<string> GetEmployeesWithSalaryOver50000(SoftUniContext context)
+    {
+        using (context)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            List<Employee> employees = await context.Employees
+                .Where(e => e.Salary > 50000)
+                .OrderBy(e => e.FirstName)
+                .ToListAsync();
+
+            foreach (var employee in employees)
+            {
+                sb.AppendLine($"{employee.FirstName} - {employee.Salary:F2}");
+            }
+
+            return sb.ToString().Trim();
+        }
+
+    }
+}
