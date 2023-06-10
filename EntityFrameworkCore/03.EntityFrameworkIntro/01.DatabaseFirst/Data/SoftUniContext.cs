c01629473c925587c7f5f4d6c22ecf8540e2fa7f
VladislavDim <Bones4pk@gmail.com>
2023-06-10 19:34:05 +0300

    Add new exercise

--- EntityFrameworkCore/03.EntityFrameworkIntro/01.DatabaseFirst/Data/SoftUniContext.cs
+++ EntityFrameworkCore/03.EntityFrameworkIntro/01.DatabaseFirst/Data/SoftUniContext.cs
@@ -27,7 +27,7 @@ namespace _01.DatabaseFirst.Data
         {
             if (!optionsBuilder.IsConfigured)
             {
-                optionsBuilder.UseSqlServer("Server=.;User Id=sa;Password=***;Database=SoftUni;TrustServerCertificate=True");
+                optionsBuilder.UseSqlServer("Server=.;User Id=sa;Password=5048Vlad;Database=SoftUni;TrustServerCertificate=True");
             }
         }
 
---
+++ EntityFrameworkCore/03.EntityFrameworkIntro/06.AddingANewAddressAndUpdatingEmployee/StartUp.cs
@@ -0,0 +1,57 @@
+﻿using _01.DatabaseFirst.Data;
+using _01.DatabaseFirst.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace _06.AddingANewAddressAndUpdatingEmployee
+{
+    public class StartUp
+    {
+        static async Task Main(string[] args)
+        {
+            SoftUniContext context = new SoftUniContext();
+            await AddNewAddressToEmployee(context);
+        }
+
+        private async static Task AddNewAddressToEmployee(SoftUniContext context)
+        {
+            const string AddressText = "Vitoshka 1";
+            const int TownId = 4;
+
+            Address newAddress = new Address();
+            newAddress.AddressText = AddressText;
+            newAddress.TownId = TownId;
+
+            using (context.Database.BeginTransaction())
+            {
+                context.Addresses.Add(newAddress);
+
+                Employee crnEmployee = await context.Employees
+                    .Where(e => e.LastName == "Nakov")
+                    .FirstOrDefaultAsync();
+
+                crnEmployee.Address = newAddress;
+                context.SaveChanges();
+
+                var addresses = await context.Employees
+                    .OrderByDescending(e => e.AddressId)
+                    .Select(e => e.Address.AddressText)
+                    .Take(10)
+                    .ToListAsync();
+
+
+
+                PrintResult(addresses);
+
+                context.Database.RollbackTransactionAsync();
+            }
+        }
+
+        private static void PrintResult(List<string> addresses)
+        {
+            foreach (var a in addresses)
+            {
+                Console.WriteLine(a);
+            }
+        }
+    }
+}
