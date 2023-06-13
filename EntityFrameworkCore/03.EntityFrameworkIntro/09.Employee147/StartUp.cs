b08201761eb733aa2467e2a508b3e22e8eb84537
VladislavDim <Bones4pk@gmail.com>
2023-06-13 13:01:35 +0300

    Add new exercise

---
+++ EntityFrameworkCore/03.EntityFrameworkIntro/09.Employee147/StartUp.cs
@@ -0,0 +1,42 @@
+﻿using _01.DatabaseFirst.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Text;
+
+namespace _09.Employee147
+{
+    public class StartUp
+    {
+        public static async Task Main(string[] args)
+        {
+            SoftUniContext context = new SoftUniContext();
+            string result = await GetEmployee147(context);
+            Console.WriteLine(result);
+        }
+
+        private static async Task<string> GetEmployee147(SoftUniContext context)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            using (context)
+            {
+                var employee147 = await context.Employees
+                    .Where(e => e.EmployeeId == 147)
+                    .Select(e => new
+                    {
+                        FirstName = e.FirstName,
+                        LastName = e.LastName,
+                        JobTitle = e.JobTitle,
+                        Projects = e.Projects
+                    })
+                    .FirstOrDefaultAsync();
+
+                sb.AppendLine($"{employee147.FirstName} {employee147.LastName} - {employee147.JobTitle}");
+                foreach (var p in employee147.Projects.OrderBy(p => p.Name))
+                {
+                    sb.AppendLine($"{p.Name}");
+                }
+            }
+            return sb.ToString().Trim();
+        }
+    }
+}
