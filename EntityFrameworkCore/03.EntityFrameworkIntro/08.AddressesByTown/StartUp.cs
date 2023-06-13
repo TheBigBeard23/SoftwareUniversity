5c0bcd2a6c2e8dd7293f4443cfd4ab281ae5218f
VladislavDim <Bones4pk@gmail.com>
2023-06-13 12:36:12 +0300

    Add new exercise

---
+++ EntityFrameworkCore/03.EntityFrameworkIntro/08.AddressesByTown/StartUp.cs
@@ -0,0 +1,45 @@
+﻿using _01.DatabaseFirst.Data;
+using _01.DatabaseFirst.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Text;
+
+namespace _08.AddressesByTown
+{
+    public class StartUp
+    {
+        public static async Task Main(string[] args)
+        {
+            SoftUniContext context = new SoftUniContext();
+            string result = await GetAddressesByTown(context);
+            Console.WriteLine(result);  
+        }
+
+        private static async Task<string> GetAddressesByTown(SoftUniContext context)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            using (context)
+            {
+                var addresses = await context.Addresses
+                    .Select(a => new
+                    {
+                        Text = a.AddressText,
+                        Town = a.Town.Name,
+                        EmployeesCount = a.Employees.Count
+                    })
+                    .OrderByDescending(a => a.EmployeesCount)
+                    .ThenBy(a => a.Town)
+                    .ThenBy(a => a.Text)
+                    .Take(10)
+                    .ToListAsync();
+
+                foreach (var a in addresses)
+                {
+                    sb.AppendLine($"{a.Text}, {a.Town} - {a.EmployeesCount} employees");
+                }
+
+                return sb.ToString().Trim();
+            }
+        }
+    }
+}
