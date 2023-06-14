203bbd9c1b477385a6e115d0006e501c2b8efc94
VladislavDim <Bones4pk@gmail.com>
2023-06-14 11:26:48 +0300

    Add new exercise

---
+++ EntityFrameworkCore/03.EntityFrameworkIntro/11.FindLatest10Projects/StartUp.cs
@@ -0,0 +1,47 @@
+﻿using _01.DatabaseFirst.Data;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel;
+using System.Text;
+
+namespace _11.FindLatest10Projects
+{
+    public class StartUp
+    {
+        static async Task Main(string[] args)
+        {
+            SoftUniContext context = new SoftUniContext();
+            string result = await GetLatestProjects(context);
+            await Console.Out.WriteLineAsync(result);
+        }
+
+        private static async Task<string> GetLatestProjects(SoftUniContext context)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            using (context)
+            {
+                var projects = await context.Projects
+                    .OrderByDescending(p => p.StartDate)
+                    .Select(p => new
+                    {
+                        Name = p.Name,
+                        Description = p.Description,
+                        StartDate = p.StartDate
+                    })
+                    .Take(10)
+                    .ToListAsync();
+
+                foreach (var p in projects.OrderBy(p => p.Name))
+                {
+                    sb.AppendLine($"{p.Name}");
+                    sb.AppendLine($"{p.Description}");
+                    sb.AppendLine($"{p.StartDate}");
+                    sb.AppendLine("- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -");
+                }
+
+            }
+            return sb.ToString().Trim();
+
+        }
+    }
+}
---
+++ JSApplications/09.WebComponents/test/test.js
@@ -0,0 +1,5 @@
+function sum(a, v) {
+    console.log(a);
+    console.log(v);
+}
+sum(2, 3);
