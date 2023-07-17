6aefe96999feb7d3e45843974731ad71d6441282
unknown <Bones4pk@gmail.com>
2023-07-17 21:46:11 +0300

    Add new exercise

---
+++ EntityFrameworkCore/08.JSONProcessing/ProductShop/Data/Configuration.cs
@@ -0,0 +1,8 @@
+﻿namespace ProductShop.Data
+{
+    public static class Configuration
+    {
+        public const string ConnectionString =
+            @$"Server=.;User Id=sa;Password=Vlad5048;Database=ProductShop;TrustServerCertificate=True";
+    }
+}
---
+++ EntityFrameworkCore/08.JSONProcessing/ProductShop/ProductShopProfile.cs
@@ -0,0 +1,16 @@
+﻿
+namespace ProductShop
+{
+    using AutoMapper;
+    using ProductShop.DTOs.Import;
+    using ProductShop.Models;
+
+    public class ProductShopProfile : Profile
+    {
+        public ProductShopProfile()
+        {
+            //User
+            this.CreateMap<ImportUserDTO, User>();
+        }
+    }
+}
