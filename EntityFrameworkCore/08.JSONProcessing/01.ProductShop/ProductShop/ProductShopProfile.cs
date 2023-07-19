b2d59064955d2c30b229953c9be356e97ae95cf9
unknown <Bones4pk@gmail.com>
2023-07-19 11:21:54 +0300

    Add new exercise

---
+++ EntityFrameworkCore/08.JSONProcessing/01.ProductShop/ProductShop/ProductShopProfile.cs
@@ -0,0 +1,17 @@
+﻿
+namespace ProductShop
+{
+
+    using AutoMapper;
+    using ProductShop.DTOs.Import;
+    using ProductShop.Models;
+
+    public class ProductShopProfile : Profile
+    {
+        public ProductShopProfile()
+        {
+            //User
+            this.CreateMap<ImportUserDto, User>();
+        }
+    }
+}
---
+++ EntityFrameworkCore/08.JSONProcessing/01.ProductShop/ProductShop/StartUp.cs
@@ -0,0 +1,46 @@
+﻿namespace ProductShop
+{
+
+    using AutoMapper;
+    using Newtonsoft.Json;
+
+    using ProductShop.Data;
+    using ProductShop.DTOs.Import;
+    using ProductShop.Models;
+
+    public class StartUp
+    {
+        private static IMapper mapper;
+        public static void Main()
+        {
+            mapper = new Mapper(new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<ProductShopProfile>();
+            }));
+
+            ProductShopContext context = new ProductShopContext();
+            
+            string inputJson = File.ReadAllText("../../../Datasets/users.json");
+            string result = ImportUsers(context, inputJson);
+            
+            Console.WriteLine(result);
+        }
+        public static string ImportUsers(ProductShopContext context, string inputJson)
+        {
+            var userDtos = JsonConvert.DeserializeObject<ImportUserDto[]>(inputJson);
+
+            ICollection<User> validUsers = new HashSet<User>();
+
+            foreach (ImportUserDto userDto in userDtos)
+            {
+                User user = mapper.Map<User>(userDto);
+                validUsers.Add(user);
+            }
+
+            context.Users.AddRange(validUsers);
+            context.SaveChanges();
+
+            return $"Successfully imported {validUsers.Count} users";
+        }
+    }
+}
