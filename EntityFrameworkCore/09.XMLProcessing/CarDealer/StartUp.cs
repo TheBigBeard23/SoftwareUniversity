b1e3e634c8059397ef390ee2eec9a45b31ff5af4
unknown <Bones4pk@gmail.com>
2023-07-24 20:09:06 +0300

    Add new exercise

---
+++ EntityFrameworkCore/09.XMLProcessing/CarDealer/StartUp.cs
@@ -0,0 +1,50 @@
+﻿using CarDealer.Data;
+using CarDealer.DTOs.Import;
+using CarDealer.Models;
+using CarDealer.Utilities;
+using System.Xml.Serialization;
+
+namespace CarDealer
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            CarDealerContext context = new CarDealerContext();
+            string xml = File.ReadAllText("../../../Datasets/suppliers.xml");
+            string result = ImportSuppliers(context, xml);
+            Console.WriteLine(result);
+        }
+
+        //---------------- Import Data ----------------
+
+        //01.Import Users
+        public static string ImportSuppliers(CarDealerContext context, string inputXml)
+        {
+            XmlHelper xmlHelper = new XmlHelper();
+
+            ImportSupplierDto[] supplierDtos =
+                xmlHelper.Deserialize<ImportSupplierDto[]>(inputXml, "Suppliers");
+
+            ICollection<Supplier> validSuppliers = new HashSet<Supplier>();
+
+            foreach (ImportSupplierDto supplierDto in supplierDtos)
+            {
+                if (supplierDto.Name != null)
+                {
+                    Supplier supplier = new Supplier()
+                    {
+                        Name = supplierDto.Name,
+                        IsImporter = supplierDto.IsImporter
+                    };
+                    validSuppliers.Add(supplier);
+                }
+            }
+
+            context.Suppliers.AddRange(validSuppliers);
+            context.SaveChanges();
+
+            return $"Successfully imported {validSuppliers.Count} suppliers!";
+        }
+    }
+}
---
+++ EntityFrameworkCore/09.XMLProcessing/CarDealer/Utilities/XmlHelper.cs
@@ -0,0 +1,40 @@
+﻿namespace CarDealer.Utilities
+{
+
+    using System.Xml.Serialization;
+
+    public class XmlHelper
+    {
+        public T Deserialize<T>(string inputXml, string rootName)
+        {
+            XmlRootAttribute xmlRoot = new XmlRootAttribute(rootName);
+
+            XmlSerializer xmlSerializer =
+                new XmlSerializer(typeof(T), xmlRoot);
+
+            StringReader sr = new StringReader(inputXml);
+
+            
+            T deserializedObject =
+                (T)xmlSerializer.Deserialize(sr);
+
+            return deserializedObject;
+        }
+
+        public IEnumerable<T> DeserializeCollection<T>(string inputXml, string rootName)
+        {
+            XmlRootAttribute xmlRoot = new XmlRootAttribute(rootName);
+
+            XmlSerializer xmlSerializer =
+                new XmlSerializer(typeof(T[]), xmlRoot);
+
+            StringReader sr = new StringReader(inputXml);
+
+
+            T[] deserializedObject =
+                (T[])xmlSerializer.Deserialize(sr);
+
+            return deserializedObject;
+        }
+    }
+}
