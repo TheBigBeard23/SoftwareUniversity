4d711c80734539f13ad6fa0817417d0bde442f54
unknown <Bones4pk@gmail.com>
2023-08-16 22:08:52 +0300

    Add new exercise

---
+++ ASP.NET Fundamentals/ChatApp/ChatApp/Controllers/ChatController.cs
@@ -0,0 +1,46 @@
+﻿using ChatApp.Models.Chat;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChatApp.Controllers
+{
+    public class ChatController : Controller
+    {
+        private static IList<KeyValuePair<string, string>> messages =
+            new List<KeyValuePair<string, string>>();
+        public IActionResult Show()
+        {
+            if (messages.Count < 1)
+            {
+                return View(new ChatViewModel());
+            }
+
+            var chatViewModel = new ChatViewModel()
+            {
+                AllMessage = messages
+                .Select(m => new MessageViewModel()
+                {
+                    Sender = m.Key,
+                    MessageText = m.Value
+                })
+                .ToArray()
+            };
+
+            return View(chatViewModel);
+        }
+
+        [HttpPost]
+        public IActionResult Send(ChatViewModel chatViewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return this.RedirectToAction("Show");
+            }
+
+            KeyValuePair<string, string> currentMessage = new KeyValuePair<string, string>(chatViewModel.CrnMessage.Sender, chatViewModel.CrnMessage.MessageText);
+
+            messages.Add(currentMessage);
+
+            return this.RedirectToAction("Show");
+        }
+    }
+}
---
+++ ASP.NET Fundamentals/ChatApp/ChatApp/Models/Chat/ChatViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace ChatApp.Models.Chat
+{
+    public class ChatViewModel
+    {
+        public ChatViewModel()
+        {
+            this.AllMessage = new HashSet<MessageViewModel>();
+        }
+        public MessageViewModel CrnMessage { get; set; } = null!;
+        public ICollection<MessageViewModel> AllMessage { get; set; } = null!; 
+
+
+    }
+}
