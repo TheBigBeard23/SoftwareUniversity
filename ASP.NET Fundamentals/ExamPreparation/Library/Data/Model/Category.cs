b86450f88086acfcb381da81fbe0364ccc848121
unknown <Bones4pk@gmail.com>
2023-09-18 21:04:14 +0300

    Add new exercise

---
+++ ASP.NET Fundamentals/ExamPreparation/Library/Data/Model/Category.cs
@@ -0,0 +1,20 @@
+﻿namespace Library.Data.Model
+{
+    using Microsoft.EntityFrameworkCore;
+    using System.ComponentModel.DataAnnotations;
+
+    [Comment("Categories for the books")]
+    public class Category
+    {
+        [Comment("Primary key")]
+        [Key]
+        public int Id { get; set; }
+
+        [Comment("Name of the category")]
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+        public IEnumerable<Book> Books { get; set; } = new HashSet<Book>();
+    }
+}
