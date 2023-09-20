aed77479c1ecc717ea7dd1c305b84b7b799ae9ec
unknown <Bones4pk@gmail.com>
2023-09-20 22:28:21 +0300

    edit login page access

---
+++ ASP.NET Fundamentals/ExamPreparation/Library/Controllers/BaseController.cs
@@ -0,0 +1,12 @@
+﻿namespace Library.Controllers
+{
+
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+        
+    [Authorize]
+    public class BaseController : Controller
+    {
+
+    }
+}
