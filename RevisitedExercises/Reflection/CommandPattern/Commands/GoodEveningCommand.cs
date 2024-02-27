f3c124a2016cafb948e1d16229b3fb028f2b2d56
unknown <Bones4pk@gmail.com>
2024-02-28 00:26:25 +0200

    Add new exercise

---
+++ RevisitedExercises/Reflection/CommandPattern/Commands/GoodEveningCommand.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommandPattern.Commands
+{
+    public class GoodEveningCommand : ICommand
+    {
+        public string Execute(string[] args)
+        {
+            return $"Good Evening, Mr. {args[0]}";
+        }
+    }
+}
--- RevisitedExercises/Reflection/CommandPattern/StartUp.cs
+++ RevisitedExercises/Reflection/CommandPattern/StartUp.cs
@@ -1,14 +1,18 @@
 ﻿using System;
+using CommandPattern.Commands;
+using CommandPattern.Core;
+using CommandPattern.Core.Contracts;
 
 namespace CommandPattern
 {
     public class StartUp
     {
-        public static void Main(string[] args)
+        public static void Main()
         {
-            //ICommandInterpreter command = new CommandInterpreter();
-            //IEngine engine = new Engine(command);
-            //engine.Run();
+            ICommandInterpreter command = new CommandInterpreter(new CommandFactory());
+            IEngine engine = new Engine(command);
+
+            engine.Run();
         }
     }
 }
