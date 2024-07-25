dc80385f280732fad2c77836be61a46121cc259c
unknown <Bones4pk@gmail.com>
2024-07-25 13:12:36 +0300

    add new exercise

---
+++ ASP.NET Fundamentals/ForumApp/ForumApp/Controllers/PostController.cs
@@ -0,0 +1,106 @@
+﻿using ForumApp.Data;
+using ForumApp.Data.Models;
+using ForumApp.Models.Post;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ForumApp.Controllers
+{
+    public class PostController : Controller
+    {
+        private readonly ForumAppDbContext _data;
+        public PostController(ForumAppDbContext data)
+        {
+            _data = data;
+        }
+        [HttpGet]
+        public async Task<IActionResult> All()
+        {
+            var posts = await _data
+                .Posts
+                .Select(p => new PostViewModel()
+                {
+                    Id = p.Id,
+                    Title = p.Title,
+                    Content = p.Content,
+                })
+                .ToListAsync();
+
+            return View(posts);
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(PostFormModel model)
+        {
+            var post = new Post()
+            {
+                Title = model.Title,
+                Content = model.Content,
+            };
+
+            await _data.Posts.AddAsync(post);
+            await _data.SaveChangesAsync();
+
+            return RedirectToAction("All");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(int id)
+        {
+            try
+            {
+                var post = await _data.Posts.FindAsync(id);
+
+                return View(new PostFormModel()
+                {
+                    Title = post.Title,
+                    Content = post.Content
+                });
+            }
+            catch (Exception)
+            {
+                return this.RedirectToAction("All", "Post");
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, PostFormModel model)
+        {
+            var post = await _data.Posts.FindAsync(id);
+
+            post.Title = model.Title;
+            post.Content = model.Content;
+
+            await _data.SaveChangesAsync();
+
+            return RedirectToAction("All");
+
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(int id)
+        {
+            try
+            {
+                var post = await _data.Posts.FindAsync(id);
+
+                _data.Posts.Remove(post);
+                await _data.SaveChangesAsync();
+            }
+            catch (Exception)
+            {
+                return this.RedirectToAction("All", "Post");
+            }
+
+            return RedirectToAction("All");
+        }
+
+
+    }
+}
---
+++ ASP.NET Fundamentals/ForumApp/ForumApp/Data/ForumAppDbContext.cs
@@ -0,0 +1,53 @@
+﻿using ForumApp.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ForumApp.Data
+{
+    public class ForumAppDbContext : DbContext
+    {
+        private Post FirstPost { get; set; }
+        private Post SecondPost { get; set; }
+        private Post ThirdPost { get; set; }
+        public ForumAppDbContext(DbContextOptions<ForumAppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Post> Posts { get; init; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            SeedPosts();
+
+            modelBuilder
+                .Entity<Post>()
+                .HasData(FirstPost, SecondPost, ThirdPost);
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        private void SeedPosts()
+        {
+            FirstPost = new Post()
+            {
+                Id = 1,
+                Title = "My First Post",
+                Content = "My first post will be about performing CRUD operations in MVC. It's so much fun!"
+            };
+
+            SecondPost = new Post()
+            {
+                Id = 2,
+                Title = "My Second Post",
+                Content = "This is second post.CRUD operations is MVC ate getting more and more interesting!"
+            };
+
+            ThirdPost = new Post()
+            {
+                Id = 3,
+                Title = "My Third Post",
+                Content = "Hello there! I'm getting better and better with the CRUD operations in MVC. Stay tuned!"
+            };
+        }
+    }
+}
